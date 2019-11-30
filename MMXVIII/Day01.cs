d258078a23e3ec609990d5ad17807f7e04dc3721
benjymous <r.munn@outlook.com>
2019-11-30 22:06:35 +0000

    Added basic framework

---
+++ MMXVIII/Day01.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent.MMXVIII
+{
+    public class Day01
+    {
+        public static int Callibrate01(string input)
+        {
+            var parts = Util.Split(input);
+
+            int sum = 0;
+
+            foreach (var part in parts)
+            {
+                if (!string.IsNullOrWhiteSpace(part))
+                {
+                    sum += Int32.Parse(part.Trim());
+                }
+            }
+
+            return sum;
+        }
+
+        public static int Callibrate02(string input)
+        {
+            var seen = new HashSet<int>();
+
+            var parts = Util.Split(input);
+
+            int freq = 0;
+
+            while (true)
+            {
+
+                foreach (var part in parts)
+                {
+                    if (!string.IsNullOrWhiteSpace(part))
+                    {
+                        seen.Add(freq);
+
+                        freq += Int32.Parse(part.Trim());
+
+                        if (seen.Contains(freq))
+                        {
+                            return freq;
+                        }                        
+                    }
+                }
+            }
+        }
+    }
+}
