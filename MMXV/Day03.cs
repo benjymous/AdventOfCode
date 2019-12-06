3badaf8bc46479f5c1007ed7c51cf162085aa4d4
benjymous <r.munn@outlook.com>
2019-12-06 20:29:49 +0000

    Added placeholders for other years, and template for next

---
+++ MMXV/Day03.cs
@@ -0,0 +1,28 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+
+// namespace Advent.MMXV
+// {
+//     public class Day09 : IPuzzle
+//     {
+//         public string Name { get { return "2015-09";} }
+ 
+//         public static int Part1(string input)
+//         {
+//             return 0;
+//         }
+
+//         public static int Part2(string input)
+//         {
+//             return 0;
+//         }
+
+//         public void Run(string input)
+//         {
+//             Console.WriteLine("- Pt1 - "+Part1(input));
+//             Console.WriteLine("- Pt2 - "+Part2(input));
+//         }
+//     }
+// }
