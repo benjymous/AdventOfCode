caf81f15da7536d6f2ed87fd9ce6f0ffcef51656
benjymous <r.munn@outlook.com>
2019-12-10 12:52:12 +0000

    2019 Day10 part2

---
+++ test/MMXIX/Day10Test.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Advent.MMXIX.Test
+{
+    [TestCategory("2019")]
+    [TestClass]
+    public class Day10Test
+    {
+        string input = Util.GetInput<Day10>();
+
+        [TestCategory("Test")]
+        [DataRow(".#..#,.....,#####,....#,...##", 8)]
+        [DataRow("......#.#.,#..#.#....,..#######.,.#.#.###..,.#..#.....,..#....#.#,#..#....#.,.##.#..###,##...#..#.,.#....####", 33)]
+        [DataRow("#.#...#.#.,.###....#.,.#....#...,##.#.#.#.#,....#.#.#.,.##..###.#,..#...##..,..##....##,......#...,.####.###.", 35)]
+        [DataRow(".#..#..###,####.###.#,....###.#.,..###.##.#,##.##.#.#.,....###..#,..#.#..#.#,#..#.#.###,,.##...##.#,.....#.#..", 41)]
+        [DataRow(".#..##.###...#######,##.############..##.,.#.######.########.#,.###.#######.####.#.,#####.##.#.##.###.##,..#####..#.#########,####################,#.####....###.#.#.##,##.#################,#####.##.###..####..,..######..##.#######,####.##.####...##..#,.#####..#.######.###,##...#.##########...,#.##########.#######,.####.#.###.###.#.##,....##.##.###..#####,.#.#.###########.###,#.#.#.#####.####.###,###.##.####.##.#..##", 210)]
+        [DataTestMethod]
+        public void AsteroidsTest(string input, int expected)
+        {
+            Assert.AreEqual(expected, Day10.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Asteroids_Part1_Regression()
+        {
+            Assert.AreEqual(278, Day10.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Asteroids_Part2_Regression()
+        {
+            Assert.AreEqual(1417, Day10.Part2(input, 200));
+        }
+    }
+}
