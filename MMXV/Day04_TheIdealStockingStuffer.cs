ab76dce24bf6e931e7e4416d47b97122e033f259
benjymous <r.munn@outlook.com>
2019-12-12 21:30:51 +0000

    More 2016 solutions

--- MMXV/Day04_TheIdealStockingStuffer.cs
+++ MMXV/Day04_TheIdealStockingStuffer.cs
@@ -10,45 +10,14 @@ namespace Advent.MMXV
     {
         public string Name { get { return "2015-04";} }
 
-        static bool IsHash(int num, string baseStr, string prefix)
-        {
-            string tryHash = baseStr + num.ToString();
-            string hashed = tryHash.GetMD5String();
-            return hashed.StartsWith(prefix);
-        }
- 
-        public static IEnumerable<int> Forever()
-        {
-            int i=0;
-            while(true) { yield return i++; }
-        }
-
-        public static int FindHash2(string baseStr, int numZeroes)
-        {
-            var prefix = String.Join("", Enumerable.Repeat('0', numZeroes));
-            int num=0;
-            while (true)
-            {
-                if (IsHash(num, baseStr, prefix)) return num;
-                num++;
-            }
-        }
-
-        public static int FindHash(string baseStr, int numZeroes)
-        {
-            var prefix = String.Join("", Enumerable.Repeat('0', numZeroes));
-            
-            return Forever().Where(n => IsHash(n, baseStr, prefix)).First();
-        }
-
         public static int Part1(string input)
         {
-            return FindHash(input.Trim(), 5);
+            return HashBreaker.FindHash(input.Trim(), 5);
         }
 
         public static int Part2(string input)
         {
-            return FindHash(input.Trim(), 6);
+            return HashBreaker.FindHash(input.Trim(), 6);
         }
 
         public void Run(string input, System.IO.TextWriter console)
---
+++ MMXVI/Day07_InternetProtocolVersion7.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent.MMXVI
+{
+    public class Day07 : IPuzzle
+    {
+        public string Name { get { return "2016-07";} }
+
+        static bool HasAbba(string address)
+        {
+            bool hasAbba = false;
+            int bracketCount = 0;
+            for (int i=0; i<address.Length-3; ++i)
+            {
+                if (address[i]=='[') 
+                {
+                    bracketCount++;
+                }
+                else if (address[i]==']')
+                {
+                    bracketCount--;
+                }
+                else  if (address[i] == address[i+3] && address[i+1] == address[i+2] && address[i]!=address[i+1])
+                {
+                    if (bracketCount > 0) 
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        hasAbba = true;
+                    }
+                }
+            }
+
+            return hasAbba;
+        }
+
+        static bool HasAbaBab(string address)
+        {
+            int bracketCount = 0;
+
+            HashSet<string> abas = new HashSet<string>();
+            HashSet<string> babs = new HashSet<string>();
+
+            for (int i=0; i<address.Length-2; ++i)
+            {
+                if (address[i]=='[') 
+                {
+                    bracketCount++;
+                }
+                else if (address[i]==']')
+                {
+                    bracketCount--;
+                }
+                else  if (address[i] == address[i+2] && address[i]!=address[i+1])
+                {
+                    var tla = $"{address[i]}{address[i+1]}{address[i+2]}";
+                    if (bracketCount > 0) 
+                    {
+                        babs.Add(tla);
+                    }
+                    else
+                    {
+                        abas.Add(tla);
+                    }
+                }
+            }
+
+            foreach (var aba in abas)
+            {
+                if (babs.Contains($"{aba[1]}{aba[0]}{aba[1]}")) return true;
+            }
+
+            return false;
+        }
+ 
+        public static int Part1(string input)
+        {
+            return Util.Split(input).Where(i => HasAbba(i)).Count();
+        }
+
+        public static int Part2(string input)
+        {
+            return Util.Split(input).Where(i => HasAbaBab(i)).Count();
+        }
+
+        public void Run(string input, System.IO.TextWriter console)
+        {
+            // console.WriteLine(HasAbba("--abba--"));
+            // console.WriteLine(HasAbba("abba--"));
+            // console.WriteLine(HasAbba("--abba"));
+            // console.WriteLine(HasAbba("--abab--"));
+            // console.WriteLine(HasAbba("--aba--"));
+
+            // console.WriteLine(HasAbba("abba[mnop]qrst"));  // true
+            // console.WriteLine(HasAbba("abcd[bddb]xyyx"));  // false
+            // console.WriteLine(HasAbba("aaaa[qwer]tyui"));  // false
+            // console.WriteLine(HasAbba("ioxxoj[asdfgh]zxcvbn"));  // true
+
+            //console.WriteLine(HasAbaBab("aba[bab]xyz")); // true
+            //console.WriteLine(HasAbaBab("xyx[xyx]xyx")); // false
+            //console.WriteLine(HasAbaBab("aaa[kek]eke")); // true
+            //console.WriteLine(HasAbaBab("zazbz[bzb]cdb")); // true
+
+            console.WriteLine("- Pt1 - "+Part1(input));
+            console.WriteLine("- Pt2 - "+Part2(input));
+        }
+    }
+}
