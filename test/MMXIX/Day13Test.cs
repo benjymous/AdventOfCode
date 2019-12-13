b32bc2a02245705d7bf75c766c8b79fb044bb4b1
benjymous <r.munn@outlook.com>
2019-12-13 16:13:34 +0000

    Added day 13 tests

---
+++ test/MMXIX/Day13Test.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Advent.MMXIX.Test
+{
+    [TestCategory("2019")]
+    [TestCategory("IntCPU")]
+    [TestClass]
+    public class Day13Test
+    {
+        string input = Util.GetInput<Day13>();
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Pong_Part1_Regression()
+        {
+            Assert.AreEqual(230, Day13.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Pong_Part2_Regression()
+        {     
+            Assert.AreEqual(11140, Day13.Part2(input));
+        }
+
+    }
+}
