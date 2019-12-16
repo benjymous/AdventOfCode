d0081842f53238540e83e11da807796f632be7c3
benjymous <r.munn@outlook.com>
2019-12-16 15:40:42 +0000

    Day16 part 2

--- MMXIX/Day16_FlawedFrequencyTransmission.cs
+++ MMXIX/Day16_FlawedFrequencyTransmission.cs
@@ -9,44 +9,37 @@ namespace Advent.MMXIX
     {
         public string Name { get { return "2019-16";} }
 
-        static int[] initialPattern = new int[] {0,1,0,-1};
+        static int[] initialPattern = new int[] {0, 1, 0, -1};
 
-        static IEnumerable<int> GetPattern(int element)
-        {
-            var sequence = Util.DuplicateDigits(Util.RepeatForever(initialPattern), element+1);
+        public static int SequenceAt(int iter, int pos) => initialPattern[((pos+1)/(iter+1)%4)];
 
-            return sequence.Skip(1);
-        }
+        static int[] ApplyFFT(int[] input)
+        {          
+            var output = new List<int>();
 
-        static string ApplyFFT(string input)
-        {
-            StringBuilder sb = new StringBuilder();
-            var ints = input.Trim().ToCharArray().Select(ch => (Int64)ch-'0').ToArray();
-
-            for (int i=0; i<ints.Length; ++i)
+            for (int i=0; i<input.Length; ++i)
             {
-                Int64 outVal = 0;
-                var sequence = GetPattern(i).Take(ints.Length).ToArray();
-                for (int j=0; j<ints.Length; ++j)
+                int outVal = 0;
+                for (int j=i; j<input.Length; ++j)
                 {
-                    outVal += ints[j]*sequence[j];
+                    outVal += input[j]*SequenceAt(i, j);
                 }
-                var outStr = $"{outVal}";
-                sb.Append(outStr.Last());
 
+                var outDigit = Math.Abs(outVal) % 10;
+                output.Add(outDigit);
             }
 
-            return sb.ToString();
+            return output.ToArray();
         }
 
         public static string ApplyRepeatedFFT(string initial, int repeats)
         {
-            string current = initial;
+            var current = initial.Trim().ToCharArray().Select(ch => (int)ch-'0').ToArray();
             for (var i=0; i<repeats; ++i)
             {
                 current = ApplyFFT(current);
             }
-            return current;
+            return string.Join("", current.Select(i => $"{i}"));
         }
  
         public static string Part1(string input)
@@ -54,17 +47,61 @@ namespace Advent.MMXIX
             return ApplyRepeatedFFT(input, 100).Substring(0, 8);
         }
 
-        public static int Part2(string input)
+        // Thanks to FirescuOvidiu
+        // All 1s in second half, past leading zeroes
+        static List<int> ProcessSignal(List<int> sequence)
         {
-            var initial = ApplyRepeatedFFT(input, 100);
 
-            return 0;
+            List<int> newSequence = Enumerable.Repeat(0, sequence.Count).ToList();
+            int sizeSequence = sequence.Count;
+            int sum = 0;
+            int phase = 0;
+
+            while(phase<100)
+            {
+                sum = 0;
+                for (int position = sizeSequence - 1; position >= sizeSequence / 2; position--)
+                {
+                    sum += sequence[position];
+                    newSequence[position] = sum % 10;
+                }
+                sequence = newSequence;
+                phase++;
+            }
+
+            return sequence;
+        }
+
+        public static string Part2(string input)
+        {
+            var initial = string.Join("", Enumerable.Repeat(input.Trim(), 10000)).Select(c => (int)c).ToList();
+
+            var signal = ProcessSignal(initial);
+
+            int messageOffset = int.Parse(input.Substring(0, 7));
+
+            var outStr = signal.Skip(messageOffset).Take(8).Select(c => (char)('0'+c)).ToArray().AsString();
+
+            return outStr;
         }
 
         public void Run(string input, System.IO.TextWriter console)
         {
+            // for (int i=0; i<1000; ++i)
+            // {
+            //     TestSequence(i);
+            // }
+
+            //Console.WriteLine(Part1("80871224585914546619083218645595")); // 24176176
+
+            //Console.WriteLine("98765432109876543210".Skip(7).Take(8).AsString());
+
+            Console.WriteLine(Part2("03036732577212944063491565474664")); // 84462026
+            Console.WriteLine(Part2("02935109699940807407585447034323")); // 78725270
+            Console.WriteLine(Part2("03081770884921959731165446850517")); // 53553731
+
             console.WriteLine("- Pt1 - "+Part1(input));
-            // console.WriteLine("- Pt2 - "+Part2(input));
+            console.WriteLine("- Pt2 - "+Part2(input));
         }
     }
 }
