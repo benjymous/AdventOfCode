1ab03328792360dc05b8cdd25fab8f3893ba5438
benjymous <r.munn@outlook.com>
2019-12-19 14:46:23 +0000

    Day18 part 1

--- MMXIX/Day18_ManyWorldsInterpretation.cs
+++ MMXIX/Day18_ManyWorldsInterpretation.cs
@@ -26,181 +26,71 @@ namespace Advent.MMXIX
 
         public class Callback : AStar.ICanWalk
         {
-            public HashSet<char> HeldKeys {get;set;}
             public bool IsWalkable(IRoom room)
             {
-                int data = room.Data();
-                return data == '.' || (data >='a' && data <='z') || HeldKeys.Contains((Char)data);
+                // we don't care about keys or doors, just the shortest possible path
+                return room.Data() != '#';
             }
         }
 
-        // static Dictionary<string, AStar.IRoom> CloneMap(Dictionary<string, AStar.IRoom> map, char remove)
-        // {
-        //     var newMap = new Dictionary<string, AStar.IRoom>();
-
-        //     foreach (var kvp in map)
-        //     {
-        //         var node = kvp.Value as Node;
-        //         char c = node.data;
-        //         if (remove == c) c = '.';
-        //         newMap[kvp.Key] = new Node(c);
-        //     }
-        //     return newMap;
-        // }
-
-        static Dictionary<char, ManhattanVector2> CloneKeys(Dictionary<char, ManhattanVector2> keys, char remove)
+        public static int KeyCode(char c)
         {
-            var newKeys = new Dictionary<char, ManhattanVector2>();
-            foreach (var k in keys)
-            {
-                if (k.Key != remove)
-                {
-                    newKeys[k.Key] = k.Value;
-                }
-            }
-            return newKeys;
+            return (1 << char.ToLower(c)-'a');
         }
 
-         static int min = int.MaxValue;
-
-        //         public static int Solve(Dictionary<string, AStar.IRoom> map, ManhattanVector2 position, Dictionary<char, ManhattanVector2> availableKeys, HashSet<char> allKeys, int totalScore)
-        //         {
-        //             if (availableKeys.Count == 0)
-        //             {
-        //                 min = Math.Min(totalScore, min);
-        //                 Console.WriteLine($"[{min}] - Path found at {totalScore}");
-
-        //                 return 0;
-        //             }
-        //             var finder = new AStar.RoomPathFinder();
-
-
-
-        //             var callback = new Callback();
-        //             callback.HeldKeys = new HashSet<char>(allKeys.Where(k => !availableKeys.ContainsKey(k)).Select(k => char.ToUpper(k)));
-
-        //             var paths = availableKeys.Select(k => Tuple.Create(k, finder.FindPath(map, position, k.Value, callback))).Where(v => v.Item2.Count() > 0 && (v.Item2.Count() + totalScore) < min).OrderBy(v => v.Item2.Count());
-
-        //             if (!paths.Any()) return 9999;
-
-        // #if BLAH
-        //             var shortestPath = paths.AsParallel().Select(tup => tup.Item2.Count + Solve(map, tup.Item1.Value, CloneKeys(availableKeys, tup.Item1.Key), allKeys, totalScore+tup.Item2.Count)).Min();
-        // #else
-        //             int shortestPath = int.MaxValue;
-        //             foreach (var tup in paths)
-        //             {           
-        //                 var path = tup.Item2;
-        //                 var key = tup.Item1;
-        //                 if (path.Count < shortestPath)
-        //                 {
-        //                     int newVal = path.Count + Solve(map, key.Value, CloneKeys(availableKeys, key.Key), allKeys, totalScore+path.Count);
-        //                     shortestPath = Math.Min(shortestPath, newVal);
-        //                 }
-        //             }
-        // #endif
-
-        //             // foreach (var key in availableKeys.OrderBy(k => k.Key))
-        //             // {
-        //             //     var path = finder.FindPath(map, position, key.Value);
-        //             //     if (path.Any())
-        //             //     {               
-        //             //         if (path.Count < shortestPath)
-        //             //         {
-        //             //             int newVal = path.Count + Solve(CloneMap(map, char.ToUpper(key.Key)), key.Value, CloneKeys(availableKeys, key.Key), doors, totalScore+path.Count);
-        //             //             shortestPath = Math.Min(shortestPath, newVal);
-        //             //         }
-        //             //     }
-        //             // }
-
-        //             return shortestPath;
-        //         }
-
-        // public static int Solve2(ManhattanVector2 position, Dictionary<string, AStar.IRoom> map, Dictionary<string, IEnumerable<ManhattanVector2>> paths, HashSet<char> allKeys, Dictionary<char, ManhattanVector2> availableKeys, int totalScore)
-        // {
-
-        //     if (availableKeys.Count == 0)
-        //     {
-        //         min = Math.Min(totalScore, min);
-        //         Console.WriteLine($"[{min}] - Path found at {totalScore}");
-        //         return 0;
-        //     }
-
-        //     int shortestPath = int.MaxValue;
-        //     var heldKeys = new HashSet<char>(allKeys.Where(k => !availableKeys.ContainsKey(k)).Select(k => char.ToUpper(k)));
-
-        //     List<Tuple<char, ManhattanVector2, IEnumerable<ManhattanVector2>>> possiblePaths = new List<Tuple<char, ManhattanVector2, IEnumerable<ManhattanVector2>>>();
-
-        //     foreach (var key in availableKeys.OrderBy(k => k.Key))
-        //     {
-
-        //         var pathKey = $"{position}:{key.Value}";
-
-        //         IEnumerable<ManhattanVector2> path;
-
-        //         if (!paths.TryGetValue(pathKey, out path))
-        //         {
-        //             pathKey = $"{key.Value}:{position}";
-
-        //             if (!paths.TryGetValue(pathKey, out path))
-        //             {
-        //                 throw new Exception("Couldn't find path!");
-        //             }
-        //         }
-
-        //         if (IsWalkable(map, path, heldKeys))
-        //         {
-        //             //var newVal = path.Count() + Solve(key.Value, map, paths, allKeys, CloneKeys(availableKeys, key.Key), totalScore+path.Count());
-        //             //shortestPath = Math.Min(shortestPath, newVal);
-
-        //             possiblePaths.Add(Tuple.Create(key.Key, key.Value, path));
-        //         }
-        //     }
-
-        //     if (totalScore == 0)
-        //     { 
-        //         return possiblePaths.OrderBy(t => t.Item3.Count()).AsParallel().Select(t => Solve(t.Item2, map, paths, allKeys, CloneKeys(availableKeys, t.Item1), totalScore + t.Item3.Count())).Min();
-        //     }
-        //     else
-        //     {
-        //         return possiblePaths.OrderBy(t => t.Item3.Count()).Select(t => Solve(t.Item2, map, paths, allKeys, CloneKeys(availableKeys, t.Item1), totalScore + t.Item3.Count())).Min();
-        //     }
-        // }
-
-        public static bool IsWalkable(Dictionary<string, AStar.IRoom> map, IEnumerable<ManhattanVector2> path, HashSet<char> heldKeys)
+        public static int AddKey(int v, char c)
         {
-            foreach (var pos in path)
+            return v | KeyCode(c);
+        }
+
+        public static IEnumerable<int> Bits(int v)
+        {
+            for(int k=1; k<=v; k<<=1)
             {
-                var node = map.GetObjKey(pos);
-                if (node==null) 
+                if ((v & k)>0) yield return k;
+            }
+        }
+
+        public class RoomPath
+        {
+            public RoomPath(Dictionary<string, AStar.IRoom> map, IEnumerable<ManhattanVector2> path)
+            {
+                Path = path;
+
+                List<char> seenDoors = new List<char>();
+
+                foreach (var pos in path)
                 {
-                    return false;
-                }
-                var c = (node as Node).data;
-                if (c != '.')
-                {
+                    var node = map.GetObjKey(pos);              
+                    var c = (node as Node).data;
+
                     if (c>='A' && c <='Z')
                     {
-                        if (!heldKeys.Contains(char.ToLower(c)))
-                        {
-                            return false;
-                        }
-                    }
+                        Doors = AddKey(Doors, c);
+                    }                  
                 }
+
+                Count = path.Count();
             }
-            return true;
+
+            public bool IsWalkable(int heldKeys) => (Doors & heldKeys) == Doors;
+
+            public IEnumerable<ManhattanVector2> Path {get;private set;}
+
+            public int Count {get;private set;}
+
+            int Doors = 0;
         }
  
         public static int Part1(string input)
         {
-            min = int.MaxValue;
-
             var lines = Util.Split(input);
 
             var map = new Dictionary<string, AStar.IRoom>();
             ManhattanVector2 startPosition = new ManhattanVector2(0,0);
-            var availableKeys = new Dictionary<char, ManhattanVector2>();
-            var allKeys = new HashSet<char>();
-            var doors = new Dictionary<char, ManhattanVector2>();
+            var keyPositions = new Dictionary<int, ManhattanVector2>();
+            int allKeys = 0;
+            var doors = new Dictionary<int, ManhattanVector2>();
 
             for (var y=0; y<lines.Length; ++y)
             {
@@ -215,38 +105,38 @@ namespace Advent.MMXIX
                     }
                     if (c>='a' && c <='z')
                     {
-                        availableKeys[c] = new ManhattanVector2(x,y);
-                        allKeys.Add(c);
+                        keyPositions[KeyCode(c)] = new ManhattanVector2(x,y);
+                        allKeys|=KeyCode(c);
                     }
                     if (c>='A' && c <='Z')
                     {
-                        doors[char.ToLower(c)] = new ManhattanVector2(x,y);
+                        doors[KeyCode(c)] = new ManhattanVector2(x,y);
                     }
                     map.PutStrKey($"{x},{y}", new Node(c));
                 }
             }
 
-            var paths = new Dictionary<string, IEnumerable<ManhattanVector2>>();
+            var paths = new Dictionary<int, RoomPath>();
 
             var finder = new AStar.RoomPathFinder();
             var callback = new Callback();
-            callback.HeldKeys = new HashSet<char>(allKeys.Select(c => char.ToUpper(c)));
 
-            foreach (var k1 in allKeys)
+            foreach (var k1 in Bits(allKeys))  
             {
-                paths[$"{startPosition}:{availableKeys[k1]}"] = finder.FindPath(map, startPosition, availableKeys[k1], callback);
-
-                foreach (var k2 in allKeys.Where(k => k > k1))
+                paths[k1] = new RoomPath(map, finder.FindPath(map, startPosition, keyPositions[k1], callback));
+                foreach (var k2 in Bits(allKeys))  
                 {
-                    var path = finder.FindPath(map, availableKeys[k1], availableKeys[k2], callback);
-                    paths[$"{availableKeys[k1]}:{availableKeys[k2]}"] = path;
-                    paths[$"{availableKeys[k2]}:{availableKeys[k1]}"] = path;
+                    if (k2 > k1)
+                    {
+                        var path = new RoomPath(map,  finder.FindPath(map, keyPositions[k1], keyPositions[k2], callback));
+                        paths[k1|k2] = path;
+                    }
                 }
             }
 
-            var queue = new Queue<Tuple<ManhattanVector2, HashSet<char>, int>>();
+            var queue = new Queue<Tuple<int, int, int>>();
 
-            queue.Enqueue(Tuple.Create(startPosition, new HashSet<char>(), 0));
+            queue.Enqueue(Tuple.Create(0, 0, 0));
 
             var cache = new Dictionary<string,int>();
 
@@ -260,19 +150,18 @@ namespace Advent.MMXIX
                 var heldKeys = item.Item2;
                 int distance = item.Item3;
 
-                var tryKeys = availableKeys.Where(k => !heldKeys.Contains(k.Key));
+                int tryKeys = allKeys - heldKeys;
 
-                if (tryKeys.Any())
+                if (tryKeys>0)
                 {
-                    foreach (var key in tryKeys)
+                    foreach (var key in Bits(tryKeys))
                     {
-                        var path = paths[$"{position}:{key.Value}"];
-                        if (IsWalkable(map, path, heldKeys))
+                        var path = paths[position|key];
+                        if (path.IsWalkable(heldKeys))
                         {
-                            var newKeys = new HashSet<char>(heldKeys);
-                            newKeys.Add(key.Key);
-                            var next = Tuple.Create(key.Value, newKeys, distance+path.Count());
-                            var nextKey = $"{next.Item1}:{next.Item2.OrderBy(c=>c).AsString()}";
+                            var newKeys = heldKeys|key;
+                            var next = Tuple.Create(key, newKeys, distance+path.Count);
+                            var nextKey = $"{next.Item1}:{next.Item2}";
                             if (!cache.TryGetValue(nextKey, out int cachedBest))
                             {
                                 cachedBest = int.MaxValue;
@@ -282,17 +171,12 @@ namespace Advent.MMXIX
                                 cache[nextKey] = next.Item3;
                                 queue.Enqueue(next);
                             }
-                            // else
-                            // {
-                            //     Console.WriteLine($"Skipped {nextKey}");
-                            // }
                         }
                     }
                 }
                 else
                 {
                     currentBest = Math.Min(currentBest, distance);
-                    //Console.WriteLine($"Current best: {currentBest}");
                 }
             }
 
@@ -315,16 +199,7 @@ namespace Advent.MMXIX
 
         public void Run(string input, System.IO.TextWriter console)
         {
-
-            //Test(Part1("#########\n#b.A.@.a#\n#########"), 8);
-
-            //Test(Part1("########################\n#f.D.E.e.C.b.A.@.a.B.c.#\n######################.#\n#d.....................#\n########################"), 86);
-        
-            //Test(Part1("########################\n#...............b.C.D.f#\n#.######################\n#.....@.a.B.c.d.A.e.F.g#\n########################"), 132);
-
-            //Test(Part1("#################\n#i.G..c...e..H.p#\n########.########\n#j.A..b...f..D.o#\n########@########\n#k.E..a...g..B.n#\n########.########\n#l.F..d...h..C.m#\n#################"), 136);
-
-            //console.WriteLine("- Pt1 - "+Part1(input));
+            console.WriteLine("- Pt1 - "+Part1(input));
             //console.WriteLine("- Pt2 - "+Part2(input));
         }
     }
