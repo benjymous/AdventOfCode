03234e6268153dd0e2631a38aec98468021c11f2
benjymous <r.munn@outlook.com>
2019-12-23 16:09:34 +0000

    More 2015 solutions

--- MMXV/Day12_JSAbacusFrameworkIO.cs
+++ MMXV/Day12_JSAbacusFrameworkIO.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Advent.MMXV
 {
@@ -32,15 +34,64 @@ namespace Advent.MMXV
                 yield return int.Parse(current.ToString());
             }
         }
- 
+
         public static int Part1(string input)
         {
             return FindNumbers(input).Sum();
         }
 
+        static bool HasRed(dynamic jsonObj)
+        {
+            if (jsonObj.red != null) return true;
+
+            foreach (var child in jsonObj)
+            {
+                if (child.Value.Type == JTokenType.String && child.Value == "red") 
+                    return true;
+            }
+
+            return false;
+        }
+
+        static int GetSum(dynamic jsonObj)
+        {
+            if (jsonObj.Type == JTokenType.String) return 0;
+            if (jsonObj.Type == JTokenType.Integer) return (int)jsonObj;
+
+            if (jsonObj.Type == JTokenType.Array) 
+            {
+                int sum = 0;
+
+                foreach (var child in jsonObj)
+                {
+                    sum += GetSum(child);
+                }
+                return sum;
+            }
+
+
+            if (HasRed(jsonObj))
+            {
+                return 0;
+            }
+            else
+            {
+                //Console.WriteLine(jsonObj);
+                int sum = 0;
+                foreach (var child in jsonObj)
+                {
+                    //Console.WriteLine($"{child.Name}");
+                    sum += GetSum(child.Value);
+                }
+                return sum;
+            }
+        }
+
         public static int Part2(string input)
         {
-            return 0;
+            dynamic jsonObj = JsonConvert.DeserializeObject(input);
+         
+            return GetSum(jsonObj);
         }
 
         public void Run(string input, System.IO.TextWriter console)
@@ -51,6 +102,11 @@ namespace Advent.MMXV
             // {
             //     console.WriteLine(i);
             // }
+
+            // console.WriteLine(Part2("[1,2,3]"));
+            // console.WriteLine(Part2("[1,{\"c\":\"red\",\"b\":2},3"));
+            // console.WriteLine(Part2("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}"));
+            // console.WriteLine(Part2("[1,\"red\",5]"));
 
             console.WriteLine("- Pt1 - "+Part1(input));
             console.WriteLine("- Pt2 - "+Part2(input));
---
+++ MMXV/Day19_MedicineForRudolph.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent.MMXV
+{
+    public class Day19 : IPuzzle
+    {
+        public string Name { get { return "2015-19";} }
+ 
+        public static int Part1(string input)
+        {
+            var lines = Util.Split(input);
+
+            var rules = lines.Take(lines.Length-1).Select(x => x.Split(" => "));
+            var molecule = lines.Last();
+
+            HashSet<string> results = new HashSet<string>();
+
+            foreach (var rule in rules)
+            {
+                var indices = molecule.AllIndexesOf(rule[0]);
+                foreach (var i in indices)
+                {
+                    var newStr = molecule.ReplaceAtIndex(i, rule[0], rule[1]);
+                    results.Add(newStr);
+                }
+            }
+
+            return results.Count;
+        }
+
+        public static int Part2(string input)
+        {
+            var lines = Util.Split(input);
+
+            var rules = lines.Take(lines.Length-1).Select(x => x.Split(" => "));
+            var molecule = lines.Last();
+
+
+            var jobqueue = new Queue<Tuple<string, int>>();
+            jobqueue.Enqueue(Tuple.Create(molecule, 0));
+            var cache = new Dictionary<string, int>();
+
+            cache[molecule] = 0;
+
+            int best = int.MaxValue;
+            int shortest = int.MaxValue;
+
+            while (jobqueue.Any())
+            {
+                var entry = jobqueue.Dequeue();
+
+                shortest = Math.Min(shortest, entry.Item1.Length);
+
+                if (entry.Item1 == "e")
+                {
+                    best = Math.Min(best, entry.Item2);
+                    continue;
+                }
+
+                foreach (var rule in rules.Where(r => entry.Item1.Contains(r[1])))
+                {
+                    int newScore = entry.Item2+1;
+
+                    if (newScore > best) continue;
+
+
+                    var indices = entry.Item1.AllIndexesOf(rule[1]);
+                    foreach (var i in indices)
+                    {
+                        var newStr = entry.Item1.ReplaceAtIndex(i, rule[1], rule[0]);
+                        
+                        if (!cache.ContainsKey(newStr) || cache[newStr] > newScore)
+                        {
+                            cache[newStr] = newScore;
+                            jobqueue.Enqueue(Tuple.Create(newStr, newScore));
+                        }
+                    }
+                }
+                jobqueue = new Queue<Tuple<string, int>>(jobqueue.OrderBy(x => x.Item1.Length));
+                
+            }
+
+            return best;
+        }
+
+        public void Run(string input, System.IO.TextWriter console)
+        {
+            console.WriteLine("- Pt1 - "+Part1(input));
+            //console.WriteLine("- Pt2 - "+Part2(input));
+        }
+    }
+}
