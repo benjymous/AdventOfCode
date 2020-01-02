878af43c2d9e55214ccda3641a49c64d99bf889b
benjymous <r.munn@outlook.com>
2020-01-02 13:47:58 +0000

    Imported missing 2015 solutions

---
+++ MMXVIII/Day10_TheStarsAlign.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent.MMXVIII
+{
+    public class Day10 : IPuzzle
+    {
+        public string Name { get { return "2018-10"; } }
+
+        public class Drone
+        {
+            public ManhattanVector2 position;
+            public ManhattanVector2 velocity;
+            public Drone(string line)
+            {
+                var nums = Util.ExtractNumbers(line);
+                position = new ManhattanVector2(nums[0], nums[1]);
+                velocity = new ManhattanVector2(nums[2], nums[3]);
+            }
+
+            public void Step() => position += velocity;
+
+            public override string ToString() => $"{position} - {velocity}";
+
+        }
+
+        public static (int steps, string message) Solve(string input)
+        {
+            var drones = Util.Parse<Drone>(input);
+
+            int steps = 0;
+            while (true)
+            {
+                steps++;
+                int minx = int.MaxValue;
+                int maxx = int.MinValue;
+
+                int miny = int.MaxValue;
+                int maxy = int.MinValue;
+
+                foreach (var drone in drones)
+                {
+                    drone.Step();
+
+                    minx = Math.Min(minx, drone.position.X);
+                    maxx = Math.Max(maxx, drone.position.X);
+
+                    miny = Math.Min(miny, drone.position.Y);
+                    maxy = Math.Max(maxy, drone.position.Y);
+                }
+
+                if (maxy - miny < 10)
+                {
+                    var sb = new StringBuilder();
+                    for (var y = miny; y <= maxy; ++y)
+                    {
+                        for (var x = minx; x <= maxx; ++x)
+                        {
+                            var hit = false;
+                            foreach (var drone in drones)
+                            {
+                                if (drone.position.X == x && drone.position.Y == y)
+                                {
+                                    hit = true; break;
+                                }
+                            }
+                            sb.Append( hit ? "#" : " ");
+                        }   
+                        sb.Append("\n");                
+                    }
+                    return (steps, sb.ToString());
+                }
+
+            }
+
+        }
+
+        public static string Part1(string input)
+        {
+            return Solve(input).message;          
+        }
+
+        public static int Part2(string input)
+        {
+            return Solve(input).steps;  
+        }
+
+        public void Run(string input, System.IO.TextWriter console)
+        {
+            console.WriteLine("- Pt1 - \n" + Part1(input));
+            console.WriteLine("- Pt2 - " + Part2(input));
+        }
+    }
+}
---
+++ test/MMXVIII/Day11Test.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Advent.MMXVIII.Test
+{
+    [TestCategory("2018")]
+    [TestClass]
+    public class Day11Test
+    {
+        string input = Util.GetInput<Day11>();
+
+        [TestCategory("Test")]
+        [DataRow(8, 3, 5, 4)] // Fuel cell at 3,5, grid with serial number 8: power level 4
+        [DataRow(57, 122, 79, -5)] // Fuel cell at  122,79, grid serial number 57: power level -5
+        [DataRow(57, 122, 79, -5)] // Fuel cell at 217,196, grid serial number 39: power level  0
+        [DataRow(57, 122, 79, -5)] // Fuel cell at 101,153, grid serial number 71: power level  4
+        public void PowerLevelTest(int serial, int x, int y, int expected)
+        {
+            Assert.AreEqual(expected, MMXVIII.Day11.Power(serial, x, y));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void PowerGrid_Part1_Regression()
+        {
+            Assert.AreEqual("243,17", Day11.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void PowerGrid_Part2_Regression()
+        {
+            Assert.AreEqual("233,228,12", Day11.Part2(input));
+        }
+
+    }
+}
