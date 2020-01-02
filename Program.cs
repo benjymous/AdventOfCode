d1687bc6b896f35dd16abbeea24cfcfbffb6f92a
benjymous <r.munn@outlook.com>
2020-01-02 13:48:31 +0000

    Turned parallel running back on

--- Program.cs
+++ Program.cs
@@ -41,7 +41,7 @@ namespace Advent
                 var watch = new System.Diagnostics.Stopwatch();        
                 watch.Start();
 
-                Parallel.ForEach(puzzles, new ParallelOptions(){MaxDegreeOfParallelism=1}, (puzzle) =>
+                Parallel.ForEach(puzzles, new ParallelOptions(){MaxDegreeOfParallelism=8}, (puzzle) =>
                 {
                     TextBuffer buffer = new TextBuffer();
                     timings[puzzle.Name] = puzzle.TimeRun(buffer);
