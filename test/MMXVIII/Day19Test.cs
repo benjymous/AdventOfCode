07a8cd5b283612d7b9137fbacdfce7efbcbaeb4e
benjymous <r.munn@outlook.com>
2020-01-14 08:56:56 +0000

    fixed test

--- test/MMXVIII/Day19Test.cs
+++ test/MMXVIII/Day19Test.cs
@@ -7,7 +7,7 @@ namespace Advent.MMXVIII.Test
     [TestClass]
     public class Day19Test
     {
-        string input = Util.GetInput<Day16>();
+        string input = Util.GetInput<Day19>();
 
         [TestCategory("Regression")]
         [DataTestMethod]
