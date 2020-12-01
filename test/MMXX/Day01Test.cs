1e03fae3c6d015ebe6163a2d9f4b6fee68327635
benjymous <r.munn@outlook.com>
2020-12-01 07:20:31 +0000

    2020 Day 1

--- test/MMXX/Day01Test.cs
+++ test/MMXX/Day01Test.cs
@@ -6,20 +6,36 @@ namespace Advent.MMXX.Test
     [TestClass]
     public class Day01Test
     {
-        //string input = Util.GetInput<Day01>();
+        string input = Util.GetInput<Day01>();
 
-        //[TestCategory("Regression")]
-        //[DataTestMethod]
-        //public void _Part1_Regression()
-        //{
-        //    Assert.AreEqual(, Day01.Part1(input));
-        //}
+        [TestCategory("Test")]
+        [DataRow("1721\n979\n366\n299\n675\n1456", 514579)]
+        [DataTestMethod]
+        public void Expenses01Test(string input, int expected)
+        {
+            Assert.IsTrue(MMXX.Day01.Part1(input) == expected);
+        }
 
-        //[TestCategory("Regression")]
-        //[DataTestMethod]
-        //public void _Part2_Regression()
-        //{
-        //    Assert.AreEqual(, Day01.Part2(input));
-        //}
+        [TestCategory("Test")]
+        [DataRow("1721\n979\n366\n299\n675\n1456", 241861950)]
+        [DataTestMethod]
+        public void Expenses02Test(string input, int expected)
+        {
+            Assert.IsTrue(MMXX.Day01.Part2(input) == expected);
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Expenses_Part1_Regression()
+        {
+            Assert.AreEqual(788739, Day01.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Expenses_Part2_Regression()
+        {
+            Assert.AreEqual(178724430, Day01.Part2(input));
+        }
     }
 }
