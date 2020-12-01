4ad2cef9c8f013e82f65f42dd9471b82ed22a2af
benjymous <r.munn@outlook.com>
2020-12-01 07:20:31 +0000

    2020 Day 1

--- MMXX/Day01.cs
+++ MMXX/Day01.cs
@@ -1,28 +1,56 @@
-﻿// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-// namespace Advent.MMXX
-// {
-//     public class Day01 : IPuzzle
-//     {
-//         public string Name { get { return "2020-01";} }
- 
-//         public static int Part1(string input)
-//         {
-//             return 0;
-//         }
+namespace Advent.MMXX
+{
+    public class Day01 : IPuzzle
+    {
+        public string Name { get { return "2020-01"; } }
 
-//         public static int Part2(string input)
-//         {
-//             return 0;
-//         }
+        public static int Part1(string input)
+        {
+            var numbers = Util.Parse32(input).OrderByDescending(x => x).ToArray();
 
-//         public void Run(string input)
-//         {
-//             Console.WriteLine("- Pt1 - "+Part1(input));
-//             Console.WriteLine("- Pt2 - "+Part2(input));
-//         }
-//     }
-// }
+            for (var i=0; i<numbers.Length; ++i)
+            {
+                for (var j=numbers.Length-1; j>=0; --j)
+                {
+                    if (numbers[i]+numbers[j] == 2020)
+                    {
+                        return numbers[i] * numbers[j];
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public static int Part2(string input)
+        {
+            var numbers = Util.Parse32(input).OrderByDescending(x => x).ToArray();
+
+            for (var i = 0; i < numbers.Length; ++i)
+            {
+                for (var j = numbers.Length - 1; j >= 0; --j)
+                {
+                    for (var k = numbers.Length - 1; k >= 0; --k)
+                    {
+                        if (numbers[i] + numbers[j] + numbers[k] == 2020)
+                        {
+                            return numbers[i] * numbers[j] * numbers[k];
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public void Run(string input, ILogger logger)
+        {
+            logger.WriteLine("- Pt1 - " + Part1(input));
+            logger.WriteLine("- Pt2 - " + Part2(input));
+        }
+    }
+}
