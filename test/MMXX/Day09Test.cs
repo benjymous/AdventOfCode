b44f4a5a73ff0f11d4cabba65d580d77581d42cb
Richard Munn <richard.munn@arup.com>
2020-12-09 15:46:04 +0000

    2020 Day 9

--- test/MMXX/Day09Test.cs
+++ test/MMXX/Day09Test.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Advent.MMXX.Test
 {
@@ -6,20 +7,36 @@ namespace Advent.MMXX.Test
     [TestClass]
     public class Day09Test
     {
-        //string input = Util.GetInput<Day09>();
+        string input = Util.GetInput<Day09>();
 
-        //[TestCategory("Regression")]
-        //[DataTestMethod]
-        //public void _Part1_Regression()
-        //{
-        //    Assert.AreEqual(, Day09.Part1(input));
-        //}
+        [TestCategory("Test")]
+        [DataRow("35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576", 5, 127)]
+        [DataTestMethod]
+        public void Encoding01Test(string input, int preamble, int expected)
+        {
+            Assert.IsTrue((int)MMXX.Day09.Part1(input, preamble) == expected);
+        }
 
-        //[TestCategory("Regression")]
-        //[DataTestMethod]
-        //public void _Part2_Regression()
-        //{
-        //    Assert.AreEqual(, Day09.Part2(input));
-        //}
+        [TestCategory("Test")]
+        [DataRow("35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576", 5, 62)]
+        [DataTestMethod]
+        public void Encoding02Test(string input, int preamble, int expected)
+        {
+            Assert.IsTrue((int)MMXX.Day09.Part2(input, preamble) == expected);
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Encoding_Part1_Regression()
+        {
+            Assert.AreEqual((Int64)1124361034, Day09.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Encoding_Part2_Regression()
+        {
+            Assert.AreEqual((Int64)129444555, Day09.Part2(input));
+        }
     }
 }
