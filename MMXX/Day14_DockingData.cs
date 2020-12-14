1aab5bb98a15ea9c30cc124ee7114b03ca0cf30d
benjymous <r.munn@outlook.com>
2020-12-14 14:49:31 +0000

    2020 Day 14, final optimisations

--- MMXX/Day14_DockingData.cs
+++ MMXX/Day14_DockingData.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Advent.Utils;
 
 namespace Advent.MMXX
@@ -24,7 +26,7 @@ namespace Advent.MMXX
                 if (bits[0] == "mask")
                 {
                     type = StatementType.mask;
-                    Mask = new Mask(bits[1]);
+                    Mask = Mask(bits[1]);
                 }
                 else
                 {
@@ -37,148 +39,113 @@ namespace Advent.MMXX
             public StatementType type;
             public Int64 Address = 0;
             public Int64 Value = 0;
-            public Mask Mask;
-
+            public (Int64 Value, Int64 QuantumBits) Mask;
         }
 
-        public class Mask
+        public static (Int64 Value, Int64 QuantumBits) Mask(string input)
         {
-            public Mask(string value)
+            Int64 v = 0, q = 0;
+            int j = 0;
+            for (var i = input.Length - 1; i >= 0; --i, ++j)
             {
-                Value = value;
-            }
-            public string Value {get; private set;}
-
-            public char Bit(int i)
-            {
-                return Value[Value.Length-1-i];
-            }
-        }
-
-        public class QuantumInt
-        {
-            public QuantumInt(Int64 initialValue)
-            {                
-                Value = initialValue;
-            }
-
-            public QuantumInt(QuantumInt other)
-            {
-                Value = other.Value;
-                QuantumBits = other.QuantumBits;
-            }
-
-            public Int64 Value {get;set;} = 0;
-            public Int64 QuantumBits {get;set;} = 0;
-        }
-
-        public static Int64 ApplyMaskV1(Int64 value, Mask mask)
-        {
-            //Console.WriteLine(Convert.ToString(value, 2).PadLeft(36,'0'));
-            //Console.WriteLine(mask.Value);
-            Int64 result = 0;
-
-            Int64 apply = value;
-
-            for (var i = 0 ; i <mask.Value.Length; ++i)
-            {
-                Int64 applyBit = 0;
-                char b = mask.Bit(i);
-                switch(b)
+                switch (input[i])
                 {
-                    case '0': 
-                        applyBit = 0;
-                        break;
                     case '1':
-                        applyBit = 1;
+                        Util.SetBit(ref v, j);
                         break;
+
                     case 'X':
-                        applyBit = apply & 1; 
+                        Util.SetBit(ref q, j);
                         break;
                 }
-                result |= (applyBit << i);
+            }
+            return (v, q);
+        }
 
-                //Console.WriteLine();
-                //Console.WriteLine("  " + Convert.ToString(result, 2));
-                //Console.WriteLine("  " + Convert.ToString(apply, 2));
+        public static Int64 ApplyMaskV1(Int64 value, (Int64 Value, Int64 QuantumBits) mask)
+        {
+            Int64 result = value;
 
-                apply >>= 1;
+            for (var i = 0; i < 36; ++i)
+            {
+                var b = 1L << i;
+
+                if ((mask.QuantumBits & b) == 0)
+                {
+                    if ((mask.Value & b) == 0)
+                    {
+                        Util.ClearBit(ref result, i);
+                    }
+                    else
+                    {
+                        Util.SetBit(ref result, i);
+                    }
+                }
             }
 
             return result;
         }
 
-        public static QuantumInt ApplyMaskV2(Int64 value, Mask mask)
+        public static (Int64 Value, Int64 QuantumBits) ApplyMaskV2(Int64 value, (Int64 Value, Int64 QuantumBits) mask)
         {
-            //Console.WriteLine();
-            //Console.WriteLine("* " + Convert.ToString(value, 2).PadLeft(36,'0'));
-            //Console.WriteLine("* " + mask.Value);
+            Int64 resval = value;
+            Int64 resqua = 0;
 
-            QuantumInt result = new QuantumInt(value);
-
-            Int64 apply = value;
-
-            for (var i = 0 ; i <mask.Value.Length; ++i)
+            for (var i = 0; i < 36; ++i)
             {
-                char b = mask.Value[i];
-                switch(b)
+                var b = 1L << i;
+
+                if ((mask.Value & b) != 0)
                 {
-                    case '0': 
-                        break;
-                    case '1':
-                        result.Value = Util.SetBit(result.Value, mask.Value.Length-i-1);
-                        break;
-                    case 'X':
-                        //Console.WriteLine("X at "+(mask.Value.Length-i-1));
-                        result.QuantumBits = Util.SetBit(result.QuantumBits, mask.Value.Length-i-1);
-                        break;
+                    Util.SetBit(ref resval, i);
+                }
+                else if ((mask.QuantumBits & b) != 0)
+                {
+                    Util.SetBit(ref resqua, i);
+                }
+      
+            }
+
+            return (resval, resqua);
+        }
+
+        class MemoryContainer<T>
+        {
+            Queue<T> items = new Queue<T>();
+            HashSet<T> seen = new HashSet<T>();
+            public void Push(T v)
+            {
+                if (!seen.Contains(v))
+                {
+                    items.Enqueue(v);
+                    seen.Add(v);
                 }
             }
 
-            //Console.WriteLine("=== "+result.Value + " / "+result.QuantumBits);
-
-            return result;
+            public T Take() => items.Dequeue();
+            public bool Any => items.Count > 0;
         }
 
-        public static IEnumerable<Int64> Combinations(QuantumInt input)
+        public static IEnumerable<Int64> Combinations((Int64 Value, Int64 QuantumBits) input)
         {
-            var inputs = new Queue<QuantumInt>();
-            inputs.Enqueue(input);
-            var seen = new HashSet<Tuple<Int64, Int64>>();
+            var inputs = new MemoryContainer<(Int64 Value, Int64 QuantumBits)>();
+            inputs.Push(input);
 
-            while (inputs.Count > 0)
+            while (inputs.Any)
             {
-                var next = inputs.Dequeue();
-
-                var hash = Tuple.Create(next.Value, next.QuantumBits);
-                if (seen.Contains(hash)) continue;
-                seen.Add(hash);
-
-                //Console.WriteLine(" ?? "+next.Value+" / "+next.QuantumBits);
+                var next = inputs.Take();
 
                 if (next.QuantumBits == 0) 
                 {
-                    //Console.WriteLine(" >> "+next.Value);
                     yield return next.Value;
                 }
                 else
                 {                    
                     foreach (var b in next.QuantumBits.BitSequence())
                     {
-                        var qu0 = new QuantumInt(next);
-                        var qu1 = new QuantumInt(next);
-
-                        qu0.QuantumBits &= ~(b);
-                        qu1.QuantumBits &= ~(b);
-
-                        qu0.Value &= ~(b);
-                        qu1.Value |= (b);
-
-                        //Console.WriteLine(" << "+qu0.Value+" / "+qu0.QuantumBits);
-                        //Console.WriteLine(" << "+qu0.Value+" / "+qu0.QuantumBits);
-
-                        inputs.Enqueue(qu0);
-                        inputs.Enqueue(qu1);
+                        Int64 newq = next.QuantumBits & ~b;
+                        inputs.Push((next.Value & ~(b), newq));
+                        inputs.Push((next.Value | b, newq));
                     }
                 }
             }
@@ -188,18 +155,16 @@ namespace Advent.MMXX
         {
             var statements = Util.Parse<Statement>(input);
 
-            Mask mask = null;
+            (Int64 Value, Int64 QuantumBits) mask = (0,0);
             var memory = new Dictionary<Int64, Int64>();
             foreach (var statement in statements)
             {
                 if (statement.type == StatementType.mask)
                 {
-                    //Console.WriteLine("MASK:" + statement.Mask.Value );
                     mask = statement.Mask;
                 }
                 else
                 {
-                    //Console.WriteLine("MEM "+statement.Address+" = "+statement.Value);
                     memory[statement.Address] = ApplyMaskV1(statement.Value, mask);
                 }
             }
@@ -211,26 +176,20 @@ namespace Advent.MMXX
         {
             var statements = Util.Parse<Statement>(input);
 
-            Mask mask = null;
+            (Int64 Value, Int64 QuantumBits) mask = (0,0);
             var memory = new Dictionary<Int64, Int64>();
             foreach (var statement in statements)
             {
                 if (statement.type == StatementType.mask)
                 {
-                    //Console.WriteLine("MASK:" + statement.Mask.Value );
                     mask = statement.Mask;
                 }
                 else
                 {
-                    //Console.WriteLine("Masking "+statement.Address + " with "+mask.Value);
-                    var addressMask = ApplyMaskV2(statement.Address, mask);
-                    //Console.WriteLine(" - "+addressMask.Value + " / " + addressMask.QuantumBits);
-                    
+                    var addressMask = ApplyMaskV2(statement.Address, mask);           
                     var addresses = Combinations(addressMask);
-                    //Console.WriteLine("Combinations: "+addresses.Count());
                     foreach (var addr in addresses)
                     {
-                        //Console.WriteLine("memory["+addr+"] = "+statement.Value);
                         memory[addr] = statement.Value;
                     }
                 }
@@ -241,26 +200,7 @@ namespace Advent.MMXX
 
         public void Run(string input, ILogger logger)
         {
-            //Console.WriteLine(ApplyMask(11, new Mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")));  // 73
-            //Console.WriteLine(ApplyMask(101, new Mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")));  // 101
-            //Console.WriteLine(ApplyMask(0, new Mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")));  // 64
-
-            //var q = ApplyMaskV2(42, new Mask("000000000000000000000000000000X1001X"));
-            //var c = Combinations(q);
-            //Console.WriteLine(String.Join("\n", c));
-            //Console.WriteLine(c.Count());
-
-            // Console.WriteLine(Part2("mask = 000000000000000000000000000000X1001X\n"+
-            //     "mem[42] = 100\n"+
-            //     "mask = 00000000000000000000000000000000X0XX\n"+
-            //     "mem[26] = 1"));
-
-            // var q = new QuantumInt(0);
-            // q.SetBit(0);
-            // q.SetBit(2);
-            // Console.WriteLine( Convert.ToString(q.Value, 2).PadLeft(36,'0'));
-
-            //logger.WriteLine("- Pt1 - "+Part1(input));
+            logger.WriteLine("- Pt1 - "+Part1(input));
             logger.WriteLine("- Pt2 - "+Part2(input));
         }
     }
