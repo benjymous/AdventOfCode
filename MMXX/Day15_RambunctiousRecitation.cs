8c23cee3a6db4cf47d3e57fff03276482a32b51f
benjymous <r.munn@outlook.com>
2020-12-15 13:56:31 +0000

    2020, Day 15

---
+++ MMXX/Day15_RambunctiousRecitation.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent.MMXX
+{
+    public class Day15 : IPuzzle
+    {
+        public string Name { get { return "2020-15"; } }
+
+        class Entry
+        {
+            public Entry(int v)
+            {
+                Set(v);
+            }
+            public void Set(int v)
+            {
+                v1 = v2;
+                v2 = v;
+            }
+
+            public bool IsFirst => v1 == -1;
+            public int Get => v1;
+
+            int v1 = -1;
+            int v2 = -1;
+        }
+
+        class Storage
+        {
+            public Storage(string input)
+            {
+                var numbers = Util.Parse32(input);
+                foreach (var n in numbers)
+                {
+                    LastNum = n;
+                    LastEntry = values[LastNum] = new Entry(++Count);
+                }
+            }
+
+            public int Next() => AddNumber(LastEntry.IsFirst ? 0 : Count - LastEntry.Get);
+
+            int AddNumber(int val)
+            {
+                LastNum = val;
+                if (values.TryGetValue(LastNum, out Entry entry))
+                {
+                    LastEntry = entry;
+                    entry.Set(++Count);
+                }
+                else
+                {
+                    LastEntry = new Entry(++Count);
+                    values[val] = LastEntry;
+                }
+                return val;
+            }
+
+            public int Count { get; private set; } = 0;
+            public int LastNum { get; private set; } = 0;
+ 
+            Dictionary<int, Entry> values = new Dictionary<int, Entry>();
+            Entry LastEntry = null;
+        }
+
+
+        public static int Solve(string input, int count)
+        {
+            var data = new Storage(input);
+
+            while (data.Count < count)
+            {
+                data.Next();
+            }
+            return data.LastNum;
+        }
+
+        public static int Part1(string input)
+        {
+            return Solve(input, 2020);
+        }
+
+        public static int Part2(string input)
+        {
+            return Solve(input, 30000000);
+        }
+
+        public void Run(string input, ILogger logger)
+        {
+            logger.WriteLine("- Pt1 - " + Part1(input));
+            logger.WriteLine("- Pt2 - " + Part2(input));
+        }
+    }
+}
--- test/MMXX/Day15Test.cs
+++ test/MMXX/Day15Test.cs
@@ -6,20 +6,48 @@ namespace Advent.MMXX.Test
     [TestClass]
     public class Day15Test
     {
-        //string input = Util.GetInput<Day15>();
+        string input = Util.GetInput<Day15>();
 
-        //[TestCategory("Regression")]
-        //[DataTestMethod]
-        //public void _Part1_Regression()
-        //{
-        //    Assert.AreEqual(, Day15.Part1(input));
-        //}
+        [TestCategory("Test")]
+        [DataRow("0,3,6", 436)]
+        [DataRow("1,3,2", 1)]
+        [DataRow("2,1,3", 10)]
+        [DataRow("1,2,3", 27)]
+        [DataRow("2,3,1", 78)]
+        [DataRow("3,2,1", 438)]
+        [DataRow("3,1,2", 1836)]
+        [DataTestMethod]
+        public void Sequence1Test(string input, int expected)
+        {
+            Assert.AreEqual(expected, MMXX.Day15.Part1(input));
+        }
 
-        //[TestCategory("Regression")]
-        //[DataTestMethod]
-        //public void _Part2_Regression()
-        //{
-        //    Assert.AreEqual(, Day15.Part2(input));
-        //}
+        [TestCategory("Test")]
+        [DataRow("0,3,6", 175594)]
+        [DataRow("1,3,2", 2578)]
+        [DataRow("2,1,3", 3544142)]
+        [DataRow("1,2,3", 261214)]
+        [DataRow("2,3,1", 6895259)]
+        [DataRow("3,2,1", 18)]
+        [DataRow("3,1,2", 362)]
+        [DataTestMethod]
+        public void Sequence2Test(string input, int expected)
+        {
+            Assert.AreEqual(expected, MMXX.Day15.Part2(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Sequence_Part1_Regression()
+        {
+            Assert.AreEqual(758, Day15.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Sequence_Part2_Regression()
+        {
+            Assert.AreEqual(814, Day15.Part2(input));
+        }
     }
 }
