15acc6d10ee6a4f53db90aa195f9fff84008ce96
benjymous <r.munn@outlook.com>
2020-12-23 14:44:37 +0000

    2020 Day 23

---
+++ MMXX/Day23_CrabCups.cs
@@ -0,0 +1,78 @@
+﻿using Advent.Utils;
+using Advent.Utils.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent.MMXX
+{
+    public class Day23 : IPuzzle
+    {
+        public string Name { get { return "2020-23"; } }
+
+        static Circle<int> CreateCircle(IEnumerable<int> vals, bool part2)
+        {
+            var start = new Circle<int>(vals.First());
+            var node = start.InsertRange(vals.Skip(1));
+
+            if (part2)
+            {
+                node.InsertRange(Enumerable.Range(10, 1000000-start.Count()));
+            }
+
+            return start;
+        }
+
+        static IEnumerable<Circle<int>> Take(Circle<int> circle) 
+            => new Circle<int>[] { circle.PopNext(), circle.PopNext(), circle.PopNext() };
+   
+
+        static Circle<int> FindDestination(Circle<int> current,IEnumerable<Circle<int>> vals, bool part2)
+        {
+            var valvals = vals.Select(v => v.Value);
+            int max = part2 ? 1000000 : 9;
+
+            var destinationVal = current.Value;
+            do
+            {
+                destinationVal = destinationVal == 1 ? max : destinationVal - 1;
+            } 
+            while (valvals.Contains(destinationVal));
+
+            return current.Find(destinationVal);
+        }
+
+        public static IEnumerable<int> Play(string input, int rounds, bool part2=false)
+        {
+            var current = CreateCircle(input.Trim().ToArray().Select(ch => ch - '0'), part2);
+            
+            while (rounds-- > 0)
+            {
+                var taken = Take(current);
+                
+                FindDestination(current, taken, part2).InsertRange(taken);
+
+                current = current.Next();
+            }
+
+            return current.Find(1).Values().Skip(1);
+        }
+
+        public static string Part1(string input, int rounds = 100)
+        {
+            return string.Join("", Play(input, rounds).Select(i => i.ToString()));
+        }
+
+        public static Int64 Part2(string input)
+        {
+            return Play(input, 10000000, true).Take(2).Product();
+        }
+
+        public void Run(string input, ILogger logger)
+        {
+            logger.WriteLine("- Pt1 - " + Part1(input));
+            logger.WriteLine("- Pt2 - " + Part2(input));
+        }
+    }
+}
