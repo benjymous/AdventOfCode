c135c0af850e9ed2885842eff9d847ee9426cfaf
benjymous <r.munn@outlook.com>
2020-12-24 14:52:36 +0000

    2020 Day 24 Part 2

--- test/MMXX/Day24Test.cs
+++ test/MMXX/Day24Test.cs
@@ -6,20 +6,55 @@ namespace Advent.MMXX.Test
     [TestClass]
     public class Day24Test
     {
-        //string input = Util.GetInput<Day24>();
+        string input = Util.GetInput<Day24>();
 
-        //[TestCategory("Regression")]
-        //[DataTestMethod]
-        //public void _Part1_Regression()
-        //{
-        //    Assert.AreEqual(, Day24.Part1(input));
-        //}
+        string test = "sesenwnenenewseeswwswswwnenewsewsw\n" +
+            "neeenesenwnwwswnenewnwwsewnenwseswesw\n" +
+            "seswneswswsenwwnwse\n" +
+            "nwnwneseeswswnenewneswwnewseswneseene\n" +
+            "swweswneswnenwsewnwneneseenw\n" +
+            "eesenwseswswnenwswnwnwsewwnwsene\n" +
+            "sewnenenenesenwsewnenwwwse\n" +
+            "wenwwweseeeweswwwnwwe\n" +
+            "wsweesenenewnwwnwsenewsenwwsesesenwne\n" +
+            "neeswseenwwswnwswswnw\n" +
+            "nenwswwsewswnenenewsenwsenwnesesenew\n" +
+            "enewnwewneswsewnwswenweswnenwsenwsw\n" +
+            "sweneswneswneneenwnewenewwneswswnese\n" +
+            "swwesenesewenwneswnwwneseswwne\n" +
+            "enesenwswwswneneswsenwnewswseenwsese\n" +
+            "wnwnesenesenenwwnenwsewesewsesesew\n" +
+            "nenewswnwewswnenesenwnesewesw\n" +
+            "eneswnwswnwsenenwnwnwwseeswneewsenese\n" +
+            "neswnwewnwnwseenwseesewsenwsweewe\n" +
+            "wseweeenwnesenwwwswnew";
 
-        //[TestCategory("Regression")]
-        //[DataTestMethod]
-        //public void _Part2_Regression()
-        //{
-        //    Assert.AreEqual(, Day24.Part2(input));
-        //}
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Lobby1Test()
+        {
+            Assert.AreEqual(10, Day24.Part1(test));
+        }
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Lobby2Test()
+        {
+            Assert.AreEqual(2208, Day24.Part2(test));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Lobby_Part1_Regression()
+        {
+            Assert.AreEqual(307, Day24.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Lobby_Part2_Regression()
+        {
+            Assert.AreEqual(3787, Day24.Part2(input));
+        }
     }
 }
