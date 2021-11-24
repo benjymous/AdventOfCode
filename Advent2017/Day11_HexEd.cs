f6e8178ddb318c1694b4d41b88987d6917b56018
benjymous <r.munn@outlook.com>
2021-11-24 21:29:21 +0000

    Refactored hex code into class and added another solution

---
+++ Advent2017/Day11_HexEd.cs
@@ -0,0 +1,53 @@
+﻿using Advent.Utils.Vectors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC.Advent2017
+{
+    public class Day11 : IPuzzle
+    {
+        public string Name { get { return "2017-11"; } }
+
+        public static int Part1(string input)
+        {
+            var steps = input.Trim().Split(",");
+
+            var pos = new HexVector(0, 0, 0, false);
+
+            foreach (var step in steps)
+            {
+                pos.TranslateHex(step);
+            }
+
+            return pos.Distance(new HexVector(0,0,0,false));
+        }
+
+        public static int Part2(string input)
+        {
+            var origin = new HexVector(0, 0, 0, false);
+            var steps = input.Trim().Split(",");
+
+            var pos = new HexVector(0, 0, 0, false);
+
+            int furthest = 0;
+
+            foreach (var step in steps)
+            {
+                pos.TranslateHex(step);
+
+                furthest = Math.Max(furthest, pos.Distance(origin));
+            }
+
+            return furthest;
+
+        }
+
+        public void Run(string input, ILogger logger)
+        {
+            logger.WriteLine("- Pt1 - " + Part1(input));
+            logger.WriteLine("- Pt2 - " + Part2(input));
+        }
+    }
+}
--- Advent2020/Day24_LobbyLayout.cs
+++ Advent2020/Day24_LobbyLayout.cs
@@ -3,28 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using AoC.Utils;
 using System.Text;
+using Advent.Utils.Vectors;
 
 namespace AoC.Advent2020
 {
     public class Day24 : IPuzzle
     {
         public string Name { get { return "2020-24";} }
-
-        static Dictionary<string, (int x, int y, int z)> directions = new Dictionary<string, (int x, int y, int z)>()
-        {
-            { "ne", (+1,-1, 0) },
-            { "e",  (+1, 0, -1) },
-            { "se", (0, +1, -1) },
-            { "sw", (-1, +1, 0) },
-            { "w",  (-1, 0, +1) },
-            { "nw", (0, -1, +1) }
-        };
-
-        static (int x, int y, int z) Translate((int x, int y, int z) pos, (int x, int y, int z) dir) =>
-            (pos.x + dir.x, pos.y + dir.y, pos.z + dir.z);
-
-        static (int x, int y, int z) TranslateHex((int x, int y, int z) pos, string dir)
-            => Translate(pos, directions[dir]);
 
         static IEnumerable<string> SplitCommands(string line)
         {
@@ -42,14 +27,14 @@ namespace AoC.Advent2020
             }
         }
 
-        static (int x, int y, int z) FollowPath(string path)
+        static HexVector FollowPath(string path)
         {
-            (int x, int y, int z) pos = (0,0,0);
+            HexVector pos = new HexVector(0,0,0);
 
             var steps = SplitCommands(path);
             foreach (var step in steps)
             {
-                pos = TranslateHex(pos, step);
+                pos.TranslateHex(step);
             }
             return pos;
         }
@@ -60,7 +45,7 @@ namespace AoC.Advent2020
             {
             }
 
-            public State(HashSet<(int x, int y, int z)> initialState)
+            public State(HashSet<HexVector> initialState)
             {
 
                 foreach (var v in initialState) Set(v, true);
@@ -75,39 +60,39 @@ namespace AoC.Advent2020
 
             Accumulator XRange = new Accumulator();
             Accumulator YRange = new Accumulator();
-            public HashSet<(int x, int y, int z)> Cells { get; private set; } = new HashSet<(int x, int y, int z)>();
+            public HashSet<HexVector> Cells { get; private set; } = new HashSet<HexVector>();
 
-            public IEnumerable<(int x, int y, int z)> Range()
+            public IEnumerable<HexVector> Range()
             {
                 foreach (var y in YRange.RangeBuffered(1))
                 {
                     foreach (var x in XRange.RangeBuffered(1))
                     {
-                        yield return ((int)x, (int)y, (int)(-x-y));
+                        yield return new HexVector((int)x, (int)y, (int)(-x-y));
                     }
                 }
             }
 
-            public void Set((int x, int y, int z) pos, bool v)
+            public void Set(HexVector pos, bool v)
             {
                 if (v)
                 {
-                    XRange.Add(pos.x);
-                    YRange.Add(pos.y);
+                    XRange.Add(pos.X);
+                    YRange.Add(pos.Y);
                     Cells.Add(pos);
                 }
             }
 
-            public bool Get((int x, int y, int z) pos)
+            public bool Get(HexVector pos)
                 => Cells.Contains(pos);
 
-            public bool CheckDirection((int x, int y, int z) pos, (int x, int y, int z) dir) =>
-                Get((pos.x + dir.x, pos.y + dir.y, pos.z + dir.z));
+            public bool CheckDirection(HexVector pos, (int x, int y, int z) dir) =>
+                Get(new HexVector(pos.X + dir.x, pos.Y + dir.y, pos.Z + dir.z));
 
-            public int Neighbours((int x, int y, int z) pos) =>
-                directions.Values.Where(d => CheckDirection(pos, d)).Count();
+            public int Neighbours(HexVector pos) =>
+                pos.Directions.Values.Where(d => CheckDirection(pos, d)).Count();
 
-            public void Tick(State oldState, (int x, int y, int z) pos)
+            public void Tick(State oldState, HexVector pos)
             {
                 int neighbours = oldState.Neighbours(pos);
                 var cellstate = oldState.Get(pos);
@@ -137,7 +122,7 @@ namespace AoC.Advent2020
             }
         }
 
-        public static int Run(HashSet<(int x, int y, int z)> initialState, int cycles)
+        public static int Run(HashSet<HexVector> initialState, int cycles)
         {
             State[] states = new State[2];
             states[0] = new State(initialState);
@@ -157,17 +142,17 @@ namespace AoC.Advent2020
             return states[current].Cells.Count();
         }
 
-        static HashSet<(int x, int y, int z)> GetInitialState(string input)
+        static HashSet<HexVector> GetInitialState(string input)
         {
             var data = input.Trim().Split("\n");
 
-            var counts = new Dictionary<(int x, int y, int z), int>();
+            var counts = new Dictionary<HexVector, int>();
 
             foreach (var line in data)
             {
                 counts.IncrementAtIndex(FollowPath(line));
             }
-            return new HashSet<(int x, int y, int z)>(counts.Where(kvp => (kvp.Value % 2 == 1)).Select(kvp => kvp.Key));
+            return new HashSet<HexVector>(counts.Where(kvp => (kvp.Value % 2 == 1)).Select(kvp => kvp.Key));
         }
 
 
