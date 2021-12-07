6ebbb2cfdc0e5d6cad5b6eb8809fbe1c5810f35e
benjymous <r.munn@outlook.com>
2021-12-07 15:48:19 +0000

    2021 Day 7

---
+++ Advent2021/Day07_TreacheryOfWhales.cs
@@ -0,0 +1,42 @@
+﻿using AoC.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC.Advent2021
+{
+    public class Day07 : IPuzzle
+    {
+        public string Name { get { return "2021-07"; } }
+
+        public static int Solve(string input, Func<int, int> FuelCost)
+        {
+            var positions = Util.Parse32(input).OrderBy(x => x).ToArray();
+
+            return Enumerable.Range(positions.First(), positions.Last())
+                             .Select(x => positions.Select(crab => FuelCost(Math.Abs(crab - x))).Sum())
+                             .Min();
+        }
+
+
+        public static int Part1(string input)
+        {
+            return Solve(input, x => x);
+        }
+
+        public static int Part2(string input)
+        {
+            return Solve(input, x => x*(x+1)/2);
+        }
+
+        public void Run(string input, ILogger logger)
+        {
+            //var test = new Advent2019.NPSA.InteractiveTerminal(input);
+            //test.Run();
+
+            logger.WriteLine("- Pt1 - " + Part1(input));
+            logger.WriteLine("- Pt2 - " + Part2(input));
+        }
+    }
+}
---
+++ test/Advent2021/Day07Test.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC.Advent2021.Test
+{
+    [TestCategory("2021")]
+    [TestClass]
+    public class Day07Test
+    {
+        string input = Util.GetInput<Day07>();
+
+        string test = "16,1,2,0,4,2,7,1,2,14";
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Crabs01Test()
+        {
+            Assert.AreEqual(37, Advent2021.Day07.Part1(test));
+        }
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Crabs02Test()
+        {
+            Assert.AreEqual(168, Advent2021.Day07.Part2(test));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Crabs_Part1_Regression()
+        {
+            Assert.AreEqual(355150, Day07.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Crabs_Part2_Regression()
+        {
+            Assert.AreEqual(98368490, Day07.Part2(input));
+        }
+    }
+}
