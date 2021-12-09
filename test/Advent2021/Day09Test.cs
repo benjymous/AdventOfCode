165944687cf7dd32bab220aaccac30622ca11e9d
benjymous <r.munn@outlook.com>
2021-12-09 20:50:35 +0000

    Day 9

---
+++ test/Advent2021/Day09Test.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC.Advent2021.Test
+{
+    [TestCategory("2021")]
+    [TestCategory("FloodFill")]
+    [TestClass]
+    public class Day09Test
+    {
+        string input = Util.GetInput<Day09>();
+
+        string test = "2199943210\n3987894921\n9856789892\n8767896789\n9899965678";
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Basins01Test()
+        {
+            Assert.AreEqual(15, Day09.Part1(test.Replace("\r","")));
+        }
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Basins02Test()
+        {
+            Assert.AreEqual(1134, Day09.Part2(test.Replace("\r", "")));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Basins_Part1_Regression()
+        {
+            Assert.AreEqual(423, Day09.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Basins_Part2_Regression()
+        {
+            Assert.AreEqual(1198704, Day09.Part2(input));
+        }
+    }
+}
