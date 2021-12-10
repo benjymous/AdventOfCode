124ca7dda9ca12f6102b7aa5ac842369db8c7064
benjymous <r.munn@outlook.com>
2021-12-10 17:16:39 +0000

    Day 10

---
+++ test/Advent2021/Day10Test.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC.Advent2021.Test
+{
+    [TestCategory("2021")]
+    [TestCategory("FloodFill")]
+    [TestClass]
+    public class Day10Test
+    {
+        string input = Util.GetInput<Day10>();
+
+        string test = @"[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Syntax01Test()
+        {
+            Assert.AreEqual(26397, Day10.Part1(test.Replace("\r","")));
+        }
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Syntax02Test()
+        {
+            Assert.AreEqual(288957, Day10.Part2(test.Replace("\r", "")));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Syntax_Part1_Regression()
+        {
+            Assert.AreEqual(316851, Day10.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Syntax_Part2_Regression()
+        {
+            Assert.AreEqual(2182912364, Day10.Part2(input));
+        }
+    }
+}
