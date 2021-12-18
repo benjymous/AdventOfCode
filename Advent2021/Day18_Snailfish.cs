e62d5e6b78ecb105cf1accadd8c6b4c874586095
benjymous <r.munn@outlook.com>
2021-12-18 19:42:24 +0000

    Day 18 - unoptimised

---
+++ Advent2021/Day18_Snailfish.cs
@@ -0,0 +1,259 @@
+﻿using AoC.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC.Advent2021
+{
+    public class Day18 : IPuzzle
+    {
+        public string Name => "2021-18";
+
+        public class Val
+        {
+            public bool isPair = false;
+            public int Value = 0;
+            public Val first = null;
+            public Val second = null;
+
+            public Val parent = null;
+            public Val left = null;
+            public Val right = null;
+
+            public int Depth
+            {
+                get
+                {
+                    if (parent == null) return 0;
+                    return parent.Depth + 1;
+                }
+            }
+
+            public void Reduce()
+            {
+                while (true)
+                {
+                    //Console.WriteLine($".. {this}");
+                    bool exploded = TryExplode();
+                    if (exploded) continue;
+
+                    bool split = TrySplit();
+                    if (split) continue;
+
+                    return;
+                }
+            }
+
+            public IEnumerable<Val> Flatten()
+            {
+                if (isPair)
+                {
+                    foreach (var val in first.Flatten()) yield return val;
+                    foreach (var val in second.Flatten()) yield return val;
+                }
+                else
+                {
+                    yield return this;
+                }
+            }
+
+            public bool TryExplode()
+            {
+                if (parent == null)
+                {
+                    var vals = Flatten();
+                    vals.First().left = null;
+                    vals.Last().right = null;
+                    foreach (var pair in vals.Windows(2))
+                    {
+                        var v1 = pair.First();
+                        var v2 = pair.Last();
+
+                        v2.left = v1;
+                        v1.right = v2;
+                    }
+                }
+
+                if (Explode())
+                {
+                    return true;
+                }
+
+                if (isPair)
+                {
+                    if (first.TryExplode()) return true;
+                    if (second.TryExplode()) return true;
+                }
+                return false;
+            }
+
+            public bool TrySplit()
+            {
+                if (Split())
+                {
+                    return true;
+                }
+
+                if (isPair)
+                {
+                    if (first.TrySplit()) return true;
+                    if (second.TrySplit()) return true;
+                }
+                return false;
+            }
+
+            public bool Split()
+            {
+                if (isPair) return false;
+                if (Value < 10) return false;
+
+                //Console.WriteLine($"Splitting {this}");
+
+                int v1 = Value / 2;
+                int v2 = v1;
+                if (Value % 2 == 1) v2++;
+
+                isPair = true;
+                first = new Val { Value = v1, parent = this };
+                second = new Val { Value = v2, parent = this };
+
+                return true;
+            }
+
+            public bool Explode()
+            {
+                if (!isPair) return false;
+
+                if (first.isPair && first.Explode()) return true;
+                if (second.isPair && second.Explode()) return true;
+
+                if (Depth != 4) return false;
+
+                var left = first.left;
+                if (left!=null)
+                {
+                    left.Value += first.Value;
+                }
+
+                var right = second.right;
+                if (right!=null)
+                {
+                    right.Value += second.Value;
+                }
+
+                isPair = false;
+                Value = 0;
+
+                return true;
+            }
+
+            public long Magnitude => isPair ? 3 * first.Magnitude + 2 * second.Magnitude : Value;
+
+
+            public override string ToString()
+            {
+                if (isPair)
+                {
+                    return $"[{first},{second}]";
+                }
+                else
+                {
+                    return Value.ToString();
+                }
+            }
+        }
+
+        public static Val Add(Val lhs, Val rhs)
+        {
+            var v = new Val
+            {
+                isPair = true,
+                first = lhs,
+                second = rhs,
+            };
+            lhs.parent = v;
+            rhs.parent = v;
+
+            v.Reduce();
+
+            return v;
+        }
+
+        public static Val Parse(string data) => Parse(data.ToQueue());
+
+        static Val Parse(Queue<char> data, Val parent = null)
+        {
+            var v = new Val { parent = parent };
+            while (true)
+            {
+                var ch = data.Dequeue();
+                switch (ch)
+                {
+                    case '[':
+                        v.isPair = true;
+                        v.first = Parse(data, v);
+                        break;
+                    case ',':
+                        v.second = Parse(data, v);
+                        break;
+                    case ']':
+                        return v;
+                    case >= '0' and <= '9':
+                        {
+                            v.Value = ch - '0';
+                            return v;
+                        }
+                }
+            }
+        }
+
+        public static Val AddList(IEnumerable<string> values)
+        {
+            var first = values.First();
+            var number = Parse(first);
+
+            foreach (var next in values.Skip(1))
+            {
+                var v = Parse(next);
+                number = Add(number, v);
+            }
+
+            return number;
+        }
+
+        public static long Part1(string input)
+        {
+            var numbers = Util.Split(input, '\n');
+
+            return AddList(numbers).Magnitude;
+        }
+
+        public static long Part2(string input)
+        {
+            var numbers = Util.Split(input, '\n');
+
+            long max = 0;
+
+            foreach (var first in numbers)
+            {
+                foreach (var second in numbers)
+                {
+                    if (first != second)
+                    {
+                        var sum = AddList(new string[] { first, second });
+                        var mag = sum.Magnitude;
+                        max = Math.Max(mag, max);
+                    }
+                }
+            }
+            return max;
+        }
+
+        public void Run(string input, ILogger logger)
+        {
+            logger.WriteLine("- Pt1 - " + Part1(input));
+            logger.WriteLine("- Pt2 - " + Part2(input));
+        }
+    }
+}
---
+++ test/Advent2021/Day18Test.cs
@@ -0,0 +1,163 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using static AoC.Advent2021.Day18;
+
+namespace AoC.Advent2021.Test
+{
+    [TestCategory("2021")]
+    [TestClass]
+    public class Day18Test
+    {
+        string input = Util.GetInput<Day18>();
+
+        [TestCategory("Test")]
+
+
+        [DataRow("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]")]
+        [DataRow("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]")]
+        [DataRow("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]")]
+        [DataRow("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")]
+        [DataRow("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")]
+        [DataTestMethod]
+        public void TestSingleExplode(string data, string expected)
+        {
+            var number = Parse(data);
+            Assert.AreEqual(data, number.ToString());
+            number.TryExplode();
+            var result = number.ToString();
+            Assert.AreEqual(expected, result);
+        }
+        [DataRow(9, "9")]
+        [DataRow(10, "[5,5]")]
+        [DataRow(11, "[5,6]")]
+        [DataRow(12, "[6,6]")]
+        [DataTestMethod]
+        public void TestSingleSplit(int val, string expected)
+        {
+            var number = new Val { Value = val };
+            number.TrySplit();
+            var result = number.ToString();
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataRow("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")]
+        [DataTestMethod]
+        public void TestReduce(string data, string expected)
+        {
+            var number = Parse(data);
+            Assert.AreEqual(data, number.ToString());
+            number.Reduce();
+            var result = number.ToString();
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataRow(new string[] { "[[[[4,3],4],4],[7,[[8,4],9]]]", "[1,1]" }, "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")]
+        [DataRow(new string[] { "[1,1]", "[2,2]", "[3,3]", "[4,4]" }, "[[[[1,1],[2,2]],[3,3]],[4,4]]")]
+        [DataRow(new string[] { "[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]" }, "[[[[3,0],[5,3]],[4,4]],[5,5]]")]
+        [DataRow(new string[] { "[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]", "[6,6]" }, "[[[[5,0],[7,4]],[5,5]],[6,6]]")]
+
+        [DataRow(new string[]{"[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]"
+        , "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
+        }, "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]")]
+
+        [DataRow(new string[]{"[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"
+        , "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]"
+        }, "[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]")]
+
+        [DataRow(new string[]{"[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]"
+        , "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]"
+        }, "[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]")]
+
+        [DataRow(new string[]{"[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]"
+        , "[7,[5,[[3,8],[1,4]]]]"
+        }, "[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]")]
+
+        [DataRow(new string[]{"[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]"
+        , "[[2,[2,2]],[8,[8,1]]]"
+        }, "[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]")]
+
+        [DataRow(new string[]{"[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]"
+        , "[2,9]"
+        }, "[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]")]
+
+        [DataRow(new string[]{"[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]"
+        , "[1,[[[9,3],9],[[9,0],[0,7]]]]"
+        }, "[[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]")]
+
+        [DataRow(new string[]{"[[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]"
+        , "[[[5,[7,4]],7],1]"
+        }, "[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]")]
+
+        [DataRow(new string[]{"[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]"
+        , "[[[[4,2],2],6],[8,7]]"
+        }, "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")]
+
+
+        [DataRow(new string[]{"[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+                            "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+                            "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+                            "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+                            "[7,[5,[[3,8],[1,4]]]]",
+                            "[[2,[2,2]],[8,[8,1]]]",
+                            "[2,9]",
+                            "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+                            "[[[5,[7,4]],7],1]",
+                            "[[[[4,2],2],6],[8,7]]"},
+            "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")]
+        [DataTestMethod]
+        public void TestAdd(string[] values, string expected)
+        {
+            var first = values.First();
+            var number = Parse(first);
+            Assert.AreEqual(first, number.ToString());       
+
+            foreach (var next in values.Skip(1))
+            {
+                Console.WriteLine($"  {number}");
+                var v = Parse(next);
+                Console.WriteLine($"+ {v}");
+                Assert.AreEqual(next, v.ToString());
+
+                number = Add(number, v);
+
+                Console.WriteLine($"= {number}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"? {expected}");
+
+            var result = number.ToString();
+            Assert.AreEqual(expected, result);
+        }
+
+    
+        [DataRow("[[1,2],[[3,4],5]]", 143)]
+        [DataRow("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384)]
+        [DataRow("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445)]
+        [DataRow("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791)]
+        [DataRow("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137)]
+        [DataRow("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488)]
+        [DataTestMethod]
+        public void TestMagnitude(string data, int expected)
+        {
+            var number = Parse(data);
+            var result = number.Magnitude;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Magnitude_Part1_Regression()
+        {
+            Assert.AreEqual(4072, Day18.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Magnitude_Part2_Regression()
+        {
+            Assert.AreEqual(4483, Day18.Part2(input));
+        }
+    }
+}
