06c1d6361c116ded8a7538f83ec2d88ae46821bd
benjymous <r.munn@outlook.com>
2022-01-11 21:18:31 +0000

    2017 Day 21 working

--- test/Advent2017/Day21Test.cs
+++ test/Advent2017/Day21Test.cs
@@ -1,25 +1,25 @@
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace AoC.Advent2017.Test
-//{
-//    [TestCategory("2017")]
-//    [TestClass]
-//    public class Day21Test
-//    {
-//        string input = Util.GetInput<Day21>();
+namespace AoC.Advent2017.Test
+{
+    [TestCategory("2017")]
+    [TestClass]
+    public class Day21Test
+    {
+        string input = Util.GetInput<Day21>();
 
-//        [TestCategory("Regression")]
-//        [DataTestMethod]
-//        public void Part1_Regression()
-//        {
-//            Assert.AreEqual(0, Day21.Part1(input));
-//        }
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Part1_Regression()
+        {
+            Assert.AreEqual(173, Day21.Part1(input));
+        }
 
-//        [TestCategory("Regression")]
-//        [DataTestMethod]
-//        public void Part2_Regression()
-//        {
-//            Assert.AreEqual(0, Day21.Part2(input));
-//        }
-//    }
-//}
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Part2_Regression()
+        {
+            Assert.AreEqual(2456178, Day21.Part2(input));
+        }
+    }
+}
