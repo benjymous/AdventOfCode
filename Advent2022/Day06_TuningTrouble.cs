ee852f2bc7e3e9064c8ca91d6bc704a1e82db431
benjymous <r.munn@outlook.com>
2022-12-06 07:00:26 +0000

    2022 Day6

---
+++ Advent2022/Day06_TuningTrouble.cs
@@ -0,0 +1,32 @@
+﻿using AoC.Utils;
+using System;
+using System.Linq;
+
+namespace AoC.Advent2022
+{
+    public class Day06 : IPuzzle
+    {
+        public string Name => "2022-06";
+
+        private static int FindSignal(string input, int size)
+        {
+            return input.Windows(size).WithIndex().Where(kvp => kvp.Value.Distinct().Count() == size).First().Index + size;
+        }
+
+        public static int Part1(string input)
+        {
+            return FindSignal(input, 4);
+        }
+
+        public static int Part2(string input)
+        {
+            return FindSignal(input, 14);
+        }
+
+        public void Run(string input, ILogger logger)
+        {
+            Console.WriteLine("- Pt1 - " + Part1(input));
+            Console.WriteLine("- Pt2 - " + Part2(input));
+        }
+    }
+}
---
+++ test/Advent2022/Day06Test.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC.Advent2022.Test
+{
+    [TestCategory("2022")]
+    [TestClass]
+    public class Day06Test
+    {
+        readonly string input = Util.GetInput < Day06 > ();
+
+        [TestCategory("Test")]
+        [DataRow("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+        [DataRow("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [DataRow("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [DataRow("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [DataRow("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        [DataTestMethod]
+        public void Signals01Test(string input, int expected)
+        {
+            Assert.AreEqual(expected, Day06.Part1(input));
+        }
+
+        [TestCategory("Test")]
+        [DataRow("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+        [DataRow("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+        [DataRow("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+        [DataRow("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+        [DataRow("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+        [DataTestMethod]
+        public void Signals02Test(string input, int expected)
+        {
+            Assert.AreEqual(expected, Day06.Part2(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Signals_Part1_Regression()
+        {
+            Assert.AreEqual(1766, Day06.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Signals_Part2_Regression()
+        {
+            Assert.AreEqual(2383, Day06.Part2(input));
+        }
+    }
+}
