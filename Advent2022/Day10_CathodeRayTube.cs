bdaf640abbcb013f33eddb681b97b1b6f8073d92
benjymous <r.munn@outlook.com>
2022-12-10 15:08:12 +0000

    Made solution more expandable, just in case...

--- Advent2022/Day10_CathodeRayTube.cs
+++ Advent2022/Day10_CathodeRayTube.cs
@@ -10,26 +10,47 @@ namespace AoC.Advent2022
     {
         public string Name => "2022-10";
 
-        private static IEnumerable<(int cycle, int x)> Simulate(string input)
+        public class Registers
         {
-            var instructions = Util.Split(input);
+            public int x = 1;
+        }
+
+        public interface IInstr
+        {
+            public int Stride { get; }
+            public void Act(Registers r);
+        }
+
+        struct Noop : IInstr
+        {
+            public int Stride => 1;
+            public void Act(Registers r) { }
+        }
+
+        record struct Addx(int Val) : IInstr
+        {
+            public int Stride => 2;
+            public void Act(Registers r) => r.x += Val;
+        }
+
+        public class Factory
+        {
+            [Regex("noop")] public static IInstr Noop() => new Noop();
+            [Regex("addx (.+)")] public static IInstr Addx(int v) => new Addx(v);
+        }
+
+        private static IEnumerable<(int cycle, Registers r)> Simulate(string input)
+        {
+            var instructions = Util.RegexFactory<IInstr>(input, new Factory()).ToArray();
 
             int cycle = 1;
-            int x = 1;
+
+            Registers r = new ();
             foreach (var instr in instructions)
             {
-                yield return(cycle, x);
-                if (instr == "noop")
-                {
-                    cycle++;
-                }
-                else
-                {
-                    yield return (cycle + 1, x);
-                    var bits = instr.Split(' ');
-                    x += int.Parse(bits[1]);
-                    cycle += 2;
-                }
+                for (int i=0; i<instr.Stride; ++i) yield return(cycle+i, r);
+                instr.Act(r);
+                cycle += instr.Stride;
             }
         }
 
@@ -37,8 +58,8 @@ namespace AoC.Advent2022
         {
             var watchValues = Simulate(input);
 
-            List<int> watchCycles = new() { 20, 60, 100, 140, 180, 220 };
-            return watchValues.Where(kvp => watchCycles.Contains(kvp.cycle)).Select(kvp => kvp.x * kvp.cycle).Sum();
+            HashSet<int> watchCycles = new() { 20, 60, 100, 140, 180, 220 };
+            return watchValues.Where(kvp => watchCycles.Contains(kvp.cycle)).Sum(kvp => kvp.r.x * kvp.cycle);
         }
 
         public static string Part2(string input, ILogger logger)
@@ -49,13 +70,9 @@ namespace AoC.Advent2022
 
             foreach (var val in watchValues.Take(240))
             {
-                int j = val.cycle % 40;
-                var check = val.x;
-                if (j >= check && j <= check + 2)
-                    crt.Append('#');
-                else
-                    crt.Append('.');
-
+                int beamPos = val.cycle % 40;
+                var spritePos = val.r.x;
+                crt.Append((beamPos >= spritePos && beamPos <= spritePos + 2) ? '#' : '.');
                 if (val.cycle % 40 == 0) crt.AppendLine();
             }
 
@@ -66,6 +83,7 @@ namespace AoC.Advent2022
 
         public void Run(string input, ILogger logger)
         {
+
             logger.WriteLine("- Pt1 - " + Part1(input));
             logger.WriteLine("- Pt2 - " + Part2(input, logger));
         }
