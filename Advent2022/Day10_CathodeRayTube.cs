eecf669dbec26b23dce8de175f8e1d984bbdfba5
benjymous <r.munn@outlook.com>
2022-12-10 14:36:26 +0000

    2022 day 10

---
+++ Advent2022/Day10_CathodeRayTube.cs
@@ -0,0 +1,73 @@
+﻿using AoC.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC.Advent2022
+{
+    public class Day10 : IPuzzle
+    {
+        public string Name => "2022-10";
+
+        private static IEnumerable<(int cycle, int x)> Simulate(string input)
+        {
+            var instructions = Util.Split(input);
+
+            int cycle = 1;
+            int x = 1;
+            foreach (var instr in instructions)
+            {
+                yield return(cycle, x);
+                if (instr == "noop")
+                {
+                    cycle++;
+                }
+                else
+                {
+                    yield return (cycle + 1, x);
+                    var bits = instr.Split(' ');
+                    x += int.Parse(bits[1]);
+                    cycle += 2;
+                }
+            }
+        }
+
+        public static int Part1(string input)
+        {
+            var watchValues = Simulate(input);
+
+            List<int> watchCycles = new() { 20, 60, 100, 140, 180, 220 };
+            return watchValues.Where(kvp => watchCycles.Contains(kvp.cycle)).Select(kvp => kvp.x * kvp.cycle).Sum();
+        }
+
+        public static string Part2(string input, ILogger logger)
+        {
+            var watchValues = Simulate(input);
+
+            StringBuilder crt = new();
+
+            foreach (var val in watchValues.Take(240))
+            {
+                int j = val.cycle % 40;
+                var check = val.x;
+                if (j >= check && j <= check + 2)
+                    crt.Append('#');
+                else
+                    crt.Append('.');
+
+                if (val.cycle % 40 == 0) crt.AppendLine();
+            }
+
+            logger.WriteLine(Environment.NewLine + crt.ToString());
+
+            return crt.ToString().GetMD5String();
+        }
+
+        public void Run(string input, ILogger logger)
+        {
+            logger.WriteLine("- Pt1 - " + Part1(input));
+            logger.WriteLine("- Pt2 - " + Part2(input, logger));
+        }
+    }
+}
---
+++ test/Advent2022/Day10Test.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC.Advent2022.Test
+{
+    [TestCategory("2022")]
+    [TestClass]
+    public class Day10Test
+    {
+        readonly string input = Util.GetInput<Day10>();
+
+        readonly string test = @"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop".Replace("\r", "");
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void CRT01Test()
+        {
+            Assert.AreEqual(13140, Day10.Part1(test));
+        }
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void CRT02Test()
+        {
+            Assert.AreEqual("45F50A605DB9F4AAA8D87734B565FAD9", Day10.Part2(test, new ConsoleOut()));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void CRT_Part1_Regression()
+        {
+            Assert.AreEqual(13860, Day10.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void CRT_Part2_Regression()
+        {
+            Assert.AreEqual("43510CF3264B016CAD42DFE6E410FB1A", Day10.Part2(input, new ConsoleOut() ));
+        }
+    }
+}
