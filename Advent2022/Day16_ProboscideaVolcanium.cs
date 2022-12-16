539e059f24a12bf4adc01be8b158f997aeaf45a5
benjymous <r.munn@outlook.com>
2022-12-16 18:22:58 +0000

    Massively optimise 2022 day 16 part1

--- Advent2022/Day16_ProboscideaVolcanium.cs
+++ Advent2022/Day16_ProboscideaVolcanium.cs
@@ -1,9 +1,11 @@
 ﻿using AoC.Utils;
 using AoC.Utils.Collections;
+using AoC.Utils.Pathfinding;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using static AoC.Advent2021.Day18;
 
 namespace AoC.Advent2022
 {
@@ -22,171 +24,198 @@ namespace AoC.Advent2022
             }
 
             public string Id;
-            public ulong IntId;
+            public uint IntId;
             public int Rate;
             public string[] Neighbours;
-            public ulong[] IntNeighbours;
         }
+
+        class MapData : IMap<string>
+        {
+            public MapData(Dictionary<string, Valve> data)
+            {
+                Data = data;
+            }
+
+            Dictionary<string, Valve> Data;
+
+            public IEnumerable<string> GetNeighbours(string location)
+            {
+                return Data[location].Neighbours;
+            }
+        }
+
         public static int Part1(string input)
         {
             var data = Util.RegexParse<Valve>(input);
+
+            var valves = data.Where(val => val.Rate > 0).ToArray();
+
+            uint id = 1 << 1;
+            foreach (var v in valves)
+            {
+                v.IntId = id <<= 1;
+            }
+
+            var map = data.ToDictionary(val => val.Id, val => val);
+            map["AA"] = data.Where(val => val.Id == "AA").First();
+
+            Dictionary<uint, int> routes = new();
+            var mapAdaptor = new MapData(map);
+
+            var lookup = valves.ToDictionary(val => val.IntId, val => val.Rate);
             
-            var map = data.ToDictionary(val => val.Id, val => val);            
+            foreach (var v1 in valves)
+            {
+                routes[1 | v1.IntId] = AStar<string>.FindPath(mapAdaptor, "AA", v1.Id).Length;
+                foreach (var v2 in valves)
+                {
+                    if (v1.IntId < v2.IntId)
+                    {
+                        routes[v1.IntId | v2.IntId] = AStar<string>.FindPath(mapAdaptor, v1.Id, v2.Id).Length;
+                    }
+                }
+            }
 
-            var queue = new Queue<(string location, int mins, int open, int released, HashSet<string> opened, string log)>();
-            queue.Enqueue(("AA", 0, 0, 0, new HashSet<string>(), ""));
+            var queue = new Queue<(uint location, int mins, int open, int released, uint toVisit)>();
+            queue.Enqueue((1, 0, 0, 0, (uint)valves.Sum(v => v.IntId)));
 
-            Dictionary<string, int> visited = new();
+            Dictionary<uint, int> seen = new();
 
             int best = int.MinValue;
-            string bestLog = "";
 
-            queue.Operate((val) =>
-            {
-                var key = $"{val.location}:{string.Join(",",val.opened.Order())}";
-                if (visited.TryGetValue(key, out var prev) && prev >= val.released) return;
-
-                var log = val.log + $"\n\n== Minute {val.mins+1} ==";
-                
-                visited[key] = val.released;
-
-                var node = map[val.location];
-
-                if (val.mins < 30)
+            queue.Operate(entry =>
+            {               
+                if (entry.mins < 30)
                 {
-                    val.released += val.open;
+                    entry.released += entry.open;
 
-                    var potential = val.released + (val.open * (29 - val.mins));
-                    if (potential > best)
-                    {
-                        bestLog = log + $"\n=> {potential} ({val.open} * {(29 - val.mins)}) after waiting";
-                        best = potential;
-                    }
+                    var potential = entry.released + (entry.open * (29 - entry.mins));                  
+                    best = Math.Max(best, potential);
 
-                    log += $"\n{val.open} pressure released";
-                    if (node.Rate > 0 && !val.opened.Contains(val.location))
-                    {
-                        queue.Enqueue((val.location, val.mins + 1, val.open + node.Rate, val.released, val.opened.Append(val.location).ToHashSet(), log + $"\nYou open valve {val.location}"));
-                    }
+                    var key = entry.location + entry.toVisit << 16;
+                    if (seen.TryGetValue(key, out var val) && val > potential) return;
+                    seen[key] = potential;
 
-                    foreach (var neighbour in node.Neighbours)
+                    foreach (var next in entry.toVisit.BitSequence())
                     {
-                        queue.Enqueue((neighbour, val.mins + 1, val.open, val.released, val.opened, log + $"\nYou move to valve {neighbour}"));
+                        var distance = routes[entry.location | next];
+
+                        var nextState = (location: next, mins: entry.mins + distance + 1, open: entry.open + lookup[next], released: entry.released + (entry.open * distance), toVisit: entry.toVisit - next);
+
+                        queue.Enqueue(nextState);
                     }
                 }
             });
-
-            Console.WriteLine(bestLog);
 
             return best;
         }
 
         public static int Part2(string input, ILogger logger)
         {
-            var data = Util.RegexParse<Valve>(input).ToArray();
-            ulong id = 1;
-            foreach (var elem in data)
-            {
-                elem.IntId = id;
-                id <<= 1;
-            }
+            //var data = Util.RegexParse<Valve>(input).ToArray();
+            //uint id = 1;
+            //foreach (var elem in data)
+            //{
+            //    elem.IntId = id;
+            //    id <<= 1;
+            //}
 
-            var lookup = data.ToDictionary(val => val.Id, val => val.IntId);
+            //var lookup = data.ToDictionary(val => val.Id, val => val.IntId);
 
-            foreach (var elem in data)
-            {
-                elem.IntNeighbours = elem.Neighbours.Select(id => lookup[id]).ToArray();
-            }
+            //foreach (var elem in data)
+            //{
+            //    elem.IntNeighbours = elem.Neighbours.Select(id => lookup[id]).ToArray();
+            //}
 
-            var map = data.ToDictionary(val => val.IntId, val => val);
+            //var map = data.ToDictionary(val => val.IntId, val => val);
 
-            var start = data.Where(el => el.Id == "AA").First();
-            var queue = new PriorityQueue<(ulong me, ulong elephant, int mins, int open, int released, ulong opened), int>();
-            queue.Enqueue((start.IntId, start.IntId, 0, 0, 0, 0), 0);
+            //var start = data.Where(el => el.Id == "AA").First();
+            //var queue = new PriorityQueue<(uint me, uint elephant, int mins, int open, int released, uint opened), int>();
+            //queue.Enqueue((start.IntId, start.IntId, 0, 0, 0, 0), 0);
 
-            Dictionary<(ulong, ulong), int> visited = new();
+            //Dictionary<(uint, uint), int> visited = new();
 
-            int best = int.MinValue;
+            //int best = int.MinValue;
 
-            int step = 0;
+            //int step = 0;
 
-            queue.Operate((val) =>
-            {
-                if (step++ % 200000 == 0)
-                {
-                    logger.WriteLine($"{step} : best: {best} : checked {visited.Count} : queue: {queue.Count}");
-                }
+            //queue.Operate((val) =>
+            //{
+            //    if (step++ % 200000 == 0)
+            //    {
+            //        logger.WriteLine($"{step} : best: {best} : checked {visited.Count} : queue: {queue.Count}");
+            //    }
 
-                var key = (val.me+val.elephant, val.opened);
-                if (visited.TryGetValue(key, out var prev) && prev < val.mins) return;
+            //    var key = (val.me+val.elephant, val.opened);
+            //    if (visited.TryGetValue(key, out var prev) && prev < val.mins) return;
 
-                visited[key] = val.mins;
+            //    visited[key] = val.mins;
 
-                var node1 = map[val.me];
-                var node2 = map[val.elephant];
+            //    var node1 = map[val.me];
+            //    var node2 = map[val.elephant];
 
-                if (val.mins < 26)
-                {
-                    val.released += val.open;
+            //    if (val.mins < 26)
+            //    {
+            //        val.released += val.open;
 
-                    var potential = val.released + (val.open * (25 - val.mins));
-                    if (potential > best)
-                    {
-                        best = potential;
-                    }
+            //        var potential = val.released + (val.open * (25 - val.mins));
+            //        if (potential > best)
+            //        {
+            //            best = potential;
+            //        }
 
-                    List<(ulong loc, int openRate)> myMoves = new(6), elephantMoves = new(6);
+            //        List<(uint loc, int openRate)> myMoves = new(6), elephantMoves = new(6);
 
-                    // me
-                    if (node1.Rate > 0 && ((val.opened & val.me) == 0))
-                    {
-                        myMoves.Add((val.me, node1.Rate));
-                    }
+            //        // me
+            //        if (node1.Rate > 0 && ((val.opened & val.me) == 0))
+            //        {
+            //            myMoves.Add((val.me, node1.Rate));
+            //        }
 
-                    foreach (var neighbour in node1.IntNeighbours)
-                    {
-                        myMoves.Add((neighbour, 0));
-                    }
+            //        foreach (var neighbour in node1.IntNeighbours)
+            //        {
+            //            myMoves.Add((neighbour, 0));
+            //        }
 
-                    // elephant
-                    if (node2.Rate > 0 && ((val.opened & val.elephant) == 0))
-                    {
-                        elephantMoves.Add((val.elephant, node2.Rate));
-                    }
+            //        // elephant
+            //        if (node2.Rate > 0 && ((val.opened & val.elephant) == 0))
+            //        {
+            //            elephantMoves.Add((val.elephant, node2.Rate));
+            //        }
 
-                    foreach (var neighbour in node2.IntNeighbours)
-                    { 
-                        elephantMoves.Add((neighbour, 0));
-                    }
+            //        foreach (var neighbour in node2.IntNeighbours)
+            //        { 
+            //            elephantMoves.Add((neighbour, 0));
+            //        }
 
-                    // combine possible moves
-                    var combos = Util.Combinations(myMoves, elephantMoves);
-                    foreach (var combo in combos)
-                    {
-                        if (val.me == val.elephant && combo.Item1.openRate > 0 && combo.Item1.openRate == combo.Item2.openRate) continue;
+            //        // combine possible moves
+            //        var combos = Util.Combinations(myMoves, elephantMoves);
+            //        foreach (var combo in combos)
+            //        {
+            //            if (val.me == val.elephant && combo.Item1.openRate > 0 && combo.Item1.openRate == combo.Item2.openRate) continue;
 
-                        var newOpened = val.opened;
-                        if (combo.Item1.openRate > 0) newOpened += val.me;
-                        if (combo.Item2.openRate > 0) newOpened += val.elephant;
-                        var next = (me: combo.Item1.loc, elephant: combo.Item2.loc, mins: val.mins + 1, val.open + combo.Item1.openRate + combo.Item2.openRate, val.released, newOpened);
-                        var nextKey = (next.me + next.elephant, newOpened);
-                        if (!visited.TryGetValue(nextKey, out var prev2) || prev2 > next.mins)
-                        {
-                            queue.Enqueue(next, 0);
-                            //visited[nextKey] = next.mins;
-                        }
-                    }
-                }
-            });
-            logger.WriteLine($"{step} : best: {best} : checked {visited.Count} : queue: {queue.Count}");
+            //            var newOpened = val.opened;
+            //            if (combo.Item1.openRate > 0) newOpened += val.me;
+            //            if (combo.Item2.openRate > 0) newOpened += val.elephant;
+            //            var next = (me: combo.Item1.loc, elephant: combo.Item2.loc, mins: val.mins + 1, val.open + combo.Item1.openRate + combo.Item2.openRate, val.released, newOpened);
+            //            var nextKey = (next.me + next.elephant, newOpened);
+            //            if (!visited.TryGetValue(nextKey, out var prev2) || prev2 > next.mins)
+            //            {
+            //                queue.Enqueue(next, 0);
+            //                //visited[nextKey] = next.mins;
+            //            }
+            //        }
+            //    }
+            //});
+            //logger.WriteLine($"{step} : best: {best} : checked {visited.Count} : queue: {queue.Count}");
 
-            return best;
+            return 0;// best;
         }
 
         public void Run(string input, ILogger logger)
         {
-            //logger.WriteLine("- Pt1 - " + Part1(input));
-            logger.WriteLine("- Pt2 - " + Part2(input, logger));
+            logger.WriteLine("- Pt1 - " + Part1(input));
+            //logger.WriteLine("- Pt2 - " + Part2(input, logger));
         }
     }
 }
