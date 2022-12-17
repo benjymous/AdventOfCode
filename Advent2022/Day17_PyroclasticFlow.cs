bf651f222ea016cc1e1078a5be43eaf6c64ca8f7
benjymous <r.munn@outlook.com>
2022-12-17 20:17:15 +0000

    day 17 optimisation

--- Advent2022/Day17_PyroclasticFlow.cs
+++ Advent2022/Day17_PyroclasticFlow.cs
@@ -14,107 +14,107 @@ namespace AoC.Advent2022
 
         static IEnumerable<int> WindSequence(string input) => input.Trim().Forever().Select(c => c == '<' ? -1 : 1);
 
-        public static bool Blocked(HashSet<(int x, int y)> data, (int x, int y) pos)
+        class State
         {
-            return (pos.y <= 0 || pos.x <= 0 || pos.x >= 8 || data.Contains(pos));
-        }
+            public int MaxY = 0;
+            Dictionary<int, byte> map = new() { [0] = 255 };
 
-        private static bool CheckBlocked(HashSet<(int x, int y)> map, IEnumerable<(int x, int y)> translated, int dx, int dy)
-        {
-            foreach (var pos in translated)
+            public bool Blocked((int x, int y) pos)
             {
-                if (Blocked(map, (pos.x + dx, pos.y + dy)))
+                return pos.y <= 0 || pos.x <= 0 || pos.x >= 8 || map.TryGetValue(pos.y, out var row) && ((row & 1 << pos.x) != 0);
+            }
+
+            public bool CheckBlocked(IEnumerable<(int x, int y)> shape, int dx, int dy)
+            {
+                foreach (var (x, y) in shape)
                 {
-                    return true;
+                    if (Blocked((x + dx, y + dy)))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public void FinishBlock(IEnumerable<(int x, int y)> shape, (int x, int y) pos)
+            {
+                foreach (var (x, y) in shape.Select(p => (p.x + pos.x, p.y + pos.y)))
+                {
+                    if (!map.ContainsKey(y)) map[y] = 0;
+                    MaxY = Math.Max(y, MaxY);
+                    map[y] |= (byte)(1 << x);
                 }
             }
-            return false;
-        }
 
-        private static void FinishBlock(HashSet<(int x, int y)> map, IEnumerable<(int, int)> translated)
-        {
-            foreach (var e in translated)
+            public void Cull()
             {
-                map.Add(e);
+                if (MaxY % 100 == 0)
+                {
+                    map = map.Where(kvp => kvp.Key >= MaxY - 20).ToDictionary();
+                }
             }
+
         }
 
-        //public static void Display(HashSet<(int x, int y)> map, HashSet<(int x, int y)> shape)
-        //{
-        //    return;
-        //    var miny = 0;
-        //    var maxy = map.Max(pos => pos.y) + 4;
-        //    var minx = 1;
-        //    var maxx = 7;
-
-        //    for (var y = maxy; y > 0; y--)
-        //    {
-        //        for (var x = minx; x <= maxx; ++x)
-        //        {
-        //            if (shape.Contains((x, y)))
-        //            {
-        //                Console.Write('O');
-        //            }
-        //            else Console.Write((map.Contains((x, y)) ? '#' : '.'));
-        //        }
-        //        Console.WriteLine();
-        //    }
-        //    Console.WriteLine();
-        //}
-
-        public static int Part1(string input)
+        private static int RunRocktris(string input, ulong rounds)
         {
             var wind = WindSequence(input).GetEnumerator();
             wind.MoveNext();
 
             var shapes = Util.Split(shapeData, '|').Select(part => Util.ParseSparseMatrix<bool>(part).Keys.ToArray()).ToArray();
 
-            HashSet<(int x, int y)> map = new()
-            {
-                (0, 0)
-            };
-
             int currentShape = 0;
 
-            for (int i = 0; i < 2022; ++i)
+            var state = new State();
+
+            for (ulong i = 0; i < rounds; ++i)
             {
-                (int x, int y) rockPos = (3, 4 + map.Max(p => p.y));
+                (int x, int y) rockPos = (3, 4 + state.MaxY);
                 while (true)
                 {
-                    var translated = shapes[currentShape].Select(pos => (x: pos.x + rockPos.x, y: pos.y + rockPos.y));
-
+                    var shape = shapes[currentShape];
                     var dx = wind.Pop();
-                    if (!CheckBlocked(map, translated, dx, 0))
+                    if (!state.CheckBlocked(shape, rockPos.x + dx, rockPos.y))
                     {
                         rockPos.x += dx;
                     }
 
-                    if (CheckBlocked(map, translated, 0, -1))
+                    if (state.CheckBlocked(shape, rockPos.x, rockPos.y - 1))
                     {
-                        FinishBlock(map, translated);
+                        state.FinishBlock(shape, rockPos);
                         break;
                     }
                     rockPos.y--;
                 }
                 currentShape = (currentShape + 1) % 5;
+
+                state.Cull();
+
+                //if (i % 100000 == 0)
+                //{
+                //    Console.WriteLine($"{i} / {rounds}");
+                //}
             }
 
-            return map.Max(p => p.y);
+            return state.MaxY;
         }
 
-        public static int Part2(string input)
+        public static int Part1(string input)
         {
-            return 0;
+            return RunRocktris(input, 2022);
+        }
+
+        public static long Part2(string input)
+        {
+            return RunRocktris(input, 1000000000000);
         }
 
         public void Run(string input, ILogger logger)
         {
-            string test = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
-
-            Console.WriteLine(Part1(test));
+            //Console.WriteLine(Part1(test));
 
             logger.WriteLine("- Pt1 - " + Part1(input));
-            logger.WriteLine("- Pt2 - " + Part2(input));
+            //logger.WriteLine("- Pt2 - " + Part2(input));
         }
     }
 }
---
+++ test/Advent2022/Day17Test.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC.Advent2022.Test
+{
+    [TestCategory("2022")]
+    [TestClass]
+    public class Day17Test
+    {
+        readonly string input = Util.GetInput<Day17>();
+        readonly string test = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Rocktris01Test()
+        {
+            Assert.AreEqual(3068, Day17.Part1(test));
+        }
+
+        [TestCategory("Test")]
+        [DataTestMethod]
+        public void Rocktris02Test()
+        {
+            Assert.AreEqual(1514285714288, Day17.Part2(test));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Rocktris_Part1_Regression()
+        {
+            Assert.AreEqual(3059, Day17.Part1(input));
+        }
+
+        [TestCategory("Regression")]
+        [DataTestMethod]
+        public void Rocktris_Part2_Regression()
+        {
+            Assert.AreEqual(0, Day17.Part2(input));
+        }
+    }
+}
