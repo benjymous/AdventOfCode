a3a342e4d3ff231081b1c25f6299c6a946c5810b
benjymous <r.munn@outlook.com>
2023-12-02 08:31:53 +0000

    2023 Day 2

---
+++ AoC/Advent2023/Day02_CubeConundrum.cs
@@ -0,0 +1,55 @@
+﻿namespace AoC.Advent2023;
+public class Day02 : IPuzzle
+{
+    [Regex(@"(.+)")]
+    record class Set(Entry[] Items)
+    {
+        public bool Possible() => Items.All(i => i.Possible());
+    }
+
+    [Regex(@"(\d+) (.+)")]
+    record class Entry(int Num, string Colour)
+    {
+        public bool Possible() => Colour switch
+        {
+            "red" => Num <= 12,
+            "green" => Num <= 13,
+            "blue" => Num <= 14,
+            _ => false,
+        };
+
+    }
+    [Regex(@"Game (\d+): (.+)")]
+    record class Game(int Id, [Split(";")] Set[] Sets)
+    {
+        public bool Possible() => Sets.All(s => s.Possible());
+
+        public Dictionary<string, int> GetMinCounts()
+        {
+            Dictionary<string, int> values = [];
+
+            foreach (var entry in Sets.SelectMany(set => set.Items.Where(entry => !values.TryGetValue(entry.Colour, out var prev) || prev < entry.Num)))
+            {
+                values[entry.Colour] = entry.Num;
+            }
+
+            return values;
+        }
+
+        public int Power()
+        {
+            var counts = GetMinCounts();
+            return counts["red"] * counts["green"] * counts["blue"];
+        }
+    }
+
+    public static int Part1(string input) => Util.RegexParse<Game>(input).Where(game => game.Possible()).Sum(g => g.Id);
+
+    public static int Part2(string input) => Util.RegexParse<Game>(input).Sum(g => g.Power());
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2023/Day02Test.cs
+++ test/Advent2023/Day02Test.cs
@@ -6,36 +6,42 @@ namespace AoC.Advent2023.Test;
 [TestClass]
 public class Day02Test
 {
-    readonly string input = ""; // Util.GetInput<Day02>();
+    readonly string input = Util.GetInput<Day02>();
+
+
+    readonly string test =
+@"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".Replace("\r","");
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void Cubes01Test()
     {
-        Assert.IsTrue(Day02.Part1(input) == expected);
+        Assert.AreEqual(8, Day02.Part1(test));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void Cubes02Test()
     {
-        Assert.IsTrue(Day02.Part2(input) == expected);
+        Assert.AreEqual(2286, Day02.Part2(test));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void CubeConundrum_Part1_Regression()
     {
-        Assert.AreEqual(0, Day02.Part1(input));
+        Assert.AreEqual(2237, Day02.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void CubeConundrum_Part2_Regression()
     {
-        Assert.AreEqual(0, Day02.Part2(input));
+        Assert.AreEqual(66681, Day02.Part2(input));
     }
 }
 
