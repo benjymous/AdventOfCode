b3283a191d3ac4211f2d51b319e44c631919f9b2
benjymous <r.munn@outlook.com>
2023-12-05 11:01:39 +0000

    2023 Day 5 - still needs optimising

--- test/Advent2023/Day05Test.cs
+++ test/Advent2023/Day05Test.cs
@@ -6,36 +6,81 @@ namespace AoC.Advent2023.Test;
 [TestClass]
 public class Day05Test
 {
-    readonly string input = ""; // Util.GetInput<Day05>();
+    readonly string input = Util.GetInput<Day05>();
+
+    readonly string test =
+@"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4".Replace("\r", "");
+
+    /*
+        //Util.Test(factory.Remap("seed", "soil", 79), 81);
+        //Util.Test(factory.Remap("seed", "soil", 14), 14);
+        //Util.Test(factory.Remap("seed", "soil", 55), 57);
+        //Util.Test(factory.Remap("seed", "soil", 13), 13);
+
+        //Util.Test(factory.Remap("seed", "location", 79), 82);
+        //Util.Test(factory.Remap("seed", "location", 14), 43);
+        //Util.Test(factory.Remap("seed", "location", 55), 86);
+        //Util.Test(factory.Remap("seed", "location", 13), 35);
+    */
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void Seeds_01Test()
     {
-        Assert.AreEqual(expected, Day05.Part1(input));
+        Assert.AreEqual(35, Day05.Part1(test));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void Seeds_02Test()
     {
-        Assert.AreEqual(expected, Day05.Part2(input));
+        Assert.AreEqual(46, Day05.Part2(test));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void SeedMapping_Part1_Regression()
     {
-        Assert.AreEqual(0, Day05.Part1(input));
+        Assert.AreEqual(289863851, Day05.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void SeedMapping_Part2_Regression()
     {
-        Assert.AreEqual(0, Day05.Part2(input));
+        // Assert.AreEqual(60568880, Day05.Part2(input));
     }
 }
 
