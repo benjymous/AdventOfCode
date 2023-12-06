78f40d1c8be2434753000fdc9c2064858e4d51c4
benjymous <r.munn@outlook.com>
2023-12-06 14:25:07 +0000

    2023 Day 6 optimisations

---
+++ AoC/Advent2023/Day05_IfYouGiveASeedAFertilizer.cs
@@ -0,0 +1,80 @@
+﻿namespace AoC.Advent2023;
+public class Day05 : IPuzzle
+{
+    public class Mapper
+    {
+        readonly List<(long from, long delta, long count)> ranges = [];
+        public void AddRange(long to, long from, long count) => ranges.Add((from, to - from, count));
+
+        public long Remap(bool forwards, long source)
+        {
+            var remap = forwards
+                ? ranges.FirstOrDefault(range => source >= range.from && source < range.from + range.count)
+                : ranges.FirstOrDefault(range => source - range.delta >= range.from && source - range.delta < range.from + range.count);
+            return remap == default ? source : forwards ? source + remap.delta : source - remap.delta;
+        }
+    }
+
+    public class Factory
+    {
+        public long[] Seeds;
+
+        public Dictionary<string, (string to, Mapper map)> maps = [], mapsReverse = [];
+        private Mapper current = null;
+
+        [Regex(@"seeds: (.+)")]
+        public void ParseSeeds([Split(" ")] long[] seedNums) => Seeds = seedNums;
+
+        [Regex(@"(.+)-to-(.+) map:")]
+        public void NewMap(string from, string to)
+        {
+            current = new();
+            maps[from] = (to, current);
+            mapsReverse[to] = (from, current);
+        }
+
+        [Regex(@"(\d+) (\d+) (\d+)")]
+        public void ParseMapEntry(long to, long from, long count) => current.AddRange(to, from, count);
+
+        private long DoMapping(bool forwards, string from, string to, long val)
+        {
+            var (mapTo, mapper) = (forwards ? maps : mapsReverse)[from];
+            var nextVal = mapper.Remap(forwards, val);
+            return mapTo == to ? nextVal : DoMapping(forwards, mapTo, to, nextVal);
+        }
+
+        public long Remap(string from, string to, long val) => DoMapping(true, from, to, val);
+        public long Reverse(string from, string to, long val) => DoMapping(false, from, to, val);
+    }
+
+    public static long Part1(string input)
+    {
+        var factory = Util.RegexFactory<Factory>(input);
+
+        return factory.Seeds.Select(seed => factory.Remap("seed", "location", seed)).Min();
+    }
+
+    public static long Part2(string input)
+    {
+        var factory = Util.RegexFactory<Factory>(input);
+
+        var chunks = factory.Seeds.Chunk(2).Select(c => (start: c[0], end: c[0] + c[1])).ToArray();
+
+        int jumpSize = 10000;
+
+        for (long location = 0; ; location++)
+        {
+            var seed = factory.Reverse("location", "seed", location);
+
+            if (chunks.Any(c => seed >= c.start && seed < c.end)) return location;
+
+            if ((factory.Reverse("location", "seed", location + jumpSize) - (location + jumpSize)) == seed - location) location += jumpSize;
+        }
+    }
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2023/Day05Test.cs
+++ test/Advent2023/Day05Test.cs
@@ -43,17 +43,53 @@ humidity-to-location map:
 60 56 37
 56 93 4".Replace("\r", "");
 
-    /*
-        //Util.Test(factory.Remap("seed", "soil", 79), 81);
-        //Util.Test(factory.Remap("seed", "soil", 14), 14);
-        //Util.Test(factory.Remap("seed", "soil", 55), 57);
-        //Util.Test(factory.Remap("seed", "soil", 13), 13);
+    [TestCategory("Test")]
+    [DataRow("seed", "soil", 79, 81)]
+    [DataRow("seed", "soil", 14, 14)]
+    [DataRow("seed", "soil", 55, 57)]
+    [DataRow("seed", "soil", 13, 13)]
 
-        //Util.Test(factory.Remap("seed", "location", 79), 82);
-        //Util.Test(factory.Remap("seed", "location", 14), 43);
-        //Util.Test(factory.Remap("seed", "location", 55), 86);
-        //Util.Test(factory.Remap("seed", "location", 13), 35);
-    */
+    [DataRow("seed", "location", 79, 82)]
+    [DataRow("seed", "location", 14, 43)]
+    [DataRow("seed", "location", 55, 86)]
+    [DataRow("seed", "location", 13, 35)]
+    [DataTestMethod]
+    public void ForwardMappingTest(string from, string to, int start, int expected)
+    {
+        var factory = Util.RegexFactory<Day05.Factory>(test);
+        Assert.AreEqual(expected, factory.Remap(from, to, start));
+    }
+
+    [TestCategory("Test")]
+
+    [DataRow("water", "fertilizer", 81, 81)]
+    [DataRow("water", "fertilizer", 49, 53)]
+    [DataRow("water", "fertilizer", 53, 57)]
+    [DataRow("water", "fertilizer", 41, 52)]
+
+    [DataRow("fertilizer", "soil", 81, 81)]
+    [DataRow("fertilizer", "soil", 53, 14)]
+    [DataRow("fertilizer", "soil", 57, 57)]
+    [DataRow("fertilizer", "soil", 52, 13)]
+
+    [DataRow("soil", "seed", 81, 79)]
+    [DataRow("soil", "seed", 14, 14)]
+    [DataRow("soil", "seed", 57, 55)]
+    [DataRow("soil", "seed", 13, 13)]
+
+    [DataRow("location", "seed", 82, 79)]
+    [DataRow("location", "seed", 43, 14)]
+    [DataRow("location", "seed", 86, 55)]
+    [DataRow("location", "seed", 35, 13)]
+
+    [DataRow("location", "seed", 46, 82)]
+
+    [DataTestMethod]
+    public void ReverseMappingTest(string from, string to, int start, int expected)
+    {
+        var factory = Util.RegexFactory<Day05.Factory>(test);
+        Assert.AreEqual(expected, factory.Reverse(from, to, start));
+    }
 
     [TestCategory("Test")]
     [TestMethod]
@@ -80,7 +116,7 @@ humidity-to-location map:
     [DataTestMethod]
     public void SeedMapping_Part2_Regression()
     {
-        // Assert.AreEqual(60568880, Day05.Part2(input));
+        Assert.AreEqual(60568880, Day05.Part2(input));
     }
 }
 
