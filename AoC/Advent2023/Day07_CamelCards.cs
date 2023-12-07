c72584a162d12592b745c1998837295686a37ac0
benjymous <r.munn@outlook.com>
2023-12-07 15:26:12 +0000

    2023 Day 7

---
+++ AoC/Advent2023/Day07_CamelCards.cs
@@ -0,0 +1,72 @@
+﻿namespace AoC.Advent2023;
+public class Day07 : IPuzzle
+{
+    public static int CardIndex(char c) => "AKQJT98765432*".IndexOf(c);
+
+    public enum HandType
+    {
+        FiveOfAKind = 6,
+        FourOfAKind = 5,
+        FullHouse = 4,
+        ThreeOfAKind = 3,
+        TwoPair = 2,
+        OnePair = 1,
+        HighCard = 0,
+    }
+
+    [Regex(@"(.....) (\d+)")]
+    public readonly record struct Hand(string Cards, int Bid = 0)
+    {
+        public readonly HandType HandType = GetHandType(Cards);
+        public readonly int Ordering = GetSortOrder(Cards);
+    }
+
+    public static int GetSortOrder(string cards)
+    {
+        int num = 0;
+        foreach (var c in cards) num = (num << 4) + CardIndex(c);
+        return -num;
+    }
+
+    public static string ApplyJokers(string cards)
+    {
+        if (!cards.Contains('*')) return cards;
+        if (cards == "*****") return "AAAAA";
+
+        var groups = GroupCards(cards.Replace("*", "")).ToArray();
+
+        if (groups.Length == 2 && groups[0].Count == 2 && groups[1].Count == 2)
+        {
+            return cards.Replace('*', (CardIndex(groups[0].Key) <= CardIndex(groups[1].Key)) ? groups[0].Key : groups[1].Key);
+        }
+
+        return cards.Replace('*', groups[0].Key);
+    }
+
+    public static HandType GetHandType(string hand) => GroupCards(ApplyJokers(hand)).Select(g => g.Count).Decompose2() switch
+    {
+        (5, 0) => HandType.FiveOfAKind,
+        (4, 1) => HandType.FourOfAKind,
+        (3, 2) => HandType.FullHouse,
+        (3, 1) => HandType.ThreeOfAKind,
+        (2, 2) => HandType.TwoPair,
+        (2, 1) => HandType.OnePair,
+        _ => HandType.HighCard
+    };
+
+    static IEnumerable<(char Key, int Count)> GroupCards(string hand) => hand.GroupBy(c => c).Select(g => (g.Key, Count: g.Count())).OrderByDescending(g => g.Count);
+
+    public static IEnumerable<Hand> SortCards(IEnumerable<Hand> cards) => cards.OrderBy(h => (h.HandType, h.Ordering));
+
+    static int Solve(string input) => SortCards(Util.RegexParse<Hand>(input).ToArray()).WithIndex(1).ToArray().Sum(h => h.Index * h.Value.Bid);
+
+    public static int Part1(string input) => Solve(input);
+
+    public static int Part2(string input) => Solve(input.Replace('J', '*'));
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2023/Day07Test.cs
+++ test/Advent2023/Day07Test.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace AoC.Advent2023.Test;
 
@@ -6,36 +7,118 @@ namespace AoC.Advent2023.Test;
 [TestClass]
 public class Day07Test
 {
-    readonly string input = ""; // Util.GetInput<Day07>();
+    readonly string input = Util.GetInput<Day07>();
+
+    readonly string test =
+@"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483".Replace("\r", "");
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow("AAAAA", Day07.HandType.FiveOfAKind)]
+    [DataRow("AA8AA", Day07.HandType.FourOfAKind)]
+    [DataRow("23332", Day07.HandType.FullHouse)]
+    [DataRow("TTT98", Day07.HandType.ThreeOfAKind)]
+    [DataRow("23432", Day07.HandType.TwoPair)]
+    [DataRow("A23A4", Day07.HandType.OnePair)]
+    [DataRow("23456", Day07.HandType.HighCard)]
+
+    [DataRow("32T3K", Day07.HandType.OnePair)]
+    [DataRow("T55J5", Day07.HandType.ThreeOfAKind)]
+    [DataRow("KK677", Day07.HandType.TwoPair)]
+    [DataRow("KTJJT", Day07.HandType.TwoPair)]
+    [DataRow("QQQJA", Day07.HandType.ThreeOfAKind)]
+
     [DataTestMethod]
-    public void _01Test(string input, int expected)
+    public void HandTypeTest1(string input, Day07.HandType expected)
     {
-        Assert.AreEqual(expected, Day07.Part1(input));
+        Assert.AreEqual(expected, Day07.GetHandType(input));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow("AAAAA", Day07.HandType.FiveOfAKind)]
+    [DataRow("AA8AA", Day07.HandType.FourOfAKind)]
+    [DataRow("23332", Day07.HandType.FullHouse)]
+    [DataRow("TTT98", Day07.HandType.ThreeOfAKind)]
+    [DataRow("23432", Day07.HandType.TwoPair)]
+    [DataRow("A23A4", Day07.HandType.OnePair)]
+    [DataRow("23456", Day07.HandType.HighCard)]
+
+    [DataRow("32T3K", Day07.HandType.OnePair)]
+    [DataRow("T55J5", Day07.HandType.FourOfAKind)]
+    [DataRow("KK677", Day07.HandType.TwoPair)]
+    [DataRow("KTJJT", Day07.HandType.FourOfAKind)]
+    [DataRow("QQQJA", Day07.HandType.FourOfAKind)]
+
     [DataTestMethod]
-    public void _02Test(string input, int expected)
+    public void HandTypeTest2(string input, Day07.HandType expected)
     {
-        Assert.AreEqual(expected, Day07.Part2(input));
+        Assert.AreEqual(expected, Day07.GetHandType(input.Replace("J", "*")));
+    }
+
+    [TestCategory("Test")]
+    [DataRow("32T3K", "32T3K")]
+    [DataRow("T55J5", "T5555")]
+    [DataRow("KK677", "KK677")]
+    [DataRow("KTJJT", "KTTTT")]
+    [DataRow("QQQJA", "QQQQA")]
+    [DataRow("JJJJJ", "AAAAA")]
+    [DataRow("JJJJK", "KKKKK")]
+
+    [DataTestMethod]
+    public void ApplyJokersTest(string input, string expected)
+    {
+        Assert.AreEqual(expected, Day07.ApplyJokers(input.Replace("J", "*")));
+    }
+
+    [TestCategory("Test")]
+    [TestMethod]
+    public void OrderingTest1()
+    {
+        string[] cards = ["T55J5", "KK677", "KTJJT", "QQQJA", "32T3K"];
+        string[] expected = ["32T3K", "KTJJT", "KK677", "T55J5", "QQQJA"];
+
+        Assert.AreEqual(string.Join(", ", expected), string.Join(", ", Day07.SortCards(cards.Select(h => new Day07.Hand(h))).Select(h => h.Cards)));
+    }
+
+    [TestCategory("Test")]
+    [TestMethod]
+    public void OrderingTest2()
+    {
+        string[] cards = ["T55J5", "KK677", "KTJJT", "QQQJA", "32T3K"];
+        string[] expected = ["32T3K", "KK677", "T55J5", "QQQJA", "KTJJT"];
+
+        Assert.AreEqual(string.Join(", ", expected), string.Join(", ", Day07.SortCards(cards.Select(h => new Day07.Hand(h.Replace("J", "*")))).Select(h => h.Cards)).Replace("*", "J"));
+    }
+
+    [TestCategory("Test")]
+    [DataTestMethod]
+    public void CardRanking_01Test()
+    {
+        Assert.AreEqual(6440, Day07.Part1(test));
+    }
+
+    [TestCategory("Test")]
+    [DataTestMethod]
+    public void CardRanking_02Test()
+    {
+        Assert.AreEqual(5905, Day07.Part2(test));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void CamelCards_Part1_Regression()
     {
-        Assert.AreEqual(0, Day07.Part1(input));
+        Assert.AreEqual(251216224, Day07.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void CamelCards_Part2_Regression()
     {
-        Assert.AreEqual(0, Day07.Part2(input));
+        Assert.AreEqual(250825971, Day07.Part2(input));
     }
 }
 
