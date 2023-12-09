cd5e56ca7a3de6ca654d9f648f9e40168e187380
benjymous <r.munn@outlook.com>
2023-12-09 20:59:43 +0000

    2023 Day 9

---
+++ AoC/Advent2023/Day09_MirageMaintenance.cs
@@ -0,0 +1,26 @@
+﻿namespace AoC.Advent2023;
+public class Day09 : IPuzzle
+{
+    public static int GetNext(int[] sequence)
+    {
+        int last = sequence[sequence.Length - 1];
+        return sequence.All(v => v == last) 
+            ? last
+            : last + GetNext([..sequence.Windows(2).Select(pair => pair[1] - pair[0])]);
+    }
+
+    public static int GetPrev(int[] sequence) => GetNext(sequence.Reverse().ToArray());
+
+    static IEnumerable<int[]> ParseData(string input) 
+        => Util.Split(input).Select(line => Util.ParseNumbers<int>(line, " "));
+
+    public static int Part1(string input) => ParseData(input).Sum(GetNext);
+
+    public static int Part2(string input) => ParseData(input).Sum(GetPrev);
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2023/Day09Test.cs
+++ test/Advent2023/Day09Test.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace AoC.Advent2023.Test;
 
@@ -6,36 +7,42 @@ namespace AoC.Advent2023.Test;
 [TestClass]
 public class Day09Test
 {
-    readonly string input = ""; // Util.GetInput<Day09>();
+    readonly string input = Util.GetInput<Day09>();
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow("0 3 6 9 12 15", 18)]
+    [DataRow("1 3 6 10 15 21", 28)]
+    [DataRow("10 13 16 21 30 45", 68)]
     [DataTestMethod]
-    public void _01Test(string input, int expected)
+    public void GetNextTest(string input, int expected)
     {
-        Assert.AreEqual(expected, Day09.Part1(input));
+        var nums = Util.ExtractNumbers<int>(input).ToArray();
+        Assert.AreEqual(expected, Day09.GetNext(nums));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow("0 3 6 9 12 15", -3)]
+    [DataRow("1 3 6 10 15 21", 0)]
+    [DataRow("10 13 16 21 30 45", 5)]
     [DataTestMethod]
-    public void _02Test(string input, int expected)
+    public void GetPrevTest(string input, int expected)
     {
-        Assert.AreEqual(expected, Day09.Part2(input));
+        var nums = Util.ExtractNumbers<int>(input).ToArray();
+        Assert.AreEqual(expected, Day09.GetPrev(nums));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void Oasis_Part1_Regression()
     {
-        Assert.AreEqual(0, Day09.Part1(input));
+        Assert.AreEqual(1868368343, Day09.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void Oasis_Part2_Regression()
     {
-        Assert.AreEqual(0, Day09.Part2(input));
+        Assert.AreEqual(1022, Day09.Part2(input));
     }
 }
 
