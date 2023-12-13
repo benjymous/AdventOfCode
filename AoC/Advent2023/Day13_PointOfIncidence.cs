d8548c0387809b57ca87eb837e522599f1374dff
benjymous <r.munn@outlook.com>
2023-12-13 14:57:27 +0000

    2023 Day 13

---
+++ AoC/Advent2023/Day13_PointOfIncidence.cs
@@ -0,0 +1,85 @@
+﻿namespace AoC.Advent2023;
+public class Day13 : IPuzzle
+{
+    private static (bool valid, int value, bool isBlotchy) ValidatePairs(string[] vals, Dictionary<int, HashSet<int>> dict, int centre)
+    {
+        int blotches = 0;
+        for (int i = centre, j = centre + 1; i >= 1 && j <= vals.Length; i--, j++)
+        {
+            if (!dict.TryGetValue(i, out HashSet<int> value) || !value.Contains(j) || (vals[i - 1] != vals[j - 1] && blotches++ == 2))
+                return default;
+        }
+        return (true, centre, blotches > 0);
+    }
+
+    private static (bool found, int position, bool isBlotchy) ValidateGroup(HashSet<int> group, string[] vals, Dictionary<int, HashSet<int>> dict)
+        => group.Where(v => group.Contains(v + 1)).Select(v => ValidatePairs(vals, dict, v)).FirstOrDefault(r => r.valid);
+
+    static Dictionary<int, HashSet<int>> DoGrouping((int index, string value)[] data, bool part2)
+    {
+        Dictionary<int, HashSet<int>> dict = [];
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            var v1 = data[i];
+            for (int j = i + 1; j < data.Length; j++)
+            {
+                var v2 = data[j];
+
+                if (part2 ? StringsDifferByNoMoreThanOne(v1.value, v2.value) : v1.value == v2.value)
+                {
+                    if (dict.TryGetValue(v1.index, out HashSet<int> value))
+                    {
+                        value.Add(v2.index);
+                    }
+                    else
+                    {
+                        dict[v1.index] = [v1.index, v2.index];
+                    }
+                }
+            }
+        }
+
+        return dict;
+    }
+
+    public static int FindMirror(IEnumerable<IEnumerable<char>> input, bool part2)
+    {
+        string[] vals = input.Select(c => c.AsString()).ToArray();
+        var groups = DoGrouping(vals.WithIndex(1).Select(c => (c.Index, value: c.Value)).ToArray(), part2);
+
+        return groups.Select(g => ValidateGroup(g.Value, vals, groups))
+                     .Where(v => v.found && v.isBlotchy == part2).FirstOrDefault().position;
+    }
+
+    public static int GetMirrorScore(string input, bool part2)
+    {
+        var g = Util.ParseMatrix<char>(input);
+
+        var col = FindMirror(g.Columns(), part2);
+        return col > 0 ? col : FindMirror(g.Rows(), part2) * 100;
+    }
+
+    public static bool StringsDifferByNoMoreThanOne(string in1, string in2)
+    {
+        if (in1 == in2) return true;
+        int diff = 0;
+
+        for (int i = 0; i < in1.Length; ++i)
+        {
+            if (in1[i] != in2[i] && (++diff > 1)) return false;
+        }
+
+        return true;
+    }
+
+    public static int Part1(string input) => input.Split("\n\n").Sum(grid => GetMirrorScore(grid, false));
+
+    public static int Part2(string input) => input.Split("\n\n").Sum(grid => GetMirrorScore(grid, true));
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2023/Day13Test.cs
+++ test/Advent2023/Day13Test.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AoC.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AoC.Advent2023.Test;
 
@@ -6,36 +7,155 @@ namespace AoC.Advent2023.Test;
 [TestClass]
 public class Day13Test
 {
-    readonly string input = ""; // Util.GetInput<Day13>();
+    readonly string input = Util.GetInput<Day13>();
+
+    readonly string test = @"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#".Replace("\r", "");
+
+    readonly string test1 = @"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.".Replace("\r", "");
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void FindColMirrorTest()
     {
-        Assert.AreEqual(expected, Day13.Part1(input));
+        Assert.AreEqual(5, Day13.FindMirror(Util.ParseMatrix<char>(test1).Columns(), false));
+    }
+
+    readonly string test2 = @"#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#".Replace("\r", "");
+
+    [TestCategory("Test")]
+    [TestMethod]
+    public void FindRowMirrorTest()
+    {
+        Assert.AreEqual(4, Day13.FindMirror(Util.ParseMatrix<char>(test2).Rows(), false));
+    }
+
+    /*
+
+    #....#.##   1
+    ...##.#.#   2
+    ###..#...   3
+    #....#.##   4
+    ######...   5
+    ..##.##..   6
+    ..#...#.#   7
+    ..#...#.#   8
+    ..##.##..   9
+    ######...   10
+    #....#.##   11
+    ###..#...   12
+    ...##.#.#   13
+    #...##.## v 14
+    #...##.## ^ 15
+
+    */
+
+    const string test3 = "#....#.##\n...##.#.#\n###..#...\n#....#.##\n######...\n..##.##..\n..#...#.#\n..#...#.#\n..##.##..\n######...\n#....#.##\n###..#...\n...##.#.#\n#...##.##\n#...##.##";
+
+    /*
+    
+    #..###.#.
+    ...#...##
+    ##...##..
+    ###.###..
+    ##...#.##
+    ...###.##
+    ...#.#.##
+    ##...#.##
+    ###.###..
+    ##...##..
+    ...#...##
+    #..###.#.
+    #..###.#.
+
+    */
+
+    const string test4 = "#..###.#.\n...#...##\n##...##..\n###.###..\n##...#.##\n...###.##\n...#.#.##\n##...#.##\n###.###..\n##...##..\n...#...##\n#..###.#.\n#..###.#.";
+
+    const string test5 = "..##.\n##..#\n#...#\n..##.\n..##.";
+
+    /*
+
+
+..##.
+##..#
+#...#
+..##.
+..##.
+
+    */
+
+    [TestCategory("Test")]
+    [DataRow(test3, 1400)]
+    [DataRow(test4, 1200)]
+    [DataRow(test5, 400)]
+    [DataTestMethod]
+    public void GetScoreTest(string input, int expected)
+    {
+        Assert.AreEqual(expected, Day13.GetMirrorScore(input, false));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void FindRowMirrorTestPt2()
     {
-        Assert.AreEqual(expected, Day13.Part2(input));
+        Assert.AreEqual(3, Day13.FindMirror(Util.ParseMatrix<char>(test1).Rows(), true));
+    }
+
+    [TestCategory("Test")]
+    [TestMethod]
+    public void Mirroring_01Test()
+    {
+        Assert.AreEqual(405, Day13.Part1(test));
+    }
+
+    [TestCategory("Test")]
+    [DataRow("###", "#.#", true)]
+    [DataRow(".##", "#.#", false)]
+    [DataRow(".##", ".##", true)]
+    [DataTestMethod]
+    public void DiffByOneTest(string in1, string in2, bool expected)
+    {
+        Assert.AreEqual(expected, Day13.StringsDifferByNoMoreThanOne(in1, in2));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void Mirroring_Part1_Regression()
     {
-        Assert.AreEqual(0, Day13.Part1(input));
+        Assert.AreEqual(37381, Day13.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void Mirroring_Part2_Regression()
     {
-        Assert.AreEqual(0, Day13.Part2(input));
+        Assert.AreNotEqual(37381, Day13.Part2(input));
     }
 }
 
