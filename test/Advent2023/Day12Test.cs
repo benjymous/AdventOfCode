838aa6b2949befd2f59fab1f1197254824d56998
benjymous <r.munn@outlook.com>
2023-12-14 10:18:31 +0000

    2023 Day 12 now working

--- test/Advent2023/Day12Test.cs
+++ test/Advent2023/Day12Test.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Linq;
 
 namespace AoC.Advent2023.Test;
 
@@ -8,20 +7,6 @@ namespace AoC.Advent2023.Test;
 public class Day12Test
 {
     readonly string input = Util.GetInput<Day12>();
-
-    [TestCategory("Test")]
-    [DataRow("#.#.###", new int[] { 1, 1, 3 })]
-    [DataRow(".#...#....###.", new int[] { 1, 1, 3 })]
-    [DataRow(".#.###.#.######", new int[] { 1, 3, 1, 6 })]
-    [DataRow("####.#...#...", new int[] { 4, 1, 1 })]
-    [DataRow("#....######..#####.", new int[] { 1, 6, 5 })]
-    [DataRow(".###.##....#", new int[] { 3, 2, 1 })]
-    [DataTestMethod]
-    public void SpringCount_Test(string input, int[] expected)
-    {
-        var count = Day12.CountSprings(input.Select(c => c == '#').ToArray());
-        Assert.IsTrue(expected.SequenceEqual(count));
-    }
 
     readonly string test = @"???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -59,15 +44,24 @@ public class Day12Test
     [DataRow("???.### 1,1,3", 1)]
     [DataRow(".??..??...?##. 1,1,3", 4)]
     [DataRow(".??..??... 1,1", 4)]
+    [DataRow(".??. 1", 2)]
     [DataRow("??..?? 1,1", 4)]
-    //[DataRow("?#?#?#?#?#?#?#? 1,3,1,6", 1)]
-    //[DataRow("????.#...#... 4,1,1", 1)]
-    //[DataRow("????.######..#####. 1,6,5", 4)]
-    //[DataRow("?###???????? 3,2,1", 10)]
+    [DataRow("?#?#?#?#?#?#?#? 1,3,1,6", 1)]
+    [DataRow("????.#...#... 4,1,1", 1)]
+    [DataRow("????.######..#####. 1,6,5", 4)]
+    [DataRow("?###???????? 3,2,1", 10)]
+    [DataRow("??????? 2,1", 10)]
+    [DataRow("#?? 2", 1)]
     [DataTestMethod]
-    public void Springs01Test1(string input, int expected)
+    public void Springs01aTest(string input, int expected)
     {
-        Assert.AreEqual(expected, Day12.Solve1Single(Util.FromString<Day12.Row>(input)));
+        Assert.AreEqual(expected, Solve1Single(input));
+    }
+
+    static long Solve1Single(string input)
+    {
+        var row = Util.FromString<Day12.Row>(input);
+        return Day12.Solve(row.Input, row.Lengths);
     }
 
     [TestCategory("Test")]
@@ -86,16 +80,16 @@ public class Day12Test
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void HotSprings_Part1_Regression()
     {
         Assert.AreEqual(7771, Day12.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void HotSprings_Part2_Regression()
     {
-        Assert.AreEqual(0, Day12.Part2(input));
+        Assert.AreEqual(10861030975833, Day12.Part2(input));
     }
 }
 
