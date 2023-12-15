985fea30825ccbdb7be84dd5750377f91b919106
benjymous <r.munn@outlook.com>
2023-12-15 08:34:22 +0000

    2023 Day 15

---
+++ AoC/Advent2023/Day15_LensLibrary.cs
@@ -0,0 +1,56 @@
+﻿namespace AoC.Advent2023;
+public class Day15 : IPuzzle
+{
+    public static int CalculateHASH(IEnumerable<char> collection) => collection.Aggregate(0, (hash, v) => (hash + v) * 17 % 256);
+
+    public record struct Instruction(string Label, bool Put, int FocalLength = 0)
+    {
+        [Regex(@"([a-z]+)=(\d+)")]
+        public Instruction(string label, int focalLength) : this(label, true, focalLength) { }
+
+        [Regex(@"([a-z]+)-")]
+        public Instruction(string label) : this(label, false) { }
+
+        public readonly int BoxNumber = CalculateHASH(Label);
+    }
+
+    public record class LensBox(int BoxIndex)
+    {
+        public readonly List<(string Label, int FocalLength)> Lenses = [];
+
+        public void AddLens((string Label, int FocalLength) lens)
+        {
+            var existing = Lenses.FirstOrDefault(l => l.Label == lens.Label);
+
+            if (existing != default) Lenses[Lenses.IndexOf(existing)] = lens;
+            else Lenses.Add(lens);
+        }
+
+        public void RemoveLens(string label) => Lenses.Remove(Lenses.FirstOrDefault(l => l.Label == label));
+
+        public int GetPower() => Lenses.WithIndex().Sum(entry => (BoxIndex + 1) * (entry.Index + 1) * entry.Value.FocalLength);
+    }
+
+    public static int Part1(string input) => Util.Split(input).Sum(CalculateHASH);
+
+    public static int Part2(string input)
+    {
+        var boxes = Enumerable.Range(0, 256).Select(i => new LensBox(i)).ToArray();
+
+        foreach (var instr in Util.RegexParse<Instruction>(Util.Split(input)))
+        {
+            if (instr.Put)
+                boxes[instr.BoxNumber].AddLens((instr.Label, instr.FocalLength));
+            else
+                boxes[instr.BoxNumber].RemoveLens(instr.Label);
+        }
+
+        return boxes.Sum(b => b.GetPower());
+    }
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2023/Day15Test.cs
+++ test/Advent2023/Day15Test.cs
@@ -6,36 +6,74 @@ namespace AoC.Advent2023.Test;
 [TestClass]
 public class Day15Test
 {
-    readonly string input = ""; // Util.GetInput<Day15>();
+    readonly string input = Util.GetInput<Day15>();
+    readonly string test = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow("rn=1", 30)]
+    [DataRow("cm-", 253)]
+    [DataRow("qp=3", 97)]
+    [DataRow("cm=2", 47)]
+    [DataRow("qp-", 14)]
+    [DataRow("pc=4", 180)]
+    [DataRow("ot=9", 9)]
+    [DataRow("ab=5", 197)]
+    [DataRow("pc-", 48)]
+    [DataRow("pc=6", 214)]
+    [DataRow("ot=7", 231)]
     [DataTestMethod]
-    public void _01Test(string input, int expected)
+    public void HashTest(string input, int expected)
     {
-        Assert.AreEqual(expected, Day15.Part1(input));
+        Assert.AreEqual(expected, Day15.CalculateHASH(input));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void Hashing01Test()
     {
-        Assert.AreEqual(expected, Day15.Part2(input));
+        Assert.AreEqual(1320, Day15.Part1(test));
+    }
+
+    [TestCategory("Test")]
+    [TestMethod]
+    public void LensBoxTest()
+    {
+        var Box = new Day15.LensBox(0);
+
+        var Lens1 = (Label: "A", FocalLength: 1);
+        var Lens2 = (Label: "B", FocalLength: 2);
+        var Lens3 = (Label: "A", FocalLength: 3);
+
+        Box.AddLens(Lens1);
+        Box.AddLens(Lens2);
+        Box.AddLens(Lens3);
+
+        Assert.AreEqual(2, Box.Lenses.Count);
+        Assert.AreEqual("A", Box.Lenses[0].Label);
+        Assert.AreEqual(3, Box.Lenses[0].FocalLength);
+        Assert.AreEqual("B", Box.Lenses[1].Label);
+        Assert.AreEqual(2, Box.Lenses[1].FocalLength);
+    }
+
+    [TestCategory("Test")]
+    [TestMethod]
+    public void Boxing02Test()
+    {
+        Assert.AreEqual(145, Day15.Part2(test));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void LensLibrary_Part1_Regression()
     {
-        Assert.AreEqual(0, Day15.Part1(input));
+        Assert.AreEqual(507666, Day15.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void LensLibrary_Part2_Regression()
     {
-        Assert.AreEqual(0, Day15.Part2(input));
+        Assert.AreEqual(233537, Day15.Part2(input));
     }
 }
 
