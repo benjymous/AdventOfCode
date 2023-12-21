825def716f1b9c4c62d7f94e5c9121841af884b3
benjymous <r.munn@outlook.com>
2023-12-21 11:45:15 +0000

    2023 Day 21 WIP

--- AoC/Advent2023/Day21_.cs
+++ AoC/Advent2023/Day21_.cs
@@ -1,17 +1,138 @@
 ﻿namespace AoC.Advent2023;
+
 public class Day21 : IPuzzle
 {
-    public static int Part1(string input)
+    static ((int x, int y) start, HashSet<(int x, int y)> walkable, int gridSize) ParseData(string input)
     {
-        Console.WriteLine(input);
-        return 0;
+        var map = Util.ParseSparseMatrix<char>(input);
+        var start = map.Where(kvp => kvp.Value == 'S').Single().Key;
+        var walkable = map.Where(kvp => kvp.Value != '#').Select(kvp => kvp.Key).ToHashSet();
+
+        return (start, walkable, map.Keys.Max(v => v.x) + 1);
     }
 
-    public static int Part2(string input)
+    public static int CountSteps(string input, int targetSteps)
     {
-        Console.WriteLine(input);
-        return 0;
+        var (start, walkable, _) = ParseData(input);
+
+        return WalkMap(targetSteps, start, walkable);
     }
+
+    private static int WalkMap(int targetSteps, (int x, int y) start, HashSet<(int x, int y)> walkable)
+    {
+        var ends = new HashSet<(int x, int y)>();
+        var visited = new HashSet<(int x, int y)>();
+
+        (int dx, int dy)[] neighbours = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+        Queue<((int x, int y) pos, int steps)> queue = [];
+        queue.Enqueue((start, 0));
+
+        while (queue.TryDequeue(out var state))
+        {
+            if (state.steps % 2 == 0)
+            {
+                ends.Add(state.pos);
+
+                if (state.steps == targetSteps || visited.Contains(state.pos))
+                {
+                    continue;
+                }
+            }
+
+            visited.Add(state.pos);
+
+            foreach (var delta in neighbours)
+            {
+                var next = state.pos.OffsetBy(delta);
+                if (walkable.Contains(next))
+                {
+                    queue.Enqueue((next, state.steps + 1));
+                }
+            }
+        }
+
+        return ends.Count;
+    }
+
+    public static long SolvePart2(string input, int maxDist)
+    {
+        var (start, walkable, gridSize) = ParseData(input);
+        var fullGridOdd = walkable.Where(v => (v.Distance(start) % 2) == 1).Count();
+        var fullGridEven = walkable.Where(v => (v.Distance(start) % 2) == 0).Count();
+
+        var oddEven = maxDist % 2;
+        int range = (maxDist / gridSize) + 1;
+        Console.WriteLine($"oddEven: {oddEven}");
+        Console.WriteLine($"distance: {maxDist}");
+        Console.WriteLine($"gridSize: {gridSize}");
+        Console.WriteLine($"range: {range}");
+
+        //range = 0;
+
+        long count = 0;
+
+        for (int y = -range; y <= range; ++y)
+        {
+            for (int x = -range; x <= range; ++x)
+            {
+                var cell = (x, y);
+
+                var realCentre = (x: cell.x * gridSize, y: cell.y * gridSize);
+
+                int centreDistance = realCentre.Distance(0, 0);
+
+                if (centreDistance - gridSize > maxDist)
+                {
+                    continue;
+                }
+
+                int thisOddEven = (centreDistance % 2) + oddEven;
+
+                //Console.WriteLine();
+                //Console.WriteLine($"{cell} : {realCentre} : {centreDistance} : {(thisOddEven == 0 ? "even" : "odd")}");
+
+                if (centreDistance < maxDist - gridSize)
+                {
+
+                    //Console.WriteLine($"{cell} is a full cell");
+                    count += thisOddEven == 0 ? fullGridEven : fullGridOdd;
+                }
+                else
+                {
+
+                    var translated = walkable.Select(p => (p.x - start.x + realCentre.x, p.y - start.y + realCentre.y));
+                    var distances = translated.Select(p => p.Distance(0, 0));
+                    var checks = distances.Where(d => d <= maxDist && ((d % 2) == thisOddEven));
+
+                    var subCount = checks.Count();
+                    //                    .Distance(0, 0)).Where(d => d <= maxDist && ((d % 2) == thisOddEven)).Count();
+
+                    count += subCount;
+
+                    //Console.WriteLine($"Need to count in {cell}");
+                }
+            }
+        }
+
+        //for (int y = 0; y < gridSize; ++y)
+        //{
+        //    for (int x = 0; x < gridSize; ++x)
+        //    {
+        //        if ((x, y) == start) Console.Write("S");
+        //        else if (fullGridOdd.Contains((x, y))) Console.Write("O");
+        //        else if (walkable.Contains((x, y))) Console.Write(".");
+        //        else Console.Write("#");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        return count;
+    }
+
+    public static int Part1(string input) => CountSteps(input, 64);
+
+    public static long Part2(string input) => SolvePart2(input, 26501365);
 
     public void Run(string input, ILogger logger)
     {
--- test/Advent2023/Day21Test.cs
+++ test/Advent2023/Day21Test.cs
@@ -6,29 +6,58 @@ namespace AoC.Advent2023.Test;
 [TestClass]
 public class Day21Test
 {
-    readonly string input = ""; // Util.GetInput<Day21>();
+    readonly string input = Util.GetInput<Day21>();
+    readonly string test = @"...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........".Replace("\r", "");
+
+    readonly string test2 = @"...\n.S.\n...";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void _01Test()
     {
-        Assert.AreEqual(expected, Day21.Part1(input));
+        Assert.AreEqual(16, Day21.CountSteps(test, 6));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow(6, 16)]
+    [DataRow(10, 50)]
+    [DataRow(50, 1594)]
+    [DataRow(100, 6536)]
+    [DataRow(500, 167004)]
+    [DataRow(1000, 668697)]
+    [DataRow(5000, 16733044)]
     [DataTestMethod]
-    public void _02Test(string input, int expected)
+    public void _02Test(int input, int expected)
     {
-        Assert.AreEqual(expected, Day21.Part2(input));
+        Assert.AreEqual(expected, Day21.SolvePart2(test, input));
+    }
+
+    [TestCategory("Test")]
+    [DataRow(1, 4)]
+    [DataRow(10, 0)]
+    [DataRow(50, 0)]
+    [DataRow(100, 0)]
+    [DataTestMethod]
+    public void _02aTest(int input, int expected)
+    {
+        Assert.AreEqual(expected, Day21.SolvePart2(test2, input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
     public void _Part1_Regression()
     {
-        Assert.AreEqual(0, Day21.Part1(input));
+        Assert.AreEqual(3682, Day21.Part1(input));
     }
 
     [TestCategory("Regression")]
