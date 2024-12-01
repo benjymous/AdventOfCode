f4528c0cb153087242c97f61bfefd04c9b0f1a61
benjymous <r.munn@outlook.com>
2024-12-01 08:56:23 +0000

    2024 Day 1

---
+++ AoC/Advent2024/Day01_HistorianHysteria.cs
@@ -0,0 +1,29 @@
+﻿namespace AoC.Advent2024;
+public class Day01 : IPuzzle
+{
+    private static (int[], int[]) Parse(string input)
+    {
+        var data = Util.Split(input).Select(Util.ExtractNumbers).ToList();
+        return (data.Select(v => v[0]).ToArray(), data.Select(v => v[1]).ToArray());
+    }
+
+    public static int Part1(string input)
+    {
+        var (a, b) = Parse(input);
+
+        return a.Order().Zip(b.Order()).Sum((v) => Math.Abs(v.First - v.Second));
+    }
+
+    public static int Part2(string input)
+    {
+        var (a, b) = Parse(input);
+
+        return a.Sum(i => i * b.Count(j => j == i));
+    }
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2024/Day01Test.cs
+++ test/Advent2024/Day01Test.cs
@@ -6,35 +6,35 @@ namespace AoC.Advent2024.Test;
 [TestClass]
 public class Day01Test
 {
-    readonly string input = ""; // Util.GetInput<Day01>();
+    readonly string input = Util.GetInput<Day01>();
+
+    string test = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void Historian_01Test()
     {
-        Assert.AreEqual(expected, Day01.Part1(input));
+        Assert.AreEqual(11, Day01.Part1(test));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void Historian_02Test()
     {
-        Assert.AreEqual(expected, Day01.Part2(input));
+        Assert.AreEqual(31, Day01.Part2(test));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void Historian_Part1_Regression()
     {
-        Assert.AreEqual(0, Day01.Part1(input));
+        Assert.AreEqual(1830467, Day01.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void Historian_Part2_Regression()
     {
-        Assert.AreEqual(0, Day01.Part2(input));
+        Assert.AreEqual(26674158, Day01.Part2(input));
     }
 }
