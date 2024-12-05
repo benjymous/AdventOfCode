0dc5f97fd3ff0f9e577cceb412df06823e727b89
benjymous <r.munn@outlook.com>
2024-12-05 15:55:12 +0000

    2024 Day 5

---
+++ AoC/Advent2024/Day05_PrintQueue.cs
@@ -0,0 +1,31 @@
+﻿namespace AoC.Advent2024;
+public class Day05 : IPuzzle
+{
+    public class UpdatePackage() : IComparer<int>
+    {
+        readonly HashSet<(int Before, int After)> Rules = [];
+        public readonly List<List<int>> ValidSets = [], InvalidSets = [];
+
+        [Regex(@"(\d+)\|(\d+)")] public void Rule(int before, int after) => Rules.Add((before, after));
+        [Regex(@"(.+)")] public void PageSet(List<int> pages) => (IsValid(pages) ? ValidSets : InvalidSets).Add(pages);
+
+        public int Compare(int x, int y) => Rules.Contains((x, y)) ? -1 : 1;
+
+        bool IsValid(List<int> pages)
+            => pages.OverlappingPairs().All(p => Compare(p.first, p.second) == -1);
+
+        public static implicit operator UpdatePackage(string data) => Parser.Factory<UpdatePackage>(data);
+    }
+
+    public static int Part1(UpdatePackage package)
+        => package.ValidSets.Sum(u => u[u.Count / 2]);
+
+    public static int Part2(UpdatePackage package)
+        => package.InvalidSets.Sum(u => { u.Sort(package); return u[u.Count / 2]; });
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2024/Day05Test.cs
+++ test/Advent2024/Day05Test.cs
@@ -6,35 +6,61 @@ namespace AoC.Advent2024.Test;
 [TestClass]
 public class Day05Test
 {
-    readonly string input = ""; // Util.GetInput<Day05>();
+    readonly string input = Util.GetInput<Day05>();
+    readonly string test = @"47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47".Replace("\r", "");
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void PageOrder_01Test()
     {
-        Assert.AreEqual(expected, Day05.Part1(input));
+        Assert.AreEqual(143, Day05.Part1(test));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void PageOrder_02Test()
     {
-        Assert.AreEqual(expected, Day05.Part2(input));
+        Assert.AreEqual(123, Day05.Part2(test));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void PrintQueue_Part1_Regression()
     {
-        Assert.AreEqual(0, Day05.Part1(input));
+        Assert.AreEqual(6034, Day05.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void PrintQueue_Part2_Regression()
     {
-        Assert.AreEqual(0, Day05.Part2(input));
+        Assert.AreEqual(6305, Day05.Part2(input));
     }
 }
