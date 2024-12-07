d894f55460663867a96aa7385eac1eae01cbd8b8
benjymous <r.munn@outlook.com>
2024-12-07 10:33:10 +0000

    2024 Day 7

---
+++ AoC/Advent2024/Day07_BridgeRepair.cs
@@ -0,0 +1,36 @@
+﻿namespace AoC.Advent2024;
+public class Day07 : IPuzzle
+{
+    [Regex(@"(\d+): (.+)")]
+    record struct Record(long TestValue, [Split(" ")] long[] Values);
+
+    static long Value(QuestionPart part, Record r)
+        => IsValid(part, r.TestValue, r.Values.First(), r.Values.Skip(1)) ? r.TestValue : 0;
+
+    static bool IsValid(QuestionPart part, long lhs, long next, IEnumerable<long> rest)
+        => rest.Any()
+             ? next <= lhs && (
+                 (part.Two() && IsValid(part, lhs, Concat(next, rest.First()), rest.Skip(1))) ||
+                 IsValid(part, lhs, next * rest.First(), rest.Skip(1)) ||
+                 IsValid(part, lhs, next + rest.First(), rest.Skip(1)))
+             : next == lhs;
+
+    static long Concat(long a, long b)
+    {
+        long multiplier = 10;
+        while (b >= multiplier) multiplier *= 10;
+        return (a * multiplier) + b;
+    }
+
+    static long Solve(string input, QuestionPart part)
+        => Parser.Parse<Record>(input).AsParallel().Sum(r => Value(part, r));
+
+    public static long Part1(string input) => Solve(input, QuestionPart.Part1);
+    public static long Part2(string input) => Solve(input, QuestionPart.Part2);
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2024/Day07Test.cs
+++ test/Advent2024/Day07Test.cs
@@ -6,35 +6,42 @@ namespace AoC.Advent2024.Test;
 [TestClass]
 public class Day07Test
 {
-    readonly string input = ""; // Util.GetInput<Day07>();
+    readonly string input = Util.GetInput<Day07>();
+    readonly string test = @"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20".Replace("\r","");
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void Operators_01Test()
     {
-        Assert.AreEqual(expected, Day07.Part1(input));
+        Assert.AreEqual(3749, Day07.Part1(test));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void Operators_02Test()
     {
-        Assert.AreEqual(expected, Day07.Part2(input));
+        Assert.AreEqual(11387, Day07.Part2(test));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void BridgeMaths_Part1_Regression()
     {
-        Assert.AreEqual(0, Day07.Part1(input));
+        Assert.AreEqual(66343330034722, Day07.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void BridgeMaths_Part2_Regression()
     {
-        Assert.AreEqual(0, Day07.Part2(input));
+        Assert.AreEqual(637696070419031, Day07.Part2(input));
     }
 }
