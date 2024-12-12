6a9389eb129c11d55d385a01ad3b4568a68ae3d2
benjymous <r.munn@outlook.com>
2024-12-12 13:13:40 +0000

    2024 Day 12

---
+++ AoC/Advent2024/Day12_GardenGroups.cs
@@ -0,0 +1,52 @@
+﻿namespace AoC.Advent2024;
+public class Day12 : IPuzzle
+{
+    static readonly (int, int)[] neighbours = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+    static IEnumerable<HashSet<(int, int)>> GetRegions(string input)
+    {
+        var map = Util.ParseSparseMatrix<char>(input);
+
+        return map.Select(kvp => FloodFill(map, kvp.Key, kvp.Value).ToHashSet());
+    }
+
+    static IEnumerable<(int x, int y)> FloodFill(Dictionary<(int, int), char> map, (int x, int y) pos, char expected)
+    {
+        if (!map.TryGetValue(pos, out var next) || next != expected) return [];
+
+        map.Remove(pos);
+        return [pos, .. neighbours.SelectMany(d => FloodFill(map, pos.OffsetBy(d), expected))];
+    }
+
+    static IEnumerable<((int, int), (int, int))> GetPerimeter(HashSet<(int, int)> region)
+        => region.SelectMany(cell => neighbours.Where(n => !region.Contains(cell.OffsetBy(n))).Select(n => (cell, n)));
+
+    static bool IsAdjacent(((int x, int y) p, (int, int) d) a, ((int x, int y) p, (int, int) d) b)
+        => !(a.d != b.d) && (a.p.x == b.p.x ? Math.Abs(a.p.y - b.p.y) == 1 : a.p.y == b.p.y && Math.Abs(a.p.x - b.p.x) == 1);
+
+    static int CountEdges(IEnumerable<((int, int) pos, (int, int))> perim)
+    {
+        List<List<((int, int), (int, int))>> edges = [];
+
+        foreach (var (face, edge) in perim.OrderBy(v => v.pos)
+            .Select(section => (section, edges.FirstOrDefault(e => e.Any(p => IsAdjacent(section, p))))))
+        {
+            if (edge != default) edge.Add(face);
+            else edges.Add([face]);
+        }
+
+        return edges.Count;
+    }
+
+    public static int Part1(string input)
+        => GetRegions(input).Sum(region => GetPerimeter(region).Count() * region.Count);
+
+    public static int Part2(string input)
+        => GetRegions(input).Sum(region => CountEdges(GetPerimeter(region)) * region.Count);
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2024/Day12Test.cs
+++ test/Advent2024/Day12Test.cs
@@ -6,35 +6,70 @@ namespace AoC.Advent2024.Test;
 [TestClass]
 public class Day12Test
 {
-    readonly string input = ""; // Util.GetInput<Day12>();
+    readonly string input = Util.GetInput<Day12>();
+    readonly string test = @"RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE".Replace("\r", "");
+
+    readonly string test2 = @"EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE".Replace("\r", "");
+
+    readonly string test3 = @"AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA".Replace("\r", "");
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void Fencing_01Test()
     {
-        Assert.AreEqual(expected, Day12.Part1(input));
+        Assert.AreEqual(1930, Day12.Part1(test));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void Fencing_02Test()
     {
-        Assert.AreEqual(expected, Day12.Part2(input));
+        Assert.AreEqual(1206, Day12.Part2(test));
+    }
+
+    [TestCategory("Test")]
+    [TestMethod]
+    public void Fencing_02aTest()
+    {
+        Assert.AreEqual(236, Day12.Part2(test2));
+    }
+
+    [TestCategory("Test")]
+    [TestMethod]
+    public void Fencing_02bTest()
+    {
+        Assert.AreEqual(368, Day12.Part2(test3));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void Fencing_Part1_Regression()
     {
-        Assert.AreEqual(0, Day12.Part1(input));
+        Assert.AreEqual(1415378, Day12.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void Fencing_Part2_Regression()
     {
-        Assert.AreEqual(0, Day12.Part2(input));
+        Assert.AreEqual(862714, Day12.Part2(input));
     }
 }
