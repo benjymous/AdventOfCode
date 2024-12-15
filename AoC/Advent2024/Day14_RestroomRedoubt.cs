a5970fb9bcbe7bd8c8cf6afb88037675a5dc87c7
benjymous <r.munn@outlook.com>
2024-12-15 20:11:59 +0000

    2024 Day 14

---
+++ AoC/Advent2024/Day14_RestroomRedoubt.cs
@@ -0,0 +1,73 @@
+﻿namespace AoC.Advent2024;
+public class Day14 : IPuzzle
+{
+    [Regex(@"p=(.+) v=(.+)")]
+    record struct Guard([Regex(@"(\d+),(\d+)")] (int x, int y) pos, [Regex(@"(-?\d+),(-?\d+)")] (int x, int y) speed)
+    {
+        public (int,int) StepGuard(int steps, int width, int height)
+        {
+            var finalPos = pos.OffsetBy(speed, steps);
+            while (finalPos.x < width) finalPos.x += width;
+            while (finalPos.y < height) finalPos.y += height;
+            finalPos.x %= width;
+            finalPos.y %= height;
+            pos = finalPos;
+            return finalPos;
+        }
+    }
+
+    public static long SolvePt1(string input, int width, int height)
+    {
+        var data = Parser.Parse<Guard>(input).ToArray();
+
+        for (int i = 0; i < data.Length; ++i)
+        {
+            data[i].StepGuard(100, width, height);
+        }
+
+        int[] quadrants = new int[4];
+        var hw = width / 2;
+        var hh = height / 2;
+        foreach (var (x, y) in data.Select(g => g.pos))
+        {
+            if (x < hw && y < hh) quadrants[0]++;
+            else if (x < hw && y > hh) quadrants[1]++;
+            else if (x > hw && y < hh) quadrants[2]++;
+            else if (x > hw && y > hh) quadrants[3]++;
+        }
+
+        return quadrants.Product();
+    }
+
+    public static long Part1(string input) => SolvePt1(input, 101, 103);
+
+    public static int Part2(string input)
+    {
+        int width = 101;
+        int height = 103;
+
+        var data = Parser.Parse<Guard>(input).ToArray();
+
+        int iter = 1;
+        while (true)
+        {
+            HashSet<(int, int)> dupes = [];
+            bool unique = true;
+
+            for (int i = 0; i < data.Length; ++i)
+            {
+                unique &= dupes.Add(data[i].StepGuard(1, width, height));
+            }
+
+            if (unique) return iter;
+            
+            iter++;
+        }
+    }
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2024/Day14Test.cs
+++ test/Advent2024/Day14Test.cs
@@ -6,35 +6,43 @@ namespace AoC.Advent2024.Test;
 [TestClass]
 public class Day14Test
 {
-    readonly string input = ""; // Util.GetInput<Day14>();
+    readonly string input = Util.GetInput<Day14>();
+    readonly string test = @"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void GuardQuadrants_01Test()
     {
-        Assert.AreEqual(expected, Day14.Part1(input));
-    }
+        int width = 11;
+        int height = 7;
 
-    [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
-    {
-        Assert.AreEqual(expected, Day14.Part2(input));
+        var r = Day14.SolvePt1(test, width, height);
+
+        Assert.AreEqual(12, r);
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void GuardWatching_Part1_Regression()
     {
-        Assert.AreEqual(0, Day14.Part1(input));
+        Assert.AreEqual(222901875, Day14.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void GuardWatching_Part2_Regression()
     {
-        Assert.AreEqual(0, Day14.Part2(input));
+        Assert.AreEqual(6243, Day14.Part2(input));
     }
 }
