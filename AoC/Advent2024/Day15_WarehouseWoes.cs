22a843a10807f1baa5ab942ba554b023faa0eb8d
benjymous <r.munn@outlook.com>
2024-12-15 20:13:02 +0000

    2024 Day 15

---
+++ AoC/Advent2024/Day15_WarehouseWoes.cs
@@ -0,0 +1,72 @@
+﻿namespace AoC.Advent2024;
+public class Day15 : IPuzzle
+{
+    class Warehouse
+    {
+        public Warehouse(string input)
+        {
+            var parts = input.SplitSections();
+
+            var grid = Util.ParseSparseMatrix<char>(parts[0], new Util.Convertomatic.SkipChars('.'));
+            Instructions = parts[1].Replace("\n", "").Select(c => new Direction2(c)).ToList();
+
+            Bot = grid.KeysWithValue('@').Single();
+            Boxes = grid.KeysWithValue('O').ToHashSet();
+            WideBoxes = grid.KeysWithValue('[').ToHashSet();
+            Walls = grid.KeysWithValue('#').ToHashSet();
+        }
+
+        public int PerformMoves()
+        {
+            Instructions.ForEach(d => { var np = Bot.OffsetBy(d); if (TryMove(np, d)) Bot = np; });
+            return Boxes.Sum(p => (p.y * 100) + p.x) + WideBoxes.Sum(p => (p.y * 100) + p.x);
+        }
+
+        static bool DoMove(HashSet<(int, int)> set, (int, int) oldPos, (int, int) newPos)
+            => set.Remove(oldPos) && set.Add(newPos);
+
+        bool TryMove((int x, int y) pos, Direction2 dir)
+        {
+            var isBox = Boxes.Contains(pos);
+            var isWideBox = WideBoxes.Contains(pos);
+            var isWideBoxSide = WideBoxes.Contains((pos.x - 1, pos.y));
+            var isWall = Walls.Contains(pos);
+            var nextPos = pos.OffsetBy(dir);
+            var nextPosRight = (pos.x + 1, pos.y).OffsetBy(dir);
+            return (!isBox && !isWall && !isWideBox && !isWideBoxSide) || 
+                   (!isWall && (isBox ? TryMove(nextPos, dir) &&
+                DoMove(Boxes, pos, nextPos) : isWideBox ? dir.DY == 0 ? TryMove(nextPos, dir) &&
+                DoMove(WideBoxes, pos, nextPos) : TestMove(nextPos, dir) && TestMove(nextPosRight, dir) &&
+                TryMove(nextPos, dir) && TryMove(nextPosRight, dir) && DoMove(WideBoxes, pos, nextPos) :
+                isWideBoxSide && (dir.DY == 0 ? (dir.DX == -1 ? TryMove((pos.x - 1, pos.y), dir) :
+                TryMove((pos.x + 1, pos.y), dir)) : TryMove((pos.x - 1, pos.y), dir))));
+        }
+
+        bool TestMove((int x, int y) pos, Direction2 dir)
+        {
+            var isWideBox = WideBoxes.Contains(pos);
+            var isWideBoxSide = WideBoxes.Contains((pos.x - 1, pos.y));
+            var isWall = Walls.Contains(pos);
+            return (!isWall && !isWideBox && !isWideBoxSide) || !isWall && (isWideBox ? dir.DY == 0 ?
+                TestMove(pos.OffsetBy(dir), dir) : TestMove(pos.OffsetBy(dir), dir) &&
+                TestMove((pos.x + 1, pos.y).OffsetBy(dir), dir) : isWideBoxSide && TestMove((pos.x - 1, pos.y), dir));
+        }
+
+        (int x, int y) Bot;
+        readonly HashSet<(int x, int y)> Boxes, WideBoxes, Walls;
+
+        readonly List<Direction2> Instructions;
+    }
+
+    static string DoubleMap(string input)
+        => input.Replace("#", "##").Replace("O", "[]").Replace(".", "..").Replace("@", "@.");
+
+    public static int Part1(string input) => new Warehouse(input).PerformMoves();
+    public static int Part2(string input) => new Warehouse(DoubleMap(input)).PerformMoves();
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2024/Day15Test.cs
+++ test/Advent2024/Day15Test.cs
@@ -6,35 +6,79 @@ namespace AoC.Advent2024.Test;
 [TestClass]
 public class Day15Test
 {
-    readonly string input = ""; // Util.GetInput<Day15>();
+    readonly string input = Util.GetInput<Day15>();
+    const string test1 = @"##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
+
+    const string test2 = @"########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<";
+
+    const string test3 = @"#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow(test1, 10092)]
+    [DataRow(test2, 2028)]
     [DataTestMethod]
     public void _01Test(string input, int expected)
     {
-        Assert.AreEqual(expected, Day15.Part1(input));
+        Assert.AreEqual(expected, Day15.Part1(input.Replace("\r", "")));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow(test1, 9021)]
+    [DataRow(test3, 618)]
     [DataTestMethod]
     public void _02Test(string input, int expected)
     {
-        Assert.AreEqual(expected, Day15.Part2(input));
+        Assert.AreEqual(expected, Day15.Part2(input.Replace("\r","")));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
     public void _Part1_Regression()
     {
-        Assert.AreEqual(0, Day15.Part1(input));
+        Assert.AreEqual(1413675, Day15.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
     public void _Part2_Regression()
     {
-        Assert.AreEqual(0, Day15.Part2(input));
+        Assert.AreEqual(1399772, Day15.Part2(input));
     }
 }
