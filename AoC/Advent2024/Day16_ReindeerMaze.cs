1bec11b46e27c101f385e73d9a91621ecec151e5
benjymous <r.munn@outlook.com>
2024-12-16 18:51:29 +0000

    2024 Day 16

---
+++ AoC/Advent2024/Day16_ReindeerMaze.cs
@@ -0,0 +1,65 @@
+﻿namespace AoC.Advent2024;
+public class Day16 : IPuzzle
+{
+    static IEnumerable<((int, int) newPos, char newDir, int cost)> GetMoves(HashSet<(int, int)> walkable, (int, int) pos, Direction2 d)
+    {
+        var forward = pos.OffsetBy(d);
+        if (walkable.Contains(forward)) yield return (forward, d, 1);
+        var d2 = d - 1;
+        if (walkable.Contains(pos.OffsetBy(d2))) yield return (pos, d2, 1000);
+        var d3 = d + 1;
+        if (walkable.Contains(pos.OffsetBy(d3))) yield return (pos, d3, 1000);
+    }
+
+    private static (int best, int visited) Solve(string input)
+    {
+        return Memoize(input, _ =>
+        {
+            var grid = Util.ParseSparseMatrix<char>(input, new Util.Convertomatic.SkipChars('#'));
+            var walkable = grid.Keys.ToHashSet();
+            var start = grid.KeysWithValue('S').Single();
+            var end = grid.KeysWithValue('E').Single();
+
+            PriorityQueue<((int x, int y) pos, char dir, PackedPos32[] history), int> queue = new();
+            queue.Enqueue((start, '>', [start]), 0);
+
+            Dictionary<((int, int), char), int> seen = [];
+
+            int best = int.MaxValue;
+            HashSet<PackedPos32> bestVisited = [];
+
+            while (queue.TryDequeue(out var step, out int score))
+            {
+                if (step.pos == end)
+                {
+                    if (score <= best)
+                    {
+                        if (best > score)
+                            bestVisited = [.. step.history];
+                        else
+                            bestVisited.UnionWith(step.history);
+                        best = score;
+                    }
+
+                    continue;
+                }
+
+                if (score >= best || (seen.TryGetValue((step.pos, step.dir), out var prev) && prev < score)) continue;
+                seen[(step.pos, step.dir)] = score;
+
+                queue.EnqueueRange(GetMoves(walkable, step.pos, step.dir)
+                     .Select(m => ((m.newPos, m.newDir,  (PackedPos32[])([m.newPos, .. step.history])), score + m.cost)));
+            }
+            return (best, bestVisited.Count);
+        });
+    }
+
+    public static int Part1(string input) => Solve(input).best;
+    public static int Part2(string input) => Solve(input).visited;
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2024/Day16Test.cs
+++ test/Advent2024/Day16Test.cs
@@ -6,35 +6,70 @@ namespace AoC.Advent2024.Test;
 [TestClass]
 public class Day16Test
 {
-    readonly string input = ""; // Util.GetInput<Day16>();
+    readonly string input = Util.GetInput<Day16>();
+    const string test1 = @"###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############";
+
+    const string test2 = @"#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow(test1, 7036)]
+    [DataRow(test2, 11048)]
     [DataTestMethod]
-    public void _01Test(string input, int expected)
+    public void ReindeerMaze_01Test(string input, int expected)
     {
         Assert.AreEqual(expected, Day16.Part1(input));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
+    [DataRow(test1, 45)]
+    [DataRow(test2, 64)]
     [DataTestMethod]
-    public void _02Test(string input, int expected)
+    public void ReindeerMaze_02Test(string input, int expected)
     {
         Assert.AreEqual(expected, Day16.Part2(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void ReindeerMaze_Part1_Regression()
     {
-        Assert.AreEqual(0, Day16.Part1(input));
+        Assert.AreEqual(134588, Day16.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void ReindeerMaze_Part2_Regression()
     {
-        Assert.AreEqual(0, Day16.Part2(input));
+        Assert.AreEqual(631, Day16.Part2(input));
     }
 }
