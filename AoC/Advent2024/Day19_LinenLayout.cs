0200421bc51e3ffb4ead7e9b10ce95b1bf8291dd
benjymous <r.munn@outlook.com>
2024-12-19 09:11:36 +0000

    2024 Day 19

---
+++ AoC/Advent2024/Day19_LinenLayout.cs
@@ -0,0 +1,34 @@
+﻿namespace AoC.Advent2024;
+public class Day19 : IPuzzle
+{
+    const int MaxLen = 8;
+    public static IEnumerable<int> GetPrefixes(HashSet<string> parts, string line)
+        => Enumerable.Range(1, Math.Min(MaxLen, line.Length)).Where(i => parts.Contains(line[..i]));
+
+    public static bool IsMatch(HashSet<string> parts, string line) => Memoize((line, parts.Count), _ =>
+        line == "" || GetPrefixes(parts, line).Any(part => IsMatch(parts, line[part..])));
+
+    public static long CountMatches(HashSet<string> parts, string line) => Memoize((line, parts.Count), _ =>
+        line == "" ? 1 : GetPrefixes(parts, line).Sum(part => CountMatches(parts, line[part..])));
+
+    public static (HashSet<string>, string[]) SplitData(string input)
+        => input.ParseSections(p1 => p1.Split(", ").ToHashSet(), p2 => Util.Split(p2));
+
+    public static int Part1(string input)
+    {
+        var (pattern, lines) = SplitData(input);
+        return lines.Count(l => IsMatch(pattern, l));
+    }
+
+    public static long Part2(string input)
+    {
+        var (pattern, lines) = SplitData(input);
+        return lines.Sum(l => CountMatches(pattern, l));
+    }
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2024/Day19Test.cs
+++ test/Advent2024/Day19Test.cs
@@ -6,35 +6,43 @@ namespace AoC.Advent2024.Test;
 [TestClass]
 public class Day19Test
 {
-    readonly string input = ""; // Util.GetInput<Day19>();
+    readonly string input = Util.GetInput<Day19>();
+    readonly string test = @"r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb".Replace("\r", "");
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
     [DataTestMethod]
-    public void _01Test(string input, int expected)
+    public void Towels_01Test()
     {
-        Assert.AreEqual(expected, Day19.Part1(input));
+        Assert.AreEqual(6, Day19.Part1(test));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
     [DataTestMethod]
-    public void _02Test(string input, int expected)
+    public void Towels_02Test()
     {
-        Assert.AreEqual(expected, Day19.Part2(input));
+        Assert.AreEqual(16, Day19.Part2(test));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void TowelPatterns_Part1_Regression()
     {
-        Assert.AreEqual(0, Day19.Part1(input));
+        Assert.AreEqual(313, Day19.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void TowelPatterns_Part2_Regression()
     {
-        Assert.AreEqual(0, Day19.Part2(input));
+        Assert.AreEqual(666491493769758, Day19.Part2(input));
     }
 }
