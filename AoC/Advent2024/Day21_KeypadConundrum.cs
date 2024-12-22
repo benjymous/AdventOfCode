097b1824b4e24a06dd8d8e03951cda2a2a18de43
benjymous <r.munn@outlook.com>
2024-12-22 12:23:56 +0000

    2024 Day 21

---
+++ AoC/Advent2024/Day21_KeypadConundrum.cs
@@ -0,0 +1,84 @@
+﻿namespace AoC.Advent2024;
+
+public class Day21 : IPuzzle
+{
+    static readonly string numericKeypad = "789\n456\n123\n 0A";
+    static readonly string directionPad = " ^A\n<v>";
+
+    public static long GetScore(string line, int level = 2)
+    {
+        var numPad = Util.ParseSparseMatrix<char>(numericKeypad, new Util.Convertomatic.SkipChars(' '));
+        var dirPad = Util.ParseSparseMatrix<char>(directionPad, new Util.Convertomatic.SkipChars(' '));
+
+        return CalculateScore(line, level + 1, numPad, dirPad) * int.Parse(line.Replace("A", ""));
+    }
+
+    private static long CalculateScore(string line, int level, Dictionary<(int, int), char> numPad, Dictionary<(int, int), char> dirPad)
+    {
+        if (level == 0 || line=="A")
+        {
+            return line.Length;
+        }
+
+        var parts = line.Split('A', StringSplitOptions.RemoveEmptyEntries).Select(p => p + "A").ToArray();
+
+        if (parts.Length == 1)
+        {
+            return ("A" + line).OverlappingPairs().Sum(pair => GetKeyCombos(pair.first, pair.second, level, numPad, dirPad));
+        }
+        else
+        {
+            return parts.Sum(subpart => CalculateScore(subpart, level - 1, numPad, dirPad));
+        }
+    }
+
+    public static long GetKeyCombos(char from, char to, int level, Dictionary<(int, int), char> numPad, Dictionary<(int, int), char> dirPad)
+    {
+        return Memoize((from, to, level), _ =>
+        {
+            Dictionary<(int x, int y), char> pad = from.IsDigit() || to.IsDigit() ? numPad : dirPad;
+
+            Queue<(string current, (int x, int y) position)> queue = [];
+            queue.Enqueue(("", pad.KeysWithValue(from).Single()));
+
+            long smallest = long.MaxValue;
+            var desired = pad.KeysWithValue(to).Single();
+
+            while (queue.TryDequeue(out var state))
+            {
+                if (!pad.ContainsKey(state.position)) continue;
+
+                if (state.position == desired)
+                {
+                    smallest = Math.Min(smallest, CalculateScore(state.current + 'A', level - 1, numPad, dirPad));
+                }
+                else
+                {
+                    var dx = Math.Sign(desired.x - state.position.x);
+                    var dy = Math.Sign(desired.y - state.position.y);
+
+                    if (dx != 0)
+                    {
+                        queue.Enqueue((state.current + (dx == -1 ? '<' : '>'), (state.position.x + dx, state.position.y)));
+                    }
+                    if (dy != 0)
+                    {
+                        queue.Enqueue((state.current + (dy == -1 ? '^' : 'v'), (state.position.x, state.position.y + dy)));
+                    }
+                }
+            }
+
+            return smallest;
+        });
+    }
+
+    public static long Part1(string input) => Util.Split(input).Sum(line => GetScore(line));
+
+    public static long Part2(string input) => Util.Split(input).Sum(line => GetScore(line, 25));
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2024/Day21Test.cs
+++ test/Advent2024/Day21Test.cs
@@ -6,14 +6,34 @@ namespace AoC.Advent2024.Test;
 [TestClass]
 public class Day21Test
 {
-    readonly string input = ""; // Util.GetInput<Day21>();
+    readonly string input = Util.GetInput<Day21>();
+    readonly string test = "029A\n980A\n179A\n456A\n379A";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
     [DataTestMethod]
-    public void _01Test(string input, int expected)
+    public void _01Test()
     {
-        Assert.AreEqual(expected, Day21.Part1(input));
+        Assert.AreEqual(126384, Day21.Part1(test));
+    }
+
+    [TestCategory("Test")]
+
+    [DataRow("029A", 68 * 29)]
+    [DataRow("980A", 60 * 980)]
+    [DataRow("179A", 68 * 179)]
+    [DataRow("456A", 64 * 456)]
+    [DataRow("379A", 64 * 379)]
+
+    [DataRow("879A", 70 * 879)]
+    [DataRow("682A", 68 * 682)]
+    [DataRow("413A", 70 * 413)]
+    [DataRow("480A", 74 * 480)]
+    [DataRow("083A", 66 * 83)]
+
+    [DataTestMethod]
+    public void _01aTest(string input, int expected)
+    {
+        Assert.AreEqual(expected, Day21.GetScore(input));
     }
 
     [TestCategory("Test")]
@@ -28,13 +48,13 @@ public class Day21Test
     [DataTestMethod]
     public void _Part1_Regression()
     {
-        Assert.AreEqual(0, Day21.Part1(input));
+        Assert.AreEqual(177814, Day21.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
     public void _Part2_Regression()
     {
-        Assert.AreEqual(0, Day21.Part2(input));
+        Assert.AreEqual(220493992841852, Day21.Part2(input));
     }
 }
