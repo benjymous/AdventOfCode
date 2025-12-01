e336885dfb548bfa215b4e46370ab4fa8d6c75c7
benjymous <r.munn@outlook.com>
2025-12-01 08:02:44 +0000

    2025 Day 1

---
+++ AoC/Advent2025/Day01_SecretEntrance.cs
@@ -0,0 +1,52 @@
+﻿namespace AoC.Advent2025;
+
+public class Day01 : IPuzzle
+{
+    [Regex("(.)(.+)")]
+    public record class Instruction(char Dir, int Value);
+
+    static int FindPasscode(Parser.AutoArray<Instruction> instructions, QuestionPart part)
+    {
+        int pos = 50, count = 0;
+        foreach (var i in instructions)
+        {
+            (pos, count) = RotateDial(part, pos, count, i.Value * (i.Dir == 'R' ? 1 : -1));
+        }
+
+        return count;
+    }
+
+    static (int pos, int count) RotateDial(QuestionPart part, int position, int count, int delta)
+    {
+        if (part.One)
+        {
+            position += delta;
+            position.ModWrap(100);
+            return (position, position == 0 ? count + 1 : count);
+        }
+        else
+        {
+            var s = Math.Sign(delta);
+            while (delta != 0)
+            {
+                position += s;
+                delta -= s;
+                if (position < 0) position = 99;
+                if (position > 99) position = 0;
+                if (position == 0) count++;
+            }
+
+            return (position, count);
+        }
+    }
+
+    public static int Part1(string input) => FindPasscode(input, QuestionPart.Part1);
+
+    public static int Part2(string input) => FindPasscode(input, QuestionPart.Part2);
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2025/Day01Test.cs
+++ test/Advent2025/Day01Test.cs
@@ -6,35 +6,35 @@ namespace AoC.Advent2025.Test;
 [TestClass]
 public class Day01Test
 {
-    readonly string input = ""; // Util.GetInput<Day01>();
+    readonly string input = Util.GetInput<Day01>();
+
+    readonly string test = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void CombinationLock_01Test()
     {
-        Assert.AreEqual(expected, Day01.Part1(input));
+        Assert.AreEqual(3, Day01.Part1(test));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void CombinationLock_02Test()
     {
-        Assert.AreEqual(expected, Day01.Part2(input));
+        Assert.AreEqual(6, Day01.Part2(test));
     }
 
     [TestCategory("Regression")]
-    [DataTestMethod]
-    public void _Part1_Regression()
+    [TestMethod]
+    public void CombinationLock_Part1_Regression()
     {
-        Assert.AreEqual(0, Day01.Part1(input));
+        Assert.AreEqual(995, Day01.Part1(input));
     }
 
     [TestCategory("Regression")]
-    [DataTestMethod]
-    public void _Part2_Regression()
+    [TestMethod]
+    public void CombinationLock_Part2_Regression()
     {
-        Assert.AreEqual(0, Day01.Part2(input));
+        Assert.AreEqual(5847, Day01.Part2(input));
     }
 }
