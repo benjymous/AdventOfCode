5e3617b24cdbd25ab186287291a4beb0912b2df6
benjymous <r.munn@outlook.com>
2025-12-06 13:29:56 +0000

    2025 Day 6

---
+++ AoC/Advent2025/Day06_TrashCompactor.cs
@@ -0,0 +1,77 @@
+﻿namespace AoC.Advent2025;
+public class Day06 : IPuzzle
+{
+    static long Operation(char op, long lhs, long rhs)
+        => op == '+' ? lhs + rhs : lhs * rhs;
+
+    public static long Part1(string input)
+    {
+        var rowsRaw = Util.Split(input).Reverse().ToArray();
+
+        var ops = Util.Split(rowsRaw.First(), " ").Select(s => s[0]).ToArray();
+
+        var numberLines = rowsRaw.Skip(1).Select(line => Util.ParseNumbers<long>(line, " "));
+
+        var values = numberLines.First();
+
+        foreach (var e in numberLines.Skip(1))
+        {
+            for (var i = 0; i < e.Length; i++)
+            {
+                values[i] = Operation(ops[i], values[i], e[i]);
+            }
+        }
+       
+        return values.Sum();
+    }
+
+    public static long Part2(string input)
+    {
+        var grid = Util.Split(input.Replace("\n", " \n"));
+
+        long total = 0;
+        char op = ' ';
+        long current = 0;
+
+        int cols = grid[0].Length;
+        int rows = grid.Length;
+
+        for (int i=0; i<cols; i++)
+        {
+            var str = "";
+
+            for (int row = 0; row < rows; row++)
+            {
+                str += grid[row][i];
+            }
+
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                total += current;
+            }
+            else
+            {
+                var num = long.Parse(str[..^1]);
+                var endChar = str[^1];
+
+                if (endChar == ' ')
+                {
+                    current = Operation(op, current, num);
+                }
+                else
+                {
+                    current = num;
+                    op = endChar;
+                }
+            }
+        }
+
+        return total;
+    }
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2025/Day06Test.cs
+++ test/Advent2025/Day06Test.cs
@@ -6,35 +6,34 @@ namespace AoC.Advent2025.Test;
 [TestClass]
 public class Day06Test
 {
-    readonly string input = ""; // Util.GetInput<Day06>();
+    readonly string input = Util.GetInput<Day06>();
+    readonly string test = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  \n";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void CephalopodMaths_01Test()
     {
-        Assert.AreEqual(expected, Day06.Part1(input));
+        Assert.AreEqual(4277556, Day06.Part1(test));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void CephalopodMaths_02Test()
     {
-        Assert.AreEqual(expected, Day06.Part2(input));
+        Assert.AreEqual(3263827, Day06.Part2(test));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void CephalopodMaths_Part1_Regression()
     {
-        Assert.AreEqual(0, Day06.Part1(input));
+        Assert.AreEqual(7229350537438, Day06.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void CephalopodMaths_Part2_Regression()
     {
-        Assert.AreEqual(0, Day06.Part2(input));
+        Assert.AreEqual(11479269003550, Day06.Part2(input));
     }
 }
