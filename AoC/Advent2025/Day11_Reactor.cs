19d14273db1e6d3010fff9dcb647460280b31bc6
benjymous <r.munn@outlook.com>
2025-12-11 08:37:55 +0000

    2025 Day 11

---
+++ AoC/Advent2025/Day11_Reactor.cs
@@ -0,0 +1,38 @@
+﻿namespace AoC.Advent2025;
+
+public class Day11 : IPuzzle
+{
+    class TreeFactory()
+    {
+        public readonly Tree<string> Tree = new();
+
+        [Regex("(...): (.+)")] public void Node(string key, [Split(" ")] string[] children) => Tree.AddChildren(key, children);
+        
+        public static TreeNode<string, object> Build(string input, string root) => Parser.Factory<TreeFactory>(input).Tree[root];
+    }
+
+    static long FindPathsToOut(TreeNode<string, object> node) => FindWorkingPathsToOut(node, node, false, false);
+    static long FindWorkingPathsToOut(TreeNode<string, object> node) => FindWorkingPathsToOut(node, node);
+    static long FindWorkingPathsToOut(TreeNode<string, object> node, TreeNode<string, object> root, bool needsDac = true, bool needsFft = true) => root.Memoize((node.Id, needsDac, needsFft), _ =>
+    {
+        if (node.Key == "dac") needsDac = false;
+        else if (node.Key == "fft") needsFft = false;
+
+        if (!needsDac && !needsFft && node.Children.Any(n => n.Key == "out"))
+        {
+            return 1;
+        }
+
+        return node.Children.Sum(c => FindWorkingPathsToOut(c, root, needsDac, needsFft));
+    });
+
+    public static long Part1(string input) => FindPathsToOut(TreeFactory.Build(input, "you"));
+
+    public static long Part2(string input) => FindWorkingPathsToOut(TreeFactory.Build(input, "svr"));
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input));
+    }
+}
--- test/Advent2025/Day11Test.cs
+++ test/Advent2025/Day11Test.cs
@@ -6,35 +6,35 @@ namespace AoC.Advent2025.Test;
 [TestClass]
 public class Day11Test
 {
-    readonly string input = ""; // Util.GetInput<Day11>();
+    readonly string input = Util.GetInput<Day11>();
+    readonly string test1 = "aaa: you hhh\nyou: bbb ccc\nbbb: ddd eee\nccc: ddd eee fff\nddd: ggg\neee: out\nfff: out\nggg: out\nhhh: ccc fff iii\niii: out";
+    readonly string test2 = "svr: aaa bbb\naaa: fft\nfft: ccc\nbbb: tty\ntty: ccc\nccc: ddd eee\nddd: hub\nhub: fff\neee: dac\ndac: fff\nfff: ggg hhh\nggg: out\nhhh: out";
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
+    [TestMethod]
+    public void ReactorPath_01Test()
     {
-        Assert.AreEqual(expected, Day11.Part1(input));
+        Assert.AreEqual(5, Day11.Part1(test1));
     }
 
     [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
+    [TestMethod]
+    public void ReactorPath_02Test()
     {
-        Assert.AreEqual(expected, Day11.Part2(input));
+        Assert.AreEqual(2, Day11.Part2(test2));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void Reactor_Part1_Regression()
     {
-        Assert.AreEqual(0, Day11.Part1(input));
+        Assert.AreEqual(724, Day11.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void Reactor_Part2_Regression()
     {
-        Assert.AreEqual(0, Day11.Part2(input));
+        Assert.AreEqual(473930047491888, Day11.Part2(input));
     }
 }
