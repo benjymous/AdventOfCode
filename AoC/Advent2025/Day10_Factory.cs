acafd6f2cce2b250d19524c14713c642f6122fca
benjymous <r.munn@outlook.com>
2025-12-11 16:24:03 +0000

    2025 Day 10 working

---
+++ AoC/Advent2025/Day10_Factory.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Z3;
+
+namespace AoC.Advent2025;
+
+public class Day10 : IPuzzle
+{
+    public class Machine
+    {
+        bool[] TargetLights = [];
+        int[] TargetJolts = [];
+        readonly List<HashSet<int>> ButtonActions = [];
+
+        public Machine(string input) => Parser.Factory(input, this, " ");
+
+        public int SolvePart1()
+        {
+            int target = TargetLights.Reverse().Aggregate(0, (v, b) => (v << 1) + (b ? 1 : 0));
+            var buttons = ButtonActions.Select(a => a.Sum(v => 1 << v)).ToArray();
+            
+            return Solver<(int lights, int presses)>.Solve((0, 0), (state, solver) =>
+            {
+                if (state.lights == target) return state.presses;
+
+                if ((solver.CurrentBest == null || state.presses < solver.CurrentBest) && solver.IsBetterThanSeen(state.lights, state.presses))
+                {
+                    solver.EnqueueRange(buttons.Select(c => ((state.lights ^ c, state.presses + 1), state.presses + 1)));
+                }
+
+                return default;
+            }, Math.Min);
+        }
+
+        public int SolvePart2()
+        {
+            using var ctx = new Context();
+            var opt = ctx.MkOptimize();
+
+            int numTargets = TargetJolts.Length;
+            int numButtons = ButtonActions.Count;
+
+            // Each button can be pressed 0 or more times
+            var numPresses = new IntExpr[numButtons];
+            for (int j = 0; j < numButtons; j++)
+            {
+                numPresses[j] = ctx.MkIntConst($"x_{j}");
+                opt.Assert(ctx.MkGe(numPresses[j], ctx.MkInt(0))); // numPresses[j] >= 0
+            }
+
+            // Define how button presses effect target jolts
+            // for each target, its value is the sum of all the button presses
+            // that effect that target
+            foreach (var target in TargetJolts.Index())
+            {
+                var terms = ButtonActions.Index()
+                    .Where(button => button.Item.Contains(target.Index))
+                    .Select(button => numPresses[button.Index]).ToList();
+
+                var lhs = terms.Count == 0 ? ctx.MkInt(0) : ctx.MkAdd(terms);
+                var rhs = ctx.MkInt(target.Item);
+
+                opt.Assert(ctx.MkEq(lhs, rhs));
+            }
+
+            // Objective: minimize sum of press values  (total presses)
+            var sumPresses = ctx.MkAdd(numPresses);
+            opt.MkMinimize(sumPresses);
+
+            if (opt.Check() != Status.SATISFIABLE) return 0; // No solution
+
+            // result 
+            var model = opt.Model;
+            return numPresses.Sum(press => ((IntNum)model.Eval(press, true)).Int);
+        }
+
+        [Regex(@"\[(.+)\]")] public void ParseLights(string l) => TargetLights = [.. l.Select(c => c == '#')];
+        [Regex(@"\((.+)\)")] public void ParseButton(int[] actions) => ButtonActions.Add([.. actions]);
+        [Regex(@"\{(.+)\}")] public void ParseJolts(int[] jolts) => TargetJolts = jolts;
+    }
+
+    static IEnumerable<Machine> Parse(string input) => Util.Split(input, "\n").Select(r => new Machine(r));
+
+    public static int Part1(string input) => Parse(input).Sum(r => r.SolvePart1());
+
+    public static int Part2(string input, ILogger logger = null)
+        => Parse(input)
+          .AsParallel()
+          .Select((r) => r.SolvePart2()).Sum();
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+        logger.WriteLine("- Pt2 - " + Part2(input, logger));
+    }
+}
--- test/Advent2025/Day10Test.cs
+++ test/Advent2025/Day10Test.cs
@@ -11,14 +11,14 @@ public class Day10Test
 
     [TestCategory("Test")]
     [TestMethod]
-    public void _01Test()
+    public void ButtonCombos01Test()
     {
         Assert.AreEqual(7, Day10.Part1(test));
     }
 
     [TestCategory("Test")]
     [TestMethod]
-    public void _02Test()
+    public void ButtonCombos02Test()
     {
         Assert.AreEqual(33, Day10.Part2(test));
     }
@@ -30,12 +30,12 @@ public class Day10Test
     [DataRow("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", 12)]
     [DataRow("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", 11)]
 
-    //[DataRow("[#.##] (3) (1,3) (0,3) (0,2) (0,1,3) (0) {42,36,8,56}", 64)]
-    //[DataRow("[#.#.#] (0,1,2) (1,3,4) (0,1) (1,3) (2,4) (0,1,4) (1,2,4) {28,50,33,6,46}", 57)]
+    [DataRow("[#.##] (3) (1,3) (0,3) (0,2) (0,1,3) (0) {42,36,8,56}", 64)]
+    [DataRow("[#.#.#] (0,1,2) (1,3,4) (0,1) (1,3) (2,4) (0,1,4) (1,2,4) {28,50,33,6,46}", 57)]
 
-    [DataRow("[#.....#..#] (1,3,4,6,8) (0,2,3,4,5,7,9) (2,3,4,8) (0,4,8) (5,7) (6,7) (0,1,3,4,6,7,8,9) (0,1,2,4,5,7,8) {34,38,37,53,64,18,40,47,57,23}", 0)]
+    [DataRow("[#.....#..#] (1,3,4,6,8) (0,2,3,4,5,7,9) (2,3,4,8) (0,4,8) (5,7) (6,7) (0,1,3,4,6,7,8,9) (0,1,2,4,5,7,8) {34,38,37,53,64,18,40,47,57,23}", 77)]
 
-    //[DataRow("[..#..] (1,4) (0,3) (2) {4,11,19,4,11}", 0)]
+    [DataRow("[..#..] (1,4) (0,3) (2) {4,11,19,4,11}", 34)]
     public void Solver02Test(string input, int expected)
     {
         Assert.AreEqual(expected, Day10.Part2(input));
@@ -43,15 +43,15 @@ public class Day10Test
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void Factory_Part1_Regression()
     {
         Assert.AreEqual(484, Day10.Part1(input));
     }
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part2_Regression()
+    public void Factory_Part2_Regression()
     {
-        Assert.AreEqual(0, Day10.Part2(input));
+        Assert.AreEqual(19210, Day10.Part2(input));
     }
 }
