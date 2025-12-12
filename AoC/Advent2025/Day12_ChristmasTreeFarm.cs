cefed2e0f4a3a901df4934e832d59ac17588fc12
benjymous <r.munn@outlook.com>
2025-12-12 11:53:43 +0000

    2025 Day 12

---
+++ AoC/Advent2025/Day12_ChristmasTreeFarm.cs
@@ -0,0 +1,34 @@
+﻿namespace AoC.Advent2025;
+
+public class Day12 : IPuzzle
+{
+    [Regex(@"\d:\s(.+\s.+\s.+)")]
+    record class Shape(string Data)
+    {
+        public int Count = Data.Count(v => v == '#');
+    }
+
+    [Regex(@"(\d+)x(\d+): (.+)")]
+    record class Grid(int W, int H, [Split(" ")] int[] Contents)
+    {
+        public bool CanSolve(Shape[] shapes) => Contents.Index().Sum(v => shapes[v.Index].Count * Contents[v.Index]) <= W * H;
+    }
+
+    public static int Part1(string input)
+    {
+        var sections = input.SplitSections();
+
+        int numShapes = sections.Length - 1;
+
+        var shapes = Parser.Parse<Shape>(sections.Take(numShapes)).ToArray();
+
+        var grids = Parser.Parse<Grid>(sections.Last());
+
+        return grids.Count(g => g.CanSolve(shapes));
+    }
+
+    public void Run(string input, ILogger logger)
+    {
+        logger.WriteLine("- Pt1 - " + Part1(input));
+    }
+}
--- test/Advent2025/Day12Test.cs
+++ test/Advent2025/Day12Test.cs
@@ -6,35 +6,12 @@ namespace AoC.Advent2025.Test;
 [TestClass]
 public class Day12Test
 {
-    readonly string input = ""; // Util.GetInput<Day12>();
-
-    [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _01Test(string input, int expected)
-    {
-        Assert.AreEqual(expected, Day12.Part1(input));
-    }
-
-    [TestCategory("Test")]
-    [DataRow("???", 0)]
-    [DataTestMethod]
-    public void _02Test(string input, int expected)
-    {
-        Assert.AreEqual(expected, Day12.Part2(input));
-    }
+    readonly string input = Util.GetInput<Day12>();
 
     [TestCategory("Regression")]
     [DataTestMethod]
-    public void _Part1_Regression()
+    public void Presentris_Part1_Regression()
     {
-        Assert.AreEqual(0, Day12.Part1(input));
-    }
-
-    [TestCategory("Regression")]
-    [DataTestMethod]
-    public void _Part2_Regression()
-    {
-        Assert.AreEqual(0, Day12.Part2(input));
+        Assert.AreEqual(534, Day12.Part1(input));
     }
 }
